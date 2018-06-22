6cdf380479fd6e872bd146b684fc63f8a11c8f32
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-22 14:19:13 -0400

    only build if we need to

--- scripts/ci-travis.sh
+++ scripts/ci-travis.sh
@@ -13,8 +13,9 @@ if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
 
     if [[ $PROJECT = *"Frontend" ]]; then
         # TODO: eventually, linting should be required to pass
-        ./run ci:lint:ui
-        ./run ci:test:ui
+        time ./run dc build
+        time ./run ci:lint:ui
+        time ./run ci:test:ui
     else
         echo "Only the frontend project is configured for contiuous testing"
     fi
