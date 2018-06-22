a95c72019337d667c43f56bb5f2d119f30271a90
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-22 09:54:22 -0400

    have to upgrade pip

--- scripts/ci-travis.sh
+++ scripts/ci-travis.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 # https://github.com/sagikazarmark/travis-monorepo-demo
 
-echo "============================================"
-if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
-    echo "$PROJECT is being built";
-    echo ""
+# for testing CI, comment out all the conditions and messaging
+./run test:ui:ci
 
-    if [[ $PROJECT = *"Frontend" ]]; then
-        ./run test:ui:ci
-    else
-        echo "Only the frontend project is configured for contiuous testing"
-    fi
-else
-    echo "$PROJECT is NOT being built";
-    echo ""
-fi
+#
+# echo "============================================"
+# if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
+#     echo "$PROJECT is being built";
+#     echo ""
+#
+#     if [[ $PROJECT = *"Frontend" ]]; then
+#         ./run test:ui:ci
+#     else
+#         echo "Only the frontend project is configured for contiuous testing"
+#     fi
+# else
+#     echo "$PROJECT is NOT being built";
+#     echo ""
+# fi
