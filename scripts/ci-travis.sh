5a9c8a2804933d9add09193ec5a04d2e614d6c61
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-22 14:11:03 -0400

    rebuild everything

--- scripts/ci-travis.sh
+++ scripts/ci-travis.sh
@@ -2,20 +2,23 @@
 # https://github.com/sagikazarmark/travis-monorepo-demo
 
 # for testing CI, comment out all the conditions and messaging
-./run ci:lint:ui || ./run ci:test:ui
+# ./run ci:lint:ui
+# ./run ci:test:ui
 
 
-# echo "============================================"
-# if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
-#     echo "$PROJECT is being built";
-#     echo ""
-#
-#     if [[ $PROJECT = *"Frontend" ]]; then
-#         ./run ci:lint:ui && ./run ci:test:ui
-#     else
-#         echo "Only the frontend project is configured for contiuous testing"
-#     fi
-# else
-#     echo "$PROJECT is NOT being built because no changes were detected.";
-#     echo ""
-# fi
+echo "============================================"
+if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
+    echo "$PROJECT is being built";
+    echo ""
+
+    if [[ $PROJECT = *"Frontend" ]]; then
+        # TODO: eventually, linting should be required to pass
+        ./run ci:lint:ui
+        ./run ci:test:ui
+    else
+        echo "Only the frontend project is configured for contiuous testing"
+    fi
+else
+    echo "$PROJECT is NOT being built because no changes were detected.";
+    echo ""
+fi
