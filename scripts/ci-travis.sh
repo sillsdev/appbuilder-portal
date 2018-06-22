3f30d143221bfbe990b7b03abb719daac5880cef
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-22 09:36:37 -0400

    try a docker-compose build?

--- scripts/ci-travis.sh
+++ scripts/ci-travis.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # https://github.com/sagikazarmark/travis-monorepo-demo
 
-if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then 
+if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
     echo "$PROJECT is being built";
 
     if [[ $PROJECT = *"Frontend" ]]; then
-        ./run yarn test:ci
+        ./run test:ui:ci
     else
         echo "Only the frontend project is configured for contiuous testing"
     fi
 else
     echo "$PROJECT is NOT being built";
 fi
-
