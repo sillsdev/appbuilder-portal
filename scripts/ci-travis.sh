faacdb977f008a1301557efc54e17e066ca5d44f
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-22 10:33:30 -0400

    try caching all docker images?

--- scripts/ci-travis.sh
+++ scripts/ci-travis.sh
@@ -2,20 +2,20 @@
 # https://github.com/sagikazarmark/travis-monorepo-demo
 
 # for testing CI, comment out all the conditions and messaging
-# ./run test:ui:ci
+./run test:ui:ci
 
 
-echo "============================================"
-if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
-    echo "$PROJECT is being built";
-    echo ""
-
-    if [[ $PROJECT = *"Frontend" ]]; then
-        ./run test:ui:ci
-    else
-        echo "Only the frontend project is configured for contiuous testing"
-    fi
-else
-    echo "$PROJECT is NOT being built because no changes were detected.";
-    echo ""
-fi
+# echo "============================================"
+# if .travis/build-condition.sh $TRAVIS_COMMIT_RANGE $PROJECT; then
+#     echo "$PROJECT is being built";
+#     echo ""
+#
+#     if [[ $PROJECT = *"Frontend" ]]; then
+#         ./run test:ui:ci
+#     else
+#         echo "Only the frontend project is configured for contiuous testing"
+#     fi
+# else
+#     echo "$PROJECT is NOT being built because no changes were detected.";
+#     echo ""
+# fi
