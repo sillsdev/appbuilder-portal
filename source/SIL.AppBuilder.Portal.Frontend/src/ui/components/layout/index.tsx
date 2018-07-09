6ff70eb0992e45ceabcf84d56f8f57a544ecaea6
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-07-09 15:41:41 -0400

    make container span the whole screen

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/layout/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/layout/index.tsx
@@ -6,7 +6,7 @@ class Layout extends React.Component {
 
   render() {
     return (
-      <div className='wrapper'>
+      <div className='wrapper flex-grow'>
         <Header />
         <div>
           {this.props.children}
