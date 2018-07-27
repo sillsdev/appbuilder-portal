4ee0c8c9a4ee983de9696ab9a5686bf353ddb3bd
L. Preston Sego III <LPSego3+dev@gmail.com>
2018-07-27 15:04:52 -0400

    Feature/locale switcher (#47)
    * initial work on locale switcher and updating the user schema
    
    * woo
    
    * all tests pass
    
    * cleanup

---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/inputs/locale-select/index.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import i18n from '@ui/../translations';
+
+export interface IProps {
+  value: string;
+  onChange: () => void;
+  className?: string;
+}
+
+export default class LocaleSelect extends React.Component {
+  constructor(props = {}) {
+    super(props);
+
+    this.state = { value: props.value };
+  }
+
+  onSelect = (e) => {
+    const selection = e.target.value;
+
+    i18n.changeLanguage(selection);
+    this.props.onChange(selection);
+  }
+
+  render() {
+    const { value, onChange } = this.props;
+    const { default: { options, language } } = i18n;
+    const languages = Object.keys(options.resources);
+    const selected = value || language;
+
+    return (
+      <select
+        data-test-locale-switcher
+        value={selected} onChange={this.onSelect}>
+        { languages.map(locale => (
+          <option value={locale}>{locale}</option>
+        )) }
+      </select>
+    );
+  }
+}
--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/profile/form/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/profile/form/index.tsx
@@ -6,6 +6,7 @@ import { translate, InjectedTranslateProps as i18nProps } from 'react-i18next';
 import TimezonePicker from 'react-timezone';
 
 import { UserAttributes } from '@data/models/user';
+import LocaleSelect from '@ui/components/inputs/locale-select';
 
 
 export interface IProps {
@@ -50,7 +51,7 @@ class EditProfileDisplay extends React.Component<IProps & i18nProps, IState> {
     const { mut, toggle } = this;
     const {
       firstName,lastName, email, phone, localization,
-      timezone, emailNotification,
+      timezone, emailNotification, locale,
       sshKey
     } = this.state;
     const { t } = this.props;
@@ -100,26 +101,36 @@ class EditProfileDisplay extends React.Component<IProps & i18nProps, IState> {
             value={localization}
             onChange={mut('localization')} />
         </Form.Field>
-        <Form.Field>
-          <label>{t('profile.timezone')}</label>
-          <div
-            data-test-profile-timezone
-            className='timezone-group'
-          >
-            <Icon name='caret down' />
-            <TimezonePicker
-              className='timezone'
-              value={timezone}
-              onChange={tz => {
-                this.setState({timezone: tz});
-              }}
-              inputProps={{
-                placeholder: t('profile.timezonePlaceholder'),
-                name: 'timezone'
-              }}
-            />
+
+        <div className='flex-row justify-content-space-between'>
+          <Form.Field>
+            <label>{t('profile.timezone')}</label>
+            <div
+              data-test-profile-timezone
+              className='timezone-group'
+            >
+              <Icon name='caret down' />
+              <TimezonePicker
+                className='timezone'
+                value={timezone}
+                onChange={tz => {
+                  this.setState({timezone: tz});
+                }}
+                inputProps={{
+                  placeholder: t('profile.timezonePlaceholder'),
+                  name: 'timezone'
+                }}
+              />
+            </div>
+          </Form.Field>
+
+          <div className='field'>
+            <label data-test-locale-label>{t('profile.locale')}</label>
+            <LocaleSelect
+              className=''
+              value={locale} onChange={mut('locale')} />
           </div>
-        </Form.Field>
+        </div>
 
         <Divider horizontal/>
 
