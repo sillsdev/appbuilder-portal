fa090bd04c3d79848ea1a9d873b2c1424efa811e
giancorzo <gian.corzo@gmail.com>
2018-07-30 14:31:29 -0400

    Adding limits to dateRanges

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/project-directory/filters/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/project-directory/filters/index.tsx
@@ -14,7 +14,11 @@ import './filters.scss';
 
 interface IState {
   products: any[],
-  organizations: any[]
+  selectedProduct : string;
+  organizations: any[],
+  selectedOrganization: string;
+  from: any,
+  to: any
 }
 
 class Filter extends React.Component<i18nProps, IState> {
@@ -23,13 +27,55 @@ class Filter extends React.Component<i18nProps, IState> {
     super(props);
     this.state = {
       products: [{ text: 'All Products', value: 'all' }, { text: 'Android APK w/ Embedded Audio', value: 'android-apk' }, { text: 'HTML Website', value: 'website'}],
-      organizations: [{ text: 'All organizations', value: 'all'}, { text: 'SIL International', value: 'SIL'}]
-    }
+      selectedProduct: null,
+      organizations: [{ text: 'All organizations', value: 'all'}, { text: 'SIL International', value: 'SIL'}],
+      selectedOrganization: null,
+      from: '',
+      to: ''
+    };
+
+    this.handleProductChange = this.handleProductChange.bind(this);
+    this.handleOrganizationChange = this.handleOrganizationChange.bind(this);
+    this.handleToChange = this.handleToChange.bind(this);
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.disableFrom = this.disableFrom.bind(this);
+    this.disableTo = this.disableTo.bind(this);
+  }
+
+  handleProductChange(e) {
+    this.setState({ selectedProduct: e.target.value });
+  }
+
+  handleOrganizationChange(e) {
+    this.setState({ selectedOrganization: e.target.value });
+  }
+
+  handleToChange(to) {
+    this.setState({ to });
+  }
+
+  handleFromChange (from) {
+    this.setState({from});
+  }
+
+  disableFrom(day) {
+
+    const { to } = this.state;
+    return day > (new Date()) || (to !== '' && day > to);
+  }
+
+  disableTo(day) {
+
+    const { from } = this.state;
+    return day > (new Date()) || (from !== '' && day < from);
   }
 
   render() {
 
+    const today = new Date();
+
     const { t } = this.props;
+    const { from, to } = this.state;
 
     return (
       <div className='flex filters'>
@@ -38,11 +84,13 @@ class Filter extends React.Component<i18nProps, IState> {
             <Dropdown
               className='w-100'
               options={this.state.products}
+              onChange={this.handleProductChange}
               defaultValue={this.state.products[0].value} />
           </div>
           <div className='input'>
             <Dropdown
               className='w-100'
+              onChange={this.handleOrganizationChange}
               options={this.state.organizations}
               defaultValue={this.state.organizations[0].value} />
           </div>
@@ -53,7 +101,12 @@ class Filter extends React.Component<i18nProps, IState> {
             <Icon name='dropdown' />
             <DayPickerInput
               locale='en'
-              showOutsideDays
+              value={from}
+              dayPickerProps={{
+                disabledDays: this.disableFrom,
+                showOutsideDays: true
+              }}
+              onDayChange={this.handleFromChange}
               placeholder=''
               formatDate={formatDate}
               parseDate={parseDate}
@@ -63,10 +116,16 @@ class Filter extends React.Component<i18nProps, IState> {
             <Icon name='dropdown' />
             <DayPickerInput
               locale='en'
-              showOutsideDays
+              dayPickerProps={{
+                disabledDays: this.disableTo,
+                showOutsideDays: true
+              }}
+              onDayChange={this.handleToChange}
               placeholder=''
               formatDate={formatDate}
               parseDate={parseDate}
+              value={to}
+
             />
           </div>
         </div>
