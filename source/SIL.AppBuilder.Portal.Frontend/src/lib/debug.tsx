32ab6cf4c0c85444b711eb1cbc9d0d9c6832299a
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-15 16:04:26 -0400

    tslint

--- source/SIL.AppBuilder.Portal.Frontend/src/lib/debug.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/lib/debug.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export function withDebugger(InnerComponent) {
 
   return props => {
+    /* tslint:disable */
     debugger;
+    /* tslint:enable */
 
     return <InnerComponent { ...props } />;
   };
