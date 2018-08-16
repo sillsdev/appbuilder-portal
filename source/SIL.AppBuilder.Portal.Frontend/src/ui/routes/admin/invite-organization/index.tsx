b6fc7dd61f89b5ce6ff10430929201951bd45a0c
L. Preston Sego III <LPSego3+dev@gmail.com>
2018-08-16 12:51:00 -0400

    Fixes 28322 - Fields do not auto populate on super admin screen (#84)
    * fixed it
    
    * add url to submit
    
    * fix test
    
    * trigger travis

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/invite-organization/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/invite-organization/index.tsx
@@ -4,13 +4,17 @@ import { withData, WithDataProps } from 'react-orbitjs';
 import { compose } from 'recompose';
 
 import * as toast from '@lib/toast';
+import { defaultOptions } from '@data';
 import { OrganizationInviteAttributes, TYPE_NAME } from '@data/models/organization-invite';
+
+import { withQueryParams, IProvidedQueryParams } from '@lib/query-string';
 
 import Display, { IProps as DisplayProps } from './display';
 
 export type IProps =
   & {}
   & RouteProps
+  & IProvidedQueryParams
   & WithDataProps;
 
 export class InviteOrganization extends React.Component<IProps> {
@@ -26,20 +30,24 @@ export class InviteOrganization extends React.Component<IProps> {
 
   create = async (payload: OrganizationInviteAttributes) => {
     const { updateStore: update } = this.props;
-    const { name, ownerEmail, expiresAt } = payload;
+    const { name, ownerEmail, expiresAt, url } = payload;
 
     return await update(t => t.addRecord({
       type: TYPE_NAME,
-      attributes: { name, ownerEmail, expiresAt }
-    }));
+      attributes: { name, ownerEmail, expiresAt, url }
+    }), defaultOptions());
   }
 
   render() {
-    return <Display onSubmit={this.submit} />;
+    const { queryParams } = this.props;
+    return <Display
+      onSubmit={this.submit}
+      { ...queryParams } />;
   }
 }
 
 export default compose(
   withRouter,
+  withQueryParams,
   withData({})
 )(InviteOrganization);
