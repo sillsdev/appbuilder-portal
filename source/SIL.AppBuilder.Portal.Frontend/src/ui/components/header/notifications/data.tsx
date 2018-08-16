99d775e031bad8ca711fdd2e602fddd020dd0d95
giancorzo <gian.corzo@gmail.com>
2018-08-15 22:59:40 -0500

    first refactor code

---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/header/notifications/data.tsx
@@ -0,0 +1,142 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+import { withData as withOrbit, WithDataProps } from 'react-orbitjs';
+import { TYPE_NAME as NOTIFICATION, NotificationAttributes } from '@data/models/notification';
+import { query, defaultSourceOptions } from '@data';
+import { withStubbedDevData } from '@data/with-stubbed-dev-data';
+import { uuid } from '@orbit/utils';
+
+// const mapNetworkToProps = (passedProps) => {
+//   return {
+//     notifications: q => q.findRecords(NOTIFICATION)
+//   };
+// };
+
+const mapRecordsToProps = (passedProps) => {
+  return {
+    notifications: q => q.findRecords(NOTIFICATION)
+  }
+}
+
+export interface DataProps {
+  notifications: Array<JSONAPI<NotificationAttributes>>;
+  seenNotifications: Array<JSONAPI<NotificationAttributes>>;
+  showNotifications: Array<JSONAPI<NotificationAttributes>>;
+}
+
+export interface ActionProps {
+  markNotificationsToSeen: () => void;
+  clearAll: () => void;
+  clearOne: (id: string) => void;
+  markNotificationToSeen: (notification: JSONAPI<NotificationAttributes>) => void;
+}
+
+export function withData(WrappedComponent) {
+
+  class DataWrapper extends React.Component<DataProps & WithDataProps> {
+
+    markNotificationsToSeen = async () => {
+
+      const { notifications } = this.props;
+
+      await this.props.updateStore(t =>
+        notifications.map(not => t.replaceAttribute({
+          type: NOTIFICATION, id: not.id
+        }, 'isViewed', true)),
+        { devOnly: true });
+    }
+
+    markNotificationToSeen = async (notification) => {
+      await this.props.updateStore(t => t.replaceAttribute({
+        type: NOTIFICATION, id: notification.id
+      }, 'isViewed', true), { devOnly: true });
+    }
+
+    clearAll = async () => {
+
+      const { notifications } = this.props;
+
+      await this.props.updateStore(t =>
+        notifications.map(not => t.removeRecord({
+          type: NOTIFICATION, id: not.id
+        })),
+        { devOnly: true });
+    }
+
+    clearOne = async (id) => {
+      await this.props.updateStore(t => t.removeRecord(
+        { type: NOTIFICATION, id }
+      ), { devOnly: true });
+    }
+
+    seenNotifications = () => {
+      return this.props.notifications && this.props.notifications.reduce((memo, not) => memo && not.attributes.isViewed, true);
+    }
+
+    showNotifications = () => {
+      return this.props.notifications && this.props.notifications.reduce((memo, not) => memo || not.attributes.show, false);
+    }
+
+    sortNotifications = (notifications) => {
+
+      if (!notifications) {
+        return [];
+      }
+
+      return notifications.sort((a, b) => {
+
+        return (a.attributes.time === b.attributes.time) ? 0 :
+          a.attributes.time > b.attributes.time ? -1 : 1;
+
+      }).sort((a, b) => {
+
+        return (a.attributes.isViewed === b.attributes.isViewed) ? 0 :
+          a.attributes.isViewed ? 1 : -1;
+
+      })
+    }
+
+
+    render() {
+
+      const { notifications, ...otherProps } = this.props;
+
+      const dataProps = {
+        notifications: this.sortNotifications(notifications), // When API endpoint ready we should add query result here
+        seenNotifications: this.seenNotifications(),
+        showNotifications: this.showNotifications(),
+      }
+
+      const actionProps = {
+        markNotificationsToSeen: this.markNotificationsToSeen,
+        clearAll: this.clearAll,
+        clearOne: this.clearOne
+      }
+
+      return <WrappedComponent {...otherProps} {...dataProps} {...actionProps} />;
+
+    }
+
+  }
+
+  return compose(
+    //query(mapNetworkToProps),
+    withStubbedDevData('notification', 1, {
+      title: 'New Task',
+      description: 'Chris Hubbard has requested approval for Sogdian Bible Public Domain.',
+      time: new Date(Date.now() - 15000 * 60),
+      link: '/tasks/1',
+      isViewed: false,
+      show: true
+    }),
+    withStubbedDevData('notification', 2, {
+      title: 'Viewed Task',
+      description: 'Chris Hubbard approved your request.',
+      time: new Date(Date.now() - 80000 * 60),
+      link: '/tasks/2',
+      isViewed: true,
+      show: true
+    }),
+    withOrbit(mapRecordsToProps)
+  )(DataWrapper);
+}
--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/header/notifications/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/header/notifications/index.tsx
@@ -2,29 +2,23 @@ import * as React from 'react';
 import { compose } from 'recompose';
 import { translate, InjectedTranslateProps as i18nProps } from 'react-i18next';
 
-import { withData, WithDataProps } from 'react-orbitjs';
-import { NotificationAttributes, TYPE_NAME } from '@data/models/notification';
+import { withData, DataProps, ActionProps } from './data';
 
-import { uuid } from '@orbit/utils';
-import { Dropdown, Icon } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 
 import './notification.scss';
+
 import { withTimeAgo } from '@lib/with-time-ago';
 
-interface Notification {
-  type: string;
-  id: string;
-  attributes: NotificationAttributes;
-}
 
 export interface Props {
-  notifications: Notification[];
   timeAgo: any;
 }
 
 export type IProps =
   & Props
-  & WithDataProps
+  & DataProps
+  & ActionProps
   & i18nProps;
 
 class Notifications extends React.Component<IProps> {
@@ -33,77 +27,14 @@ class Notifications extends React.Component<IProps> {
     visible: false
   };
 
-  // TODO: Remove this method we are connected to the Backend
-  generateRandomTaks = async () => {
-    await this.props.updateStore(t => [
-      t.addRecord({
-        type: TYPE_NAME,
-        id: uuid(),
-        attributes: {
-          title: 'New Task',
-          description: 'Chris Hubbard has requested approval for Sogdian Bible Public Domain.',
-          time: new Date(Date.now() - 15000 * 60),
-          link: '/tasks/1',
-          isViewed: false,
-          show: true
-        }
-      }),
-      t.addRecord({
-        type: TYPE_NAME,
-        id: uuid(),
-        attributes: {
-          title: 'Viewed Task',
-          description: 'Chris Hubbard approved your request.',
-          time: new Date(Date.now() - 80000 * 60)     ,
-          link: '/tasks/2',
-          isViewed: true,
-          show: true
-        }
-      })
-    ], { devOnly: true });
-  }
-
-  componentDidMount() {
-    this.generateRandomTaks();
-  }
-
-  seenNotifications = () => {
-    return this.props.notifications.reduce((memo,not) => memo && not.attributes.isViewed, true);
-  }
-
-  showNotifications = () => {
-    return this.props.notifications.reduce((memo, not) => memo || not.attributes.show, false);
-  }
-
-  markNotificationToSeen = async () => {
-
-    const { notifications } = this.props;
-
-    await this.props.updateStore(t =>
-      notifications.map(not => t.replaceAttribute({
-        type: TYPE_NAME, id: not.id
-      }, 'isViewed', true)),
-      { devOnly: true });
-  }
-
-  clearAll = async (e) => {
-
-    const { notifications } = this.props;
-
-    await this.props.updateStore(t =>
-      notifications.map(not => t.removeRecord({
-        type: TYPE_NAME, id: not.id
-      })),
-    { devOnly: true });
-  }
-
-  clearOne = async (id) => {
-    await this.props.updateStore(t => t.removeRecord(
-      { type: TYPE_NAME, id }
-    ), { devOnly: true });
-  }
-
   toggle = (e) => {
+
+    const { markNotificationsToSeen } = this.props;
+
+    if (this.state.visible) {
+      markNotificationsToSeen();
+    }
+
     this.setState({
       visible: !this.state.visible
     });
@@ -112,11 +43,11 @@ class Notifications extends React.Component<IProps> {
   render() {
 
     const { t, timeAgo } = this.props;
-    const { notifications } = this.props;
-    const seenNotifications = this.seenNotifications();
+    const { notifications, seenNotifications, showNotifications } = this.props;
+    const { clearAll, clearOne, markNotificationToSeen } = this.props;
 
     return (
-      <div className='ui top right pointing dropdown ' data-test-header-notification>
+      <div className='ui top right pointing dropdown' data-test-header-notification>
         <div
           style={{ zIndex: 1 }}
           className={`full-overlay transition ${this.state.visible ? 'visible invisible' : ''}`}
@@ -125,19 +56,20 @@ class Notifications extends React.Component<IProps> {
         <div
           data-test-notification-trigger
           style={{ position: 'relative' }}
-          onClick={this.toggle}>
+          onClick={this.toggle}
+        >
           {!seenNotifications && <div className='red-dot' />}
           <i className='alarm large circular icon no-shadows' />
         </div>
 
         <div className={`ui menu transition notifications ${this.state.visible ? 'visible' : ''}`}>
           {
-            notifications && notifications.length > 0 && this.showNotifications() ?
+            notifications && notifications.length > 0 && showNotifications ?
             <>
               <div className="notification-buttons">
                 <a href="#" onClick={e => {
                   e.preventDefault();
-                  this.clearAll(e);
+                   clearAll();
                   }}
                 >
                   {t('header.clearAll') }
@@ -145,32 +77,23 @@ class Notifications extends React.Component<IProps> {
               </div>
               <div className={notifications.length > 3 ? 'scrollable-menu' : ''}>
                 {
-                  notifications.sort((a, b) => {
-
-                    return (a.attributes.time === b.attributes.time) ? 0 :
-                      a.attributes.time > b.attributes.time ? -1 : 1;
-
-                  }).sort((a, b) => {
-
-                    return (a.attributes.isViewed === b.attributes.isViewed) ? 0 :
-                      a.attributes.isViewed ? 1 : -1;
-
-                  }).map((notification, index) => {
+                  notifications.map((notification, index) => {
 
                     const { title, description, time, isViewed } = notification.attributes;
+                    const viewState = isViewed ? 'seen' : 'not-seen';
 
                     if (!notification.attributes.show) {
                       return null;
                     }
 
                     return (
-                      <div
-                        className={`notification-item ${isViewed ? 'seen' : 'not-seen'}`}
-                        key={index}
-                      >
+                      <div className={`notification-item ${viewState}`} key={notification.id} onClick={e => {
+                        e.preventDefault();
+                        markNotificationToSeen(notification);
+                      }}>
                         <a className='close' href='#' onClick={e => {
                           e.preventDefault();
-                          this.clearOne(notification.id);
+                          clearOne(notification.id);
                         }}>
                           <Icon name='close' />
                         </a>
@@ -193,14 +116,8 @@ class Notifications extends React.Component<IProps> {
   }
 }
 
-const mapRecordsToProps = (ownProps) => {
-  return {
-    notifications: q => q.findRecords(TYPE_NAME)
-  };
-};
-
 export default compose(
   withTimeAgo,
-  withData(mapRecordsToProps),
+  withData,
   translate('translations')
 )(Notifications);
