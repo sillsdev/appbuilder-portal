efe950bf9547c6d4c9100c3899a5762f7634a599
giancorzo <gian.corzo@gmail.com>
2018-08-17 10:03:21 -0500

    fixing conflicts

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/project/__tests__/acceptance-test.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/project/__tests__/acceptance-test.tsx
@@ -53,31 +53,10 @@ describe('Acceptance | Archive Project', () => {
         }});
       });
 
-<<<<<<< HEAD
-        beforeEach(async function () {
-          this.mockPatch(200, 'projects/1', {
-            data: {
-              type: 'projects',
-              id: '1',
-              attributes: {
-                'name': "Dummy Project",
-                'type': "Sogdian Transalation",
-                'description': "This is a dummy project",
-                'language': "Sogdian",
-                'private': false,
-                'date-created': "2018-08-09T20:23:54.809962",
-                'date-updated': "2018-08-10T23:59:55.7694263Z",
-                'date-archived': "2018-08-10T23:59:55.259Z"
-              },
-              relationships: {}
-            }
-          });
-=======
       describe('the archive button is clicked', () => {
         beforeEach(async () => {
           await visit('/project/1');
           await page.clickArchiveLink();
->>>>>>> master
         });
 
         it("changes the button text", () => {
@@ -106,27 +85,6 @@ describe('Acceptance | Archive Project', () => {
       });
     });
 
-<<<<<<< HEAD
-          beforeEach(async function () {
-            this.mockPatch(200, 'projects/1', {
-              data: {
-                type: 'projects',
-                id: '1',
-                attributes: {
-                  'name': "Dummy Project",
-                  'type': "Sogdian Transalation",
-                  'description': "This is a dummy project",
-                  'language': "Sogdian",
-                  'private': false,
-                  'date-created': "2018-08-09T20:23:54.809962",
-                  'date-updated': "2018-08-10T23:59:55.7694263Z",
-                  'date-archived': null
-                },
-                relationships: {}
-              }
-            });
-          });
-=======
     describe('reactivating a project', () => {
       beforeEach(function () {
         this.mockPatch(200, 'projects/1', {
@@ -145,7 +103,6 @@ describe('Acceptance | Archive Project', () => {
           await visit('/project/1');
           await page.clickArchiveLink();
         });
->>>>>>> master
 
         it("changes the button text", () => {
           expect(page.archiveText).to.equal('Archive');
