58637684f6a9b6a6d425cb213abf6f79ef6bf8cd
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-09-07 17:52:49 -0400

    add some better debuggability

--- source/SIL.AppBuilder.Portal.Frontend/src/lib/debug.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/lib/debug.tsx
@@ -10,6 +10,28 @@ export function withDebugger(InnerComponent) {
   };
 }
 
+export function requireProps(...propsToCheck) {
+  return InnerComponent => props => {
+    let missingProps: string[] = [];
+
+    propsToCheck.forEach(prop => {
+      if (props[prop] === undefined) {
+        missingProps.push(prop);
+      }
+    });
+
+    const componentName = InnerComponent.displayName || 'unnamed';
+
+    assert(
+      missingProps.length === 0, 
+      `This component (${componentName}) is missing the required props: ${missingProps.join()}`,
+      true
+    );
+
+    return <InnerComponent { ...props } />
+  }
+}
+
 export function assert(condition, message, throwError = true) {
   if (!condition) {
     console.error(message);
