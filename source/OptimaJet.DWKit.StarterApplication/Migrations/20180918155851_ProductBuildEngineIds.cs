fb40d0c2b614c98c0eff67c0141a2222ceb8b67e
Chris Hubbard <chris@thehubbards.org>
2018-09-18 12:00:52 -0400

    Fix workflow ids to be ints and add workflow-job-id

---
+++ source/OptimaJet.DWKit.StarterApplication/Migrations/20180918155851_ProductBuildEngineIds.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Optimajet.DWKit.StarterApplication.Migrations
+{
+    public partial class ProductBuildEngineIds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkflowPublishId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkflowBuildId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkflowJobId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkflowJobId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WorkflowPublishId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WorkflowBuildId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
