511349511b4c73ab9f6b42c30d4b114482b299a2
David Moore <david_moore1@sil.org>
2018-09-21 14:19:15 -0400

    Build Engine Monitor
    Add code to check build engine system status.
    Still need to schedule task and tests

---
+++ source/OptimaJet.DWKit.StarterApplication/Models/SystemStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JsonApiDotNetCore.Models;
+using OptimaJet.DWKit.StarterApplication.Models;
+
+namespace Optimajet.DWKit.StarterApplication.Models
+{
+    public class SystemStatus : Identifiable, ITrackDate, IBuildEngineReference
+    {
+        [Attr("build-engine-url")]
+        public string BuildEngineUrl { get; set; }
+        [Attr("build-engine-api-access-token")]
+        public string BuildEngineApiAccessToken { get; set; }
+        [Attr("system-available")]
+        public bool SystemAvailable { get; set; }
+        [Attr("date-created")]
+        public DateTime? DateCreated { get; set; }
+        [Attr("date-updated")]
+        public DateTime? DateUpdated { get; set; }
+    }
+}
---
+++ source/OptimaJet.DWKit.StarterApplication/Services/BuildEngine/BuildEngineSystemMonitor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using JsonApiDotNetCore.Data;
+using Optimajet.DWKit.StarterApplication.Models;
+using OptimaJet.DWKit.StarterApplication.Models;
+using SIL.AppBuilder.BuildEngineApiClient;
+
+namespace OptimaJet.DWKit.StarterApplication.Services.BuildEngine
+{
+    public class BuildEngineSystemMonitor
+    {
+        public IEntityRepository<Organization> OrganizationRepository;
+        public IEntityRepository<SystemStatus> SystemStatusRepository;
+        public BuildEngineSystemMonitor(
+            IEntityRepository<Organization> organizationRepository,
+            IEntityRepository<SystemStatus> systemStatusRepository
+        )
+        {
+            OrganizationRepository = organizationRepository;
+            SystemStatusRepository = systemStatusRepository;
+        }
+        public void CheckBuildEngineStatus()
+        {
+            // Hangfire methods cannot be async, hence the Waits
+            SyncDatabaseTablesAsync().Wait();
+            CheckSystemStatusesAsync().Wait();
+        }
+         private async System.Threading.Tasks.Task SyncDatabaseTablesAsync()
+        {
+            var organizations = OrganizationRepository.Get().AsEnumerable();
+            var statuses = SystemStatusRepository.Get().AsEnumerable();
+            var removeList = statuses.Except(organizations, new BuildEngineReferenceComparer()).ToList();
+            var addList = organizations.Except(statuses, new BuildEngineReferenceComparer()).ToList();
+            foreach (SystemStatus removeEntry in removeList)
+            {
+                await SystemStatusRepository.DeleteAsync(removeEntry.Id);
+            }
+            foreach (Organization addEntry in addList)
+            {
+                var newEntry = new SystemStatus
+                {
+                    BuildEngineApiAccessToken = addEntry.BuildEngineApiAccessToken,
+                    BuildEngineUrl = addEntry.BuildEngineUrl
+                };
+                await SystemStatusRepository.CreateAsync(newEntry);
+            }
+        }
+        private async System.Threading.Tasks.Task CheckSystemStatusesAsync()
+        {
+            var systems = SystemStatusRepository.Get().AsEnumerable();
+            foreach (SystemStatus systemEntry in systems)
+            {
+                var available = CheckConnection(systemEntry);
+                if (available != systemEntry.SystemAvailable)
+                {
+                    systemEntry.SystemAvailable = available;
+                    await SystemStatusRepository.UpdateAsync(systemEntry.Id, systemEntry);
+                }
+            }
+        }
+        private bool CheckConnection(SystemStatus systemEntry)
+        {
+            var client = new BuildEngineApi(systemEntry.BuildEngineUrl, systemEntry.BuildEngineApiAccessToken);
+            var response = client.SystemCheck();
+            if (response == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
