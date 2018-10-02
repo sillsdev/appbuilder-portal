e53d13c6679b93c87120d5d8231208fe2faca2b6
Chris Hubbard <chris@thehubbards.org>
2018-10-02 12:00:46 -0400

    Support deploying to AWS (#163)
    * Support deploying to AWS
    
    * hardcode deployment to staging for now

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 # $1 - current version (e.g. commit sha)
-# $2 - deployment level (e.g. staging/production)
+# $2 - branch
+
 
 REPO_PORTAL_NGINX=appbuilder-portal-nginx
 REPO_PORTAL_API=appbuilder-portal-api
 CURRENT_VERSION=$1
-DEPLOY_LEVEL=$2
+
+DEPLOY_LEVEL=staging
+#case "$2" in
+#  master)  DEPLOY_LEVEL=production ;;
+#  develop) DEPLOY_LEVEL=staging ;;
+#  "")      DEPLOY_LEVEL=unknown ;;
+#  *)       DEPLOY_LEVEL=$2 ;;
+#esac 
+
+ECS_CLUSTER=aps-stg
+#case "$2" in
+#  master)  ECS_CLUSTER=aps-prd ;;
+#  *)       ECS_CLUSTER=aps-stg ;;
+#esac 
+
 
 docker --version # document the version travis is using
+sudo apt-get install jq
+which jq && jq --version
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in path
 (cd $HOME/.local/bin && curl -O https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy && chmod +x ecs-deploy) 
@@ -15,7 +32,7 @@ eval $(aws ecr get-login --no-include-email --region us-east-1) #needs AWS_ACCES
 
 NGINX_BUILD_TAG=nginx-$CURRENT_VERSION
 NGINX_IMAGE_URL=$AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPO_PORTAL_NGINX
-(cd source && docker build . -f Dockerfile.nginx -t $NGINX_BUILD_TAG --target release)
+docker build . -f Dockerfile.nginx -t $NGINX_BUILD_TAG --target release
 docker tag $NGINX_BUILD_TAG $NGINX_IMAGE_URL:$DEPLOY_LEVEL
 docker push $NGINX_IMAGE_URL:$DEPLOY_LEVEL
 docker tag $NGINX_BUILD_TAG $NGINX_IMAGE_URL:$CURRENT_VERSION
@@ -23,9 +40,10 @@ docker push $NGINX_IMAGE_URL:$CURRENT_VERSION
 
 API_BUILD_TAG=api-$CURRENT_VERSION
 API_IMAGE_URL=$AWS_ECR_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPO_PORTAL_API
-(cd source && docker build . -f Dockerfile.backend -t $API_BUILD_TAG --target runtime-release)
+docker build . -f Dockerfile.backend -t $API_BUILD_TAG --target runtime-release
 docker tag $API_BUILD_TAG $API_IMAGE_URL:$DEPLOY_LEVEL
 docker push $API_IMAGE_URL:$DEPLOY_LEVEL
 docker tag $API_BUILD_TAG $API_IMAGE_URL:$CURRENT_VERSION
 docker push $API_IMAGE_URL:$CURRENT_VERSION
 
+ecs-deploy -c $ECS_CLUSTER -n portal -i ignore -to $CURRENT_VERSION --max-definitions 20 --timeout 300
--- source/run-nginx.sh
+++ source/run-nginx.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
+env
+
 if [ "$NGINX_CONF_DIR" = "" ]
 then
 	NGINX_CONF_DIR=/etc/nginx/conf.d
 fi
 envsubst '$$API_URL' < $NGINX_CONF_DIR/default.conf.template > $NGINX_CONF_DIR/default.conf
 
-#nginx -g 'daemon off;'
+echo "Starting NGINX"
+nginx -g 'daemon off;'
