5eeab84727742f0d4a90900b5e62dbad7ee9faed
giancorzo <gian.corzo@gmail.com>
2018-10-10 08:30:51 -0500

    Adding product to productIcon

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/product-icon/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/product-icon/index.tsx
@@ -4,7 +4,7 @@ import WebIcon from '@material-ui/icons/Web';
 import MissingIcon from '@material-ui/icons/ErrorOutline';
 
 import { attributesFor } from '@data';
-import { ProductDefinitionResource } from '@data';
+import { ProductDefinitionResource, ProductResource } from '@data';
 
 const iconMap = {
   android: () => <AndriodIcon />,
@@ -14,7 +14,7 @@ const iconMap = {
 
 // TODO: We need to change this to product definition
 export interface IProps {
-  product: ProductDefinitionResource;
+  product: ProductDefinitionResource | ProductResource;
 }
 
 export default class ProductIcon extends React.Component<IProps> {
