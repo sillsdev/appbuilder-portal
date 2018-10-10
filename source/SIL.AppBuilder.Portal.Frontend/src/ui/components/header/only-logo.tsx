7efd7f5c0b4a03a6bf665410feded600cc076bf6
giancorzo <gian.corzo@gmail.com>
2018-10-10 11:09:26 -0500

    adding css

---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/header/only-logo.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+import './header.scss';
+
+class Header extends React.Component<RouteComponentProps> {
+
+  render() {
+
+    const { history } = this.props;
+
+    return (
+      <div data-test-header-menu className='ui menu menu-navbar'>
+        <div className='ui container'>
+          <div
+            className='item header logo'
+            onClick={() => history.push('/')}>
+            SCRIPTORIA
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default compose(
+  withRouter
+)(Header);
--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/index.tsx
@@ -4,13 +4,15 @@ import { compose } from 'recompose';
 import { requireAuth } from '@lib/auth';
 
 import InviteOrganization from './invite-organization';
+import Header from '@ui/components/header/only-logo';
 
 export const pathName = '/admin';
 
 class AdminRoute extends React.Component {
   render() {
     return (
-      <div>
+      <div className='flex flex-column flex-grow'>
+        <Header/>
         <InviteOrganization />
       </div>
     );
