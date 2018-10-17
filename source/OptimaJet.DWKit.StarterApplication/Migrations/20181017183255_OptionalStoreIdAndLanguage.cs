fbae111f5b613ebb37c74f6a20daea68f3c92097
Chris Hubbard <chris@thehubbards.org>
2018-10-17 16:16:09 -0400

    Add Default Product and Sample Product Data (#213)

---
+++ source/OptimaJet.DWKit.StarterApplication/Migrations/20181017183255_OptionalStoreIdAndLanguage.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OptimaJet.DWKit.StarterApplication.Migrations
+{
+    public partial class OptionalStoreIdAndLanguage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Stores_StoreId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_StoreLanguages_StoreLanguageId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkflowDefinitions_StoreTypes_StoreTypeId",
+                table: "WorkflowDefinitions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreTypeId",
+                table: "WorkflowDefinitions",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreLanguageId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreId",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Stores_StoreId",
+                table: "Products",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_StoreLanguages_StoreLanguageId",
+                table: "Products",
+                column: "StoreLanguageId",
+                principalTable: "StoreLanguages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkflowDefinitions_StoreTypes_StoreTypeId",
+                table: "WorkflowDefinitions",
+                column: "StoreTypeId",
+                principalTable: "StoreTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Stores_StoreId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_StoreLanguages_StoreLanguageId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkflowDefinitions_StoreTypes_StoreTypeId",
+                table: "WorkflowDefinitions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreTypeId",
+                table: "WorkflowDefinitions",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreLanguageId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreId",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Stores_StoreId",
+                table: "Products",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_StoreLanguages_StoreLanguageId",
+                table: "Products",
+                column: "StoreLanguageId",
+                principalTable: "StoreLanguages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkflowDefinitions_StoreTypes_StoreTypeId",
+                table: "WorkflowDefinitions",
+                column: "StoreTypeId",
+                principalTable: "StoreTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
