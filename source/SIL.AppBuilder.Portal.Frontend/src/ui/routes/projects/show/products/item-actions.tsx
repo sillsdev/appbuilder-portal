8737961bc338ec5aa758753b8a9d2ab4db5e281c
giancorzo <gian.corzo@gmail.com>
2018-10-26 12:40:32 -0500

    fix kabob

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/products/item-actions.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/products/item-actions.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { compose } from 'recompose';
 import MoreVerticalIcon from '@material-ui/icons/MoreVert';
 import { Dropdown } from 'semantic-ui-react';
+import { withTranslations, i18nProps } from '@lib/i18n';
 
-class ItemActions extends React.Component {
+class ItemActions extends React.Component<i18nProps> {
 
   render() {
+
+    const { t } = this.props;
+
     return (
       <Dropdown
         pointing='top right'
@@ -15,10 +20,8 @@ class ItemActions extends React.Component {
         className='line-height-0'
       >
         <Dropdown.Menu>
-          <Dropdown.Item text='Update' />
-          <Dropdown.Item text='Publish' />
-          <Dropdown.Item text='Copy Source URL' />
-          <Dropdown.Item text='Download' />
+          <Dropdown.Item text={t('project.products.options.update')} />
+          <Dropdown.Item text={t('project.products.options.publish')} />
         </Dropdown.Menu>
       </Dropdown>
     );
@@ -26,4 +29,6 @@ class ItemActions extends React.Component {
 
 }
 
-export default ItemActions;
+export default compose(
+  withTranslations
+)(ItemActions);
