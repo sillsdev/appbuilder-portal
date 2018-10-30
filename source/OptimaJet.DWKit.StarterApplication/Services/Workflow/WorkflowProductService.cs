0f04eaee0cd8575e049b2a1a0743203a9f4faf3b
Chris Hubbard <chris@thehubbards.org>
2018-10-30 11:01:13 -0400

    Default Workflow 2 (#252)
    * Add UserTask model
    * Create and Delete DWKit Workflow Process when Products are added/removed
    * Implement IsOwner and IsOrgAdmin Rules for assigning permission to commands
    * Update UserTasks when Activity changes on Workflow process
    * Change OptimaJet.Application to allow override of ActionProvider, RuleProvider,
    and TimeManager from the outside
    * Rename android_google_play product definition
    * Remove dwSecurityRole (since we are using our own roles and not DWKit roles)

--- source/OptimaJet.DWKit.StarterApplication/Services/Workflow/WorkflowProductService.cs
+++ source/OptimaJet.DWKit.StarterApplication/Services/Workflow/WorkflowProductService.cs
@@ -1,23 +1,33 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using OptimaJet.DWKit.Application;
 using OptimaJet.DWKit.StarterApplication.Models;
 using OptimaJet.DWKit.StarterApplication.Repositories;
-using System.Threading.Tasks;
-using OptimaJet.DWKit.Application;
 using OptimaJet.Workflow.Core.Runtime;
-using System;
+using Serilog;
 
 namespace OptimaJet.DWKit.StarterApplication.Services.Workflow
 {
     public class WorkflowProductService
     {
         IJobRepository<Product> ProductRepository { get; set; }
+        public IJobRepository<UserTask> TaskRepository { get; }
+        public IJobRepository<User> UserRepository { get; }
+        public WorkflowRuntime Runtime { get; }
 
         public WorkflowProductService(
-            IJobRepository<Product> productRepository
+            IJobRepository<Product> productRepository,
+            IJobRepository<UserTask> taskRepository,
+            IJobRepository<User> userRepository,
+            WorkflowRuntime runtime
         )
         {
             ProductRepository = productRepository;
+            TaskRepository = taskRepository;
+            UserRepository = userRepository;
+            Runtime = runtime;
         }
 
 
@@ -25,6 +35,18 @@ namespace OptimaJet.DWKit.StarterApplication.Services.Workflow
         {
             ManageNewProductAsync(productId).Wait();
         }
+
+        public void ManageDeletedProduct(Guid workflowProcessId)
+        {
+            DeleteWorkflowProcessInstance(workflowProcessId);
+        }
+
+        public void ProductProcessChanged(Guid workflowProcessId, string activityName, string currentState)
+        {
+            ProductProcessChangedAsync(workflowProcessId, activityName, currentState).Wait();
+        }
+
+
 
         public async Task ManageNewProductAsync(int productId)
         {
@@ -55,11 +77,16 @@ namespace OptimaJet.DWKit.StarterApplication.Services.Workflow
             await CreateWorkflowProcessInstance(product);
         }
 
+        protected void DeleteWorkflowProcessInstance(Guid processId)
+        {
+            WorkflowInit.Runtime.DeleteInstance(processId);
+        }
+
         protected async Task CreateWorkflowProcessInstance(Product product)
         {
 
             var processId = Guid.NewGuid();
-            await WorkflowInit.Runtime.CreateInstanceAsync(
+            Task createInstance = WorkflowInit.Runtime.CreateInstanceAsync(
                 new CreateInstanceParams(
                     product.ProductDefinition.Workflow.WorkflowScheme,
                     processId)
@@ -70,6 +97,45 @@ namespace OptimaJet.DWKit.StarterApplication.Services.Workflow
 
             product.WorkflowProcessId = processId;
             await ProductRepository.UpdateAsync(product);
+        }
+
+        public async Task ProductProcessChangedAsync(Guid workflowProcessId, string activityName, string currentState)
+        {
+            // Find the Product assoicated with the ProcessId
+            var product = await ProductRepository.Get().Where(p => p.WorkflowProcessId == workflowProcessId).FirstOrDefaultAsync();
+            if (product == null)
+            {
+                Log.Error($"Could find Product for ProcessId={workflowProcessId}");
+                return;
+            }
+
+            await RemoveTasksByProductId(product.Id);
+
+            // Find all users who could perform the current activity and create tasks for them
+            var workflowUserIds = Runtime.GetAllActorsForDirectCommandTransitions(workflowProcessId, activityName: activityName).ToList();
+            var users = UserRepository.Get().Where(u => workflowUserIds.Contains(u.WorkflowUserId.GetValueOrDefault().ToString())).ToList();
+            foreach (var user in users)
+            {
+                var task = new UserTask
+                {
+                    UserId = user.Id,
+                    ProductId = product.Id,
+                    ActivityName = activityName,
+                    Status = currentState
+                };
+                task = await TaskRepository.CreateAsync(task);
+            }
+
+        }
+
+        private async Task RemoveTasksByProductId(int productId)
+        {
+            // Remove all the current tasks associated with the Product
+            var tasks = TaskRepository.Get().Where(t => t.ProductId == productId);
+            foreach (var task in tasks)
+            {
+                await TaskRepository.DeleteAsync(task.Id);
+            }
         }
     }
 }
