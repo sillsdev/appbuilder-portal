0e6b92a66ee1f3f7d891bdb74aa145bb8cd21034
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-31 11:54:00 -0400

    revert loader changes

--- source/SIL.AppBuilder.Portal.Frontend/src/data/containers/with-loader.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/data/containers/with-loader.tsx
@@ -2,17 +2,13 @@ import * as React from 'react';
 
 import { PageLoader as Loader } from '@ui/components/loaders';
 
-export function withLoader<TWrappedProps>(isLoading: (props: TWrappedProps) => boolean, Wrapper?: any) {
+export function withLoader<TWrappedProps>(isLoading: (props: TWrappedProps) => boolean) {
   return WrappedComponent => {
     class LoadingWrapper extends React.Component<TWrappedProps> {
       render() {
         const isCurrentlyLoading = isLoading(this.props);
 
         if (isCurrentlyLoading) {
-          if (Wrapper) {
-            return <Wrapper><Loader /></Wrapper>;
-          }
-
           return <Loader />;
         }
 
