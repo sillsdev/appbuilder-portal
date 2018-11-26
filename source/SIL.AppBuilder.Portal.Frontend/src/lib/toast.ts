5b88ef4d00e5cef4a23cdba5399fb11cbb872617
giancorzo <gian.corzo@gmail.com>
2018-11-25 21:58:05 -0500

    removing toast queue

--- source/SIL.AppBuilder.Portal.Frontend/src/lib/toast.ts
+++ source/SIL.AppBuilder.Portal.Frontend/src/lib/toast.ts
@@ -2,6 +2,10 @@ import { notify } from 'react-notify-toast';
 
 import { parseError } from '@ui/components/errors/parse-error';
 
+// TODO: do we want queued notifications
+// for displaying multiple?
+// not sure if at same time
+// https://www.npmjs.com/package/react-notify-toast#queued-notifications
 export interface NotifyOptions {
   type?: string;
   timeout?: number; // ms, default 5000ms
@@ -10,16 +14,15 @@ export interface NotifyOptions {
     text: string;
   };
 }
-const notifyQueue = notify.createShowQueue();
 
 export function show(msg: string, options: NotifyOptions) {
   console.debug(msg, options);
-  notifyQueue(msg, options.type, options.timeout, options.color);
+  notify.show(msg, options.type, options.timeout, options.color);
 }
 
 export function custom(msg: string, options: NotifyOptions) {
   console.debug(msg, options);
-  notifyQueue(msg, 'custom', options.timeout, options.color);
+  notify.show(msg, 'custom', options.timeout, options.color);
 }
 
 export function success(msg: string, options: NotifyOptions = {}) {
