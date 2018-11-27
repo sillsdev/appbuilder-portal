9d50318696373e9fae7860be9e9fa24253800089
giancorzo <gian.corzo@gmail.com>
2018-11-26 22:08:43 -0500

    tests

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/__tests__/products-acceptance-test.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/__tests__/products-acceptance-test.tsx
@@ -152,6 +152,113 @@ describe('Acceptance | Project View | Products', () => {
 
   });
 
+  describe('Workflow project URL not present', () => {
+
+    beforeEach(function () {
+      this.mockGet(200, 'users', { data: [] });
+      this.mockGet(200, '/groups', { data: [] });
+      this.mockGet(200, 'projects/1', {
+        data: {
+          type: 'projects',
+          id: 1,
+          attributes: {
+            name: 'Fake project',
+            workflowProjectUrl: null
+          },
+          relationships: {
+            organization: { data: { id: 1, type: 'organizations' } },
+            group: { data: { id: 1, type: 'groups' } },
+            owner: { data: { id: 2, type: 'users' } },
+            reviewers: { data: [] },
+            products: { data: [{ id: 1, type: 'products' }] }
+          }
+        },
+        included: [
+          { type: 'organizations', id: 1, },
+          { type: 'groups', id: 1, attributes: { name: 'Some Group' } },
+          { type: 'users', id: 2, attributes: { familyName: 'last', givenName: 'first' } },
+          {
+            type: 'products', id: 1,
+            attributes: {
+              'date-created': '2018-10-20T16:19:09.878193',
+              'date-updated': '2018-10-20T16:19:09.878193',
+              'date-built': null,
+              'date-published': null
+            },
+            relationships: {
+              'product-definition': { data: { type: 'product-definitions', id: 1 } },
+              project: { data: { type: 'projects', id: 1 } }
+            }
+          }, {
+            type: 'organization-product-definitions', id: 1,
+            attributes: {},
+            relationships: {
+              organization: { data: { type: 'organization', id: 1 } },
+              'product-definition': { data: { type: 'product-definitions', id: 1 } }
+            }
+          }, {
+            type: 'organization-product-definitions', id: 2,
+            attributes: {},
+            relationships: {
+              organization: { data: { type: 'organization', id: 1 } },
+              'product-definition': { data: { type: 'product-definitions', id: 2 } }
+            }
+          }, {
+            type: 'product-definitions', id: 1,
+            attributes: {
+              description: 'Publish Android app to S3',
+              name: 'android_s3'
+            }
+          }, {
+            type: 'product-definitions', id: 2,
+            attributes: {
+              description: 'Publish Android App to Google Play',
+              name: 'android_amazon_app'
+            }
+          }
+        ]
+      });
+      this.mockPost(200, 'products', {
+        data: {
+          id: 1,
+          type: 'products',
+          attributes: {
+            'date-built': null,
+            'date-created': "2018-10-22T17:34:37.3281818Z",
+            'date-published': null,
+            'date-updated': "2018-10-22T17:34:37.3281818Z",
+          },
+          relationships: {
+            project: { data: { id: 1, type: 'projects' } },
+            'product-definition': { data: { id: 2, type: 'product-definitions' } }
+          }
+        }
+      });
+    });
+
+    beforeEach(async function () {
+      await visit('/projects/1');
+    });
+
+    it('navigates to project detail page', () => {
+      expect(location().pathname).to.equal('/projects/1');
+    });
+
+    describe('try to add a product to the list', () => {
+
+      beforeEach(async function () {
+        await page.productsInteractor.clickManageProductButton();
+      });
+
+      it('popup is not visible', () => {
+        expect(
+          page.isProductModalPresent
+        ).to.be.false;
+      });
+    });
+
+  });
+
   describe('Show empty product list', () => {
 
     beforeEach(function() {
