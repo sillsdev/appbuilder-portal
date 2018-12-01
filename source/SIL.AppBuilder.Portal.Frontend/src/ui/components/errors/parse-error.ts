76b28ce4a47856548fbc17217e86fcab7f6c2340
giancorzo <gian.corzo@gmail.com>
2018-12-01 13:20:10 -0500

    extracting function

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/errors/parse-error.ts
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/errors/parse-error.ts
@@ -5,6 +5,13 @@ export interface ParsedError {
   body?: string | string[];
 }
 
+const getFirstJSONAPIError = (error) => {
+  return error.data &&
+    error.data.errors &&
+    error.data.errors.length > 0 &&
+    error.data.errors[0].detail;
+}
+
 export function parseError(error: any): ParsedError {
 
   if (error instanceof RecordNotFoundException) {
@@ -21,16 +28,10 @@ export function parseError(error: any): ParsedError {
     };
   }
 
-  const errorDetail =
-    error.data &&
-    error.data.errors &&
-    error.data.errors.length > 0 &&
-    error.data.errors[0].detail;
+  const jsonApiError = getFirstJSONAPIError(error);
 
-  if(errorDetail) {
-    return {
-      title: errorDetail
-    };
+  if (jsonApiError) {
+    return { title: jsonApiError };
   }
 
   const title = error.message || error;
