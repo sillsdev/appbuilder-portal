37f27fa6c8f9fbc81e3c980773c25bfa1935434d
giancorzo <gian.corzo@gmail.com>
2018-12-05 10:38:57 -0500

    admin layout

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/settings/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/settings/index.tsx
@@ -1,39 +1,23 @@
 import * as React from 'react';
 import { compose } from 'recompose';
 
-import { withTranslations, i18nProps } from '@lib/i18n';
-import { withLayout } from '@ui/components/layout';
-import Navigation from './navigation';
+import { withAdminLayout } from '@ui/components/layout/admin';
+
 import { Switch, Route } from 'react-router-dom';
 import OrganizationsRoute from '@ui/routes/admin/settings/organizations';
 
 export const pathName = '/admin/settings';
 
-type IProps =
-  & i18nProps;
-
-class AdminSettingsRoute extends React.Component<IProps> {
+class AdminSettingsRoute extends React.Component {
   render() {
-    const { t } = this.props;
     return (
-      <div className='ui container'>
-        <h2 className='page-heading page-heading-border-sm'>
-          {t('admin.settings.title')}
-        </h2>
-        <div className='flex-column-xs flex-row-sm align-items-start-sm'>
-          <Navigation/>
-          <div className='m-l-lg flex-grow'>
-            <Switch>
-              <Route path={pathName} component={OrganizationsRoute} />
-            </Switch>
-          </div>
-        </div>
-      </div>
+      <Switch>
+        <Route path={pathName} component={OrganizationsRoute} />
+      </Switch>
     );
   }
 }
 
 export default compose(
-  withLayout,
-  withTranslations,
+  withAdminLayout,
 )(AdminSettingsRoute);
