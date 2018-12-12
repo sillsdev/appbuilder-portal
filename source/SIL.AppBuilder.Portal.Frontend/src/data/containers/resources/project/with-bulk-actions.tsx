1e8ad55fab19d702e4e5438a55ef50bc13136b37
giancorzo <gian.corzo@gmail.com>
2018-12-11 22:47:09 -0500

    wip

--- source/SIL.AppBuilder.Portal.Frontend/src/data/containers/resources/project/with-bulk-actions.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/data/containers/resources/project/with-bulk-actions.tsx
@@ -3,6 +3,11 @@ import { compose } from 'recompose';
 import { withData as withOrbit, WithDataProps } from 'react-orbitjs';
 import { defaultOptions, ProjectResource } from '@data';
 
+import { idFromRecordIdentity } from '@data/store-helpers';
+import { defaultHeaders } from '@lib/fetch';
+
+import { api as apiEnv } from '@env';
+
 import {
   withMomentTimezone,
   IProvidedProps as TimezoneProps
@@ -24,12 +29,42 @@ export function withBulkActions(WrappedComponent) {
     bulkArchive = (projects: ProjectResource[]) => {
       const { dataStore, moment } = this.props;
 
-      return dataStore.update(q =>
-        projects.map(p =>
-          q.replaceAttribute(p, 'dateArchived', moment().format('YYYY-MM-DD HH:mm:ss'))
-        ),
-        defaultOptions()
-      );
+      // return dataStore.update(q =>
+      //   projects.map(p => {
+      //     debugger;
+      //     const id = idFromRecordIdentity(p);
+      //     return q.replaceAttribute({
+      //       type: 'project',
+      //       id
+      //     }, 'dateArchived', moment().format('YYYY-MM-DD HH:mm:ss'));
+      //   }),
+      //   defaultOptions()
+      // );
+
+      const baseUrl = apiEnv.host ? `http://${apiEnv.host}` : '/';
+
+      //Hardcode test
+      const headers = {
+        ...defaultHeaders()
+      };
+      const data = {
+        operations: [{
+          "op": "update",
+          "data": {
+            id: "11",
+            type: "project",
+            attributes: {
+              "date-archived": moment().format('YYYY-MM-DD HH:mm:ss')
+            }
+          }
+        }]
+      }
+
+      fetch(`${baseUrl}/JsonApiOperations/PatchAsync`, {
+        method: 'PATCH',
+        headers: headers,
+        body: JSON.stringify(data)
+      });
     }
 
     bulkReactivate = (projects: ProjectResource[]) => {
