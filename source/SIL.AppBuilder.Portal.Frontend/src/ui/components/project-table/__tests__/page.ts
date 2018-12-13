e08ae909b7bfe1598632447dad827baa0c52c674
giancorzo <gian.corzo@gmail.com>
2018-12-13 13:53:16 -0500

    linting

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/project-table/__tests__/page.ts
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/project-table/__tests__/page.ts
@@ -7,10 +7,6 @@ import {
   Interactor
 } from '@bigtest/interactor';
 
-@interactor class RowInteractor {
-  isRowActionPresent = isPresent('[data-test-row-actions]');
-}
-
 
 class ProjectTable {
   constructor(selector?: string) { }
@@ -24,7 +20,9 @@ class ProjectTable {
   isEmptyTextPresent = isPresent('[data-test-project-list-empty]');
   emptyText = text('[data-test-project-list-empty]');
 
-  rows = collection('[data-test-project-row]', RowInteractor);
+  rows = collection('[data-test-project-row]', {
+    isRowActionPresent: isPresent('[data-test-row-actions]');
+  });
 
   isSortingUp = isPresent('[data-test-up-arrow]');
   isSortingDown = isPresent('[data-test-down-arrow]');
