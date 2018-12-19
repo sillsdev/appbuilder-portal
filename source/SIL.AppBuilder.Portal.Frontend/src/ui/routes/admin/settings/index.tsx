9e5d50c49f3fe102fc821cc2e4ad87df5c3d95d0
giancorzo <gian.corzo@gmail.com>
2018-12-19 16:10:05 -0500

    store types list

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/settings/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/settings/index.tsx
@@ -7,6 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 import OrganizationsRoute from '@ui/routes/admin/settings/organizations';
 import WorkflowDefinitionsRoute from '@ui/routes/admin/settings/workflow-definitions';
 import ProductDefinitionsRoute from '@ui/routes/admin/settings/product-definitions';
+import StoreTypesRoute from '@ui/routes/admin/settings/store-types';
 
 export const pathName = '/admin/settings';
 
@@ -17,6 +18,7 @@ class AdminSettingsRoute extends React.Component {
         <Route path={'/admin/settings/organizations'} component={OrganizationsRoute} />
         <Route path={'/admin/settings/workflow-definitions'} component={WorkflowDefinitionsRoute} />
         <Route path={'/admin/settings/product-definitions'} component={ProductDefinitionsRoute}/>
+        <Route path={'/admin/settings/store-types'} component={StoreTypesRoute} />
       </Switch>
     );
   }
---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/settings/store-types/list/item.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+import CreateIcon from '@material-ui/icons/Create';
+import { Link } from 'react-router-dom';
+import { withData as withOrbit } from 'react-orbitjs';
+
+import {
+  attributesFor,
+  StoreTypeResource,
+  idFromRecordIdentity
+} from '@data';
+
+import { withTranslations, i18nProps } from '@lib/i18n';
+
+
+interface IOwnProps {
+  storeType: StoreTypeResource;
+}
+
+type IProps =
+  & IOwnProps
+  & i18nProps;
+
+class ProductDefinitionItem extends React.Component<IProps> {
+
+  render() {
+
+    const { t, storeType } = this.props;
+
+    const {
+      name, description
+    } = attributesFor(storeType);
+
+    return (
+      <div
+        className='flex p-md fs-13 m-b-sm thin-border round-border-4'
+      >
+        <div className='flex-grow'>
+          <div className='bold fs-16'>{name}</div>
+          <div className='p-t-md'>
+            <span className='bold m-r-sm'>
+              {t('admin.settings.storeTypes.description')}:
+            </span>
+            <span>{description}</span>
+          </div>
+        </div>
+        <div>
+          <Link
+            className='gray-text'
+            to={`/admin/settings/store-types/${idFromRecordIdentity(storeType)}/edit`}
+          >
+            <CreateIcon className='fs-16' />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default compose<IProps, IOwnProps>(
+  withTranslations
+)(ProductDefinitionItem);
