fc028c808794d9e47d86731d9fc2f500f05a0b2a
L. Preston Sego III <LPSego3+dev@gmail.com>
2019-01-08 16:11:38 -0500

    Bugfix/31186 tasks need to be filtered by organization (#394)
    * need tests now
    
    * tests!
    
    * cleanup'

--- source/OptimaJet.DWKit.StarterApplication/Services/UserTaskService.cs
+++ source/OptimaJet.DWKit.StarterApplication/Services/UserTaskService.cs
@@ -8,23 +8,42 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using OptimaJet.DWKit.StarterApplication.Models;
 using OptimaJet.DWKit.StarterApplication.Repositories;
+using static OptimaJet.DWKit.StarterApplication.Utility.ServiceExtensions;
 
 namespace OptimaJet.DWKit.StarterApplication.Services
 {
     public class UserTaskService : EntityResourceService<UserTask>
     {
+        public IOrganizationContext OrganizationContext { get; }
         public IEntityRepository<UserTask> UserTaskRepository { get; }
         public CurrentUserRepository CurrentUserRepository { get; }
+        public IJsonApiContext JsonApiContext { get; }
 
-    public UserTaskService(
+        public UserTaskService(
             IJsonApiContext jsonApiContext,
             IEntityRepository<UserTask> userTaskRepository,
             CurrentUserRepository currentUserRepository,
+            IOrganizationContext organizationContext,
             ILoggerFactory loggerFactory) 
             : base(jsonApiContext, userTaskRepository, loggerFactory)
         {
+            this.OrganizationContext = organizationContext;
             this.UserTaskRepository = userTaskRepository;
             this.CurrentUserRepository = currentUserRepository;
+            this.JsonApiContext = jsonApiContext;
+        }
+
+        public override async Task<IEnumerable<UserTask>> GetAsync()
+        {
+            if (this.OrganizationContext.IsOrganizationHeaderPresent) 
+            {
+                return await GetScopedToOrganization<UserTask>(
+                    base.GetAsync,
+                    this.OrganizationContext,
+                    JsonApiContext);
+            }
+
+            return await base.GetAsync();
         }
     }
 }
---
+++ source/SIL.AppBuilder.Portal.Frontend/src/data/models/user-task.ts
@@ -0,0 +1,15 @@
+import { AttributesObject, ResourceObject } from "jsonapi-typescript";
+
+export type USER_TASK_TYPE = 'userTask';
+export const TYPE_NAME = 'user-task';
+
+export interface UserTaskAttributes extends AttributesObject {
+  activityName: string;
+  comment: string;
+  status: string;
+  dateCreated: string;
+  dateUpdated: string;
+  waitTime: number;
+}
+
+export type UserTaskResource = ResourceObject<USER_TASK_TYPE, UserTaskAttributes>;
