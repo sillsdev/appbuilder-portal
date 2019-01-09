d68f9b553e87cc529580f4a76a40d4085912c037
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-01-09 14:22:27 -0500

    allow hitting enter on the form

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/users/list/add/user-input.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/users/list/add/user-input.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {compose} from 'recompose';
+import { compose } from 'recompose';
 
 import { IAttributeProps } from '@lib/dom';
 import { i18nProps, withTranslations } from '@lib/i18n';
@@ -12,37 +12,41 @@ interface IState {
 }
 
 type IProps = IOwnProps & IAttributeProps & i18nProps;
-class UserInput extends React.Component<IProps, IState>{
-  state = {email: ''};
+class UserInput extends React.Component<IProps, IState> {
+  state = { email: '' };
 
   didType = (e: React.ChangeEvent<HTMLInputElement>) => {
     const email = e.target.value;
     this.setState({ email });
   }
-  onSubmit = (e: React.MouseEvent<HTMLInputElement>) => {
-    const {onSubmit, t} = this.props;
-    const {email} = this.state;
+
+  onSubmit = (e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { onSubmit, t } = this.props;
+    const { email } = this.state;
     onSubmit(email);
   }
 
   public render() {
-    const {t} = this.props;
-    const {email} = this.state;
-    return (<div className="flex justify-content-space-between">
-      <div className="large ui input">
-        <input data-test-email
-          type='text'
-          placeholder={t("users.addUser.placeholder")}
-          value={email}
-          onChange={this.didType} />
-      </div>
-      <button
-        data-test-add
-        className='large ui button'
-        onClick={this.onSubmit} >
-        {t("users.addUser.modalAddButton")}
-      </button>
-    </div>);
+    const { t } = this.props;
+    const { email } = this.state;
+    return (
+      <form onSubmit={this.onSubmit} className="flex justify-content-space-between">
+        <div className="large ui input">
+          <input
+            data-test-email
+            type="text"
+            placeholder={t('users.addUser.placeholder')}
+            value={email}
+            onChange={this.didType}
+          />
+        </div>
+        <button data-test-add type="submit" className="large ui button" onClick={this.onSubmit}>
+          {t('users.addUser.modalAddButton')}
+        </button>
+      </form>
+    );
   }
 }
 
