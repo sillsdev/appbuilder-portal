e2d522b4efe4c527d60dbc6320d574c90ba6c436
L. Preston Sego III <LPSego3+dev@gmail.com>
2019-01-21 16:05:49 -0500

    Add status message to deploy script

--- scripts/deploy-to-ecs.sh
+++ scripts/deploy-to-ecs.sh
@@ -2,6 +2,8 @@ set -a
 
 ./scripts/build-setup.sh $1 $2
 
+echo "Attempting to deploy $CURRENT_VERSION to Cluster: '$ECS_CLUSTER'..."
+
 if [ -n "$ECS_CLUSTER" ]; then
   ecs-deploy -c $ECS_CLUSTER -n portal -i ignore -to $CURRENT_VERSION --max-definitions 20 --timeout 300
 fi
