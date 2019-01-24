698b87ce9108a169db858120b6ef45767623ffb1
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-01-24 09:18:56 -0500

    blocked by language display rules, need from SIL, they want to re-use something from another project

---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/inputs/locale-input/index.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Autosuggest from 'react-autosuggest';
+
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages
+    .map(section => {
+      return {
+        title: section.title,
+        languages: section.languages.filter(language => regex.test(language.name))
+      };
+    })
+    .filter(section => section.languages.length > 0);
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+function renderSectionTitle(section) {
+  return (
+    <strong>{section.title}</strong>
+  );
+}
+
+function getSectionSuggestions(section) {
+  return section.languages;
+}
+
+interface IProps {}
+interface IState {
+  value: string;
+  suggestions: string[];
+}
+
+
+
+export default class extends React.Component<IProps, IState> {
+  state = {
+    value: '',
+    suggestions: []
+  };    
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest 
+        multiSection={false}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        renderSectionTitle={renderSectionTitle}
+        getSectionSuggestions={getSectionSuggestions}
+        inputProps={inputProps} />
+    );
+  }
+}
