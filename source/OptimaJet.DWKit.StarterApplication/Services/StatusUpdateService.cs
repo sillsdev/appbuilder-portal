489eb869226edd42ab3a7b7bfd8f637c5da0cb63
Chris Hubbard <chris@thehubbards.org>
2019-01-31 12:50:14 -0500

    Use Hangfire to restart update service on lost connction

--- source/OptimaJet.DWKit.StarterApplication/Services/StatusUpdateService.cs
+++ source/OptimaJet.DWKit.StarterApplication/Services/StatusUpdateService.cs
@@ -11,11 +11,13 @@ using Serilog;
 
 namespace OptimaJet.DWKit.StarterApplication.Services
 {
-    public class StatusUpdateService : BackgroundService
+    public class StatusUpdateService
     {
         public IBackgroundJobClient HangfireClient { get; }
         public IHubContext<ScriptoriaHub> HubContext { get; }
         public String ConnectionString { get; set; }
+        protected IJobCancellationToken CancellationToken { get; set; }
+        protected NpgsqlConnection Connection { get; set; }
         public StatusUpdateService(
             IBackgroundJobClient hangfireClient,
             IHubContext<ScriptoriaHub> hubContext
@@ -26,43 +28,69 @@ namespace OptimaJet.DWKit.StarterApplication.Services
         }
         public async Task SendStatusUpdateAsync(string message)
         {
+            Log.Information($"StatusUpdateService::SendStatusUpdate: {message}");
             dynamic messageObj = JsonConvert.DeserializeObject(message);
             var tableName = messageObj.table.Value as string;
             var id = messageObj.id.Value as string;
             var groupName = "/" + tableName + "/" + id;
             await HubContext.Clients.Group(groupName).SendAsync("StatusUpdate", message);
         }
-        public void ListenForNotifications(CancellationToken stoppingToken)
+        public async Task ListenForNotifications()
         {
-            Log.Information($"ListenForNotifications: Start");
+            Log.Information($"StatusUpdateService::ListenForNotifications: Start");
 
-            NpgsqlConnection conn = new NpgsqlConnection(ConnectionString);
-            conn.Open();
-            var listenCommand = conn.CreateCommand();
-            listenCommand.CommandText = $"listen db_notifications;";
-            listenCommand.ExecuteNonQuery();
-
-            conn.Notification += PostgresNotificationReceived;
+            Connection.Notification += PostgresNotificationReceived;
+            Connection.StateChange += PostgresStateChange;
             while (true)
             {
                 // wait until an asynchronous PostgreSQL notification arrives...
-                conn.Wait(5000);
-                if (stoppingToken.IsCancellationRequested)
+                CancellationToken.ThrowIfCancellationRequested();
+                await Connection.WaitAsync(CancellationToken.ShutdownToken);
+
+                if (Connection.FullState == System.Data.ConnectionState.Closed)
                 {
                     break;
                 }
             }
-            Log.Information($"ListenForNotifications: End");
+            Log.Information($"StatusUpdateService::ListenForNotifications: End");
         }
+
+        private NpgsqlConnection Connect()
+        {
+            // For log running connections, use keepalive
+            // https://www.npgsql.org/doc/wait.html
+            // https://www.npgsql.org/doc/keepalive.html
+            NpgsqlConnection conn = new NpgsqlConnection(ConnectionString + ";Keepalive=5");
+            conn.Open();
+            var listenCommand = conn.CreateCommand();
+            listenCommand.CommandText = $"listen db_notifications;";
+            listenCommand.ExecuteNonQuery();
+            return conn;
+        }
+
+        void PostgresStateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            Log.Information($"StatusUpdateService::StateChange state={e.CurrentState.ToString()}");
+            if (e.CurrentState == System.Data.ConnectionState.Closed)
+            {
+                Log.Warning($"DB Connection Closed: Retry Job!");
+                HangfireClient.Enqueue<StatusUpdateService>(s => s.ExecuteAsync(ConnectionString, CancellationToken));
+            }
+        }
+
         private void PostgresNotificationReceived(object sender, NpgsqlNotificationEventArgs e)
         {
             string message = e.AdditionalInformation.ToString();
             SendStatusUpdateAsync(message).Wait();
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected async Task ExecuteAsync(String connectionString, IJobCancellationToken cancellationToken)
         {
-            await Task.Run(() => ListenForNotifications(stoppingToken));
+            this.CancellationToken = cancellationToken ?? throw new ArgumentNullException(nameof(cancellationToken));
+            this.ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            this.Connection = Connect();
+
+            await Task.Run(() => ListenForNotifications());
         }
     }
 }
