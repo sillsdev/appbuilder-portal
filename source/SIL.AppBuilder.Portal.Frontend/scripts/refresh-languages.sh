b5048d240ef98622d0928c2ef442b4c2380625d9
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-02-07 17:43:18 -0500

    start working on the ldml stuff

--- source/SIL.AppBuilder.Portal.Frontend/scripts/refresh-languages.sh
+++ source/SIL.AppBuilder.Portal.Frontend/scripts/refresh-languages.sh
@@ -10,4 +10,26 @@ targetPath="${folder}alltags.json"
 
 mkdir -p $folder
 
+echo "downloading all supported languages..."
 wget --output-document $targetPath $url
+echo "downloaded all supported languages"
+
+echo "downloading transaltion support for the language data..."
+npm install -g fast-xml-parser
+
+function downloadAndConvert {
+  local lang=$1
+  local fileName=${2:-$1}
+
+
+  wget -qO- "https://ldml.api.sil.org/$lang?inc[0]=localeDisplayNames" | xml2js -o "$folder/ldml.tmp.$fileName.json"
+
+  # TODO: use a custom node script to convert the language 
+  # list to something more easily consumeable by a javascript app.
+}
+
+downloadAndConvert "es-419"
+downloadAndConvert "en" "en-us"
+downloadAndConvert "fr-FR"
+
+ls -la $folder
