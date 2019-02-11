ff652a4c6ef96a057c8d617a7a402167eee4bf63
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-02-11 12:12:07 -0500

    cleanup

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/inputs/locale-input/field.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/inputs/locale-input/field.tsx
@@ -121,7 +121,7 @@ const FieldDisplay = withTranslations(Field);
 const LocaleInputField = memo(
   (props: IProps) => {
     const { t, i18n } = useLdml();
-    const data = useMemo(() => localizeTagData(props.data, t), [null, i18n.language]);
+    const data = useMemo(() => localizeTagData(props.data, t), [i18n.language]);
 
     return <FieldDisplay {...{ ...props, data }} />;
   },
