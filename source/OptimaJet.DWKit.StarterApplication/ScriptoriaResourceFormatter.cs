e362e8632924cbdb07f5a7e04df19df3a76683f5
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-04-01 15:18:11 -0400

    remove excessive commentS

--- source/OptimaJet.DWKit.StarterApplication/ScriptoriaResourceFormatter.cs
+++ source/OptimaJet.DWKit.StarterApplication/ScriptoriaResourceFormatter.cs
@@ -14,32 +14,19 @@ namespace OptimaJet.DWKit.StarterApplication
 
     public string FormatPropertyName(PropertyInfo property) => str.Dasherize(property.Name);
 
-
-    /// <summary>
-    /// Uses the internal type name to determine the external resource name.
-    /// By default we us Humanizer for pluralization and then we dasherize the name.
-    /// </summary>
-    /// <example>
-    /// <code>
-    /// _default.FormatResourceName(typeof(TodoItem)).Dump(); 
-    /// // > "todo-items"
-    /// </code>
-    /// </example>
     public string FormatResourceName(Type type)
     {
-        try
-        {
-            // check the class definition first
-            // [Resource("models"] public class Model : Identifiable { /* ... */ }
-            if (type.GetCustomAttribute(typeof(ResourceAttribute)) is ResourceAttribute attribute)
-                return attribute.ResourceName;
+      try
+      {
+        if (type.GetCustomAttribute(typeof(ResourceAttribute)) is ResourceAttribute attribute)
+          return attribute.ResourceName;
 
-            return ApplyCasingConvention(type.Name.Singularize()); // removed pluralize -- now whatever the class name is will be the type
-        }
-        catch (InvalidOperationException e)
-        {
-            throw new InvalidOperationException($"Cannot define multiple {nameof(ResourceAttribute)}s on type '{type}'.", e);
-        }
+        return ApplyCasingConvention(type.Name.Singularize()); // removed pluralize -- now whatever the class name is will be the type
+      }
+      catch (InvalidOperationException e)
+      {
+        throw new InvalidOperationException($"Cannot define multiple {nameof(ResourceAttribute)}s on type '{type}'.", e);
+      }
     }
   }
 }
