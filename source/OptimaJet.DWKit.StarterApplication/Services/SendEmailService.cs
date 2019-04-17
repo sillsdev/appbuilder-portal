85d544e8c9ac6df0494741971854e3ddfe5459ad
David Moore <david_moore1@sil.org>
2019-04-17 15:03:12 -0400

    Send rejection email to org admins and user when Reverse transition occurs (#618)

--- source/OptimaJet.DWKit.StarterApplication/Services/SendEmailService.cs
+++ source/OptimaJet.DWKit.StarterApplication/Services/SendEmailService.cs
@@ -9,23 +9,27 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using OptimaJet.DWKit.StarterApplication.Models;
 using OptimaJet.DWKit.StarterApplication.Repositories;
+using OptimaJet.DWKit.StarterApplication.Services.Workflow;
 
 namespace OptimaJet.DWKit.StarterApplication.Services
 {
     public class SendEmailService
     {
         public ITranslator Translator { get; }
+        public IJobRepository<UserRole> UserRolesRepository { get; }
         public IJobRepository<Product, Guid> ProductRepository { get; }
         public IJobRepository<ProductBuild> ProductBuildRepository { get; }
         public IJobRepository<Email> EmailRepository { get; }
         public SendEmailService(
             ITranslator translator,
+            IJobRepository<UserRole> userRolesRepository,
             IJobRepository<Product, Guid> productRepository,
             IJobRepository<ProductBuild> productBuildRepository,
             IJobRepository<Email> emailRepository
         )
         {
             Translator = translator;
+            UserRolesRepository = userRolesRepository;
             ProductRepository = productRepository;
             ProductBuildRepository = productBuildRepository;
             EmailRepository = emailRepository;
@@ -34,32 +38,23 @@ namespace OptimaJet.DWKit.StarterApplication.Services
         {
             var template = "Notification.txt";
             var buildEngineUrlText = "";
-            var locale = notification.User.LocaleOrDefault();
-            var fullBodyId = "notifications.body." + notification.MessageId;
-            var fullSubjectId = "notifications.subject." + notification.MessageId;
             var subsDict = notification.MessageSubstitutions as Dictionary<string, object>;
-            var subject = await Translator.TranslateAsync(locale, "notifications", fullSubjectId, subsDict);
-            subject = HttpUtility.HtmlDecode(subject);
-            var body = await Translator.TranslateAsync(locale, "notifications", fullBodyId, subsDict);
+            var locale = notification.User.LocaleOrDefault();
             if (!string.IsNullOrEmpty(notification.LinkUrl))
             {
                 template = "NotificationWithLink.txt";
                 var buildEngineUrlIndex = "notifications.body.buildEngineUrl";
                 buildEngineUrlText = await Translator.TranslateAsync(locale, "notifications", buildEngineUrlIndex, subsDict);
             }
-            var email = new Email
-            {
-                To = notification.User.Email,
-                Subject = subject,
-                ContentTemplate = template,
-                ContentModel = new
-                {
-                    Message = body,
-                    BuildEngineUrlText = buildEngineUrlText,
-                    LinkUrl = notification.LinkUrl
-                }
-            };
-            var result = await EmailRepository.CreateAsync(email);
+            dynamic contentModel = new System.Dynamic.ExpandoObject();
+            contentModel.BuildEngineUrlText = buildEngineUrlText;
+            contentModel.LinkUrl = notification.LinkUrl;
+            await SendEmailAsync(notification.User.Email,
+                                 locale,
+                                 notification.MessageId,
+                                 subsDict,
+                                 contentModel,
+                                 template);
         }
         public void SendProductReviewEmail(Guid productId, Dictionary<string, object> parmsDictionary)
         {
@@ -81,10 +76,10 @@ namespace OptimaJet.DWKit.StarterApplication.Services
             {
                 // Create a list of names/urls for all artifacts for this build
                 var links = BuildLinks(parmsDict, lastBuildRecord);
-                await SendEmailsAsync(product, links);
+                await SendEmailToReviewersAsync(product, links);
             }
         }
-        protected async Task SendEmailsAsync(Product product, string links)
+        protected async Task SendEmailToReviewersAsync(Product product, string links)
         {
             var subsDictionary = new Dictionary<string, object>
                     {
@@ -96,23 +91,87 @@ namespace OptimaJet.DWKit.StarterApplication.Services
             {
                 foreach (var reviewer in reviewers)
                 {
-                    var locale = reviewer.LocaleOrDefault();
-                    var subject = await Translator.TranslateAsync(locale, "notifications", "notifications.subject.reviewProduct", subsDictionary);
-                    var body = await Translator.TranslateAsync(locale, "notifications", "notifications.body.reviewProduct", subsDictionary);
-                    var email = new Email
-                    {
-                        To = reviewer.Email,
-                        Subject = subject,
-                        ContentTemplate = "ReviewProduct.txt",
-                        ContentModel = new
-                        {
-                            Message = body,
-                            Links = links
-                        }
-                    };
-                    var result = await EmailRepository.CreateAsync(email);
+                    dynamic contentModel = new System.Dynamic.ExpandoObject();
+                    contentModel.Links = links;
+                    await SendEmailAsync(reviewer.Email,
+                                         reviewer.LocaleOrDefault(),
+                                         "reviewProduct",
+                                         subsDictionary,
+                                         contentModel,
+                                         "ReviewProduct.txt");
                 }
             }
+        }
+        public void SendRejectEmail(Guid productId, WorkflowProductService.ProductActivityChangedArgs args, String comment)
+        {
+            SendRejectEmailAsync(productId, args, comment).Wait();
+        }
+        protected async Task SendRejectEmailAsync(Guid productId, WorkflowProductService.ProductActivityChangedArgs args, String comment)
+        {
+            var product = await ProductRepository.Get()
+                  .Where(p => p.Id == productId)
+                  .Include(p => p.Project)
+                        .ThenInclude(pr => pr.Type)
+                  .Include(p => p.Project)
+                        .ThenInclude(pr => pr.Organization)
+                  .Include(p => p.Project)
+                        .ThenInclude(pr => pr.Owner)
+                  .Include(p => p.Store)
+                  .Include(p => p.ProductDefinition)
+                  .FirstOrDefaultAsync();
+            if (product != null)
+            {
+                var messageParms = new Dictionary<string, object>()
+                    {
+                        { "projectName", product.Project.Name },
+                        { "productName", product.ProductDefinition.Name},
+                        { "previousState", args.PreviousState},
+                        { "newState", args.CurrentState},
+                        { "comment", comment}
+                    };
+                var sentEmailToOwner = false;
+                var orgAdmins = UserRolesRepository.Get()
+                    .Include(ur => ur.User)
+                    .Include(ur => ur.Role)
+                    .Where(ur => ur.OrganizationId == product.Project.Organization.Id && ur.Role.RoleName == RoleName.OrganizationAdmin)
+                    .ToList();
+                foreach (UserRole orgAdmin in orgAdmins)
+                {
+                    if (orgAdmin.UserId == product.Project.OwnerId)
+                    {
+                        sentEmailToOwner = true;
+                    }
+                    await SendRejectEmailToUserAsync(orgAdmin.User, messageParms);
+                }
+                if (!sentEmailToOwner)
+                {
+                    await SendRejectEmailToUserAsync(product.Project.Owner, messageParms);
+                }
+            }
+        }
+        protected async Task SendRejectEmailToUserAsync(User user, Dictionary<string, object> subsDictionary)
+        {
+            var locale = user.LocaleOrDefault();
+            dynamic contentModel = new System.Dynamic.ExpandoObject();
+            await SendEmailAsync(user.Email, locale, "rejectionEmail", subsDictionary, contentModel, "Notification.txt");
+        }
+        protected async Task SendEmailAsync(string emailAddress, string locale, string messageId, Dictionary<string, object> subsDictionary, dynamic contentModel, string template)
+        {
+            var fullBodyId = "notifications.body." + messageId;
+            var fullSubjectId = "notifications.subject." + messageId;
+            var subject = await Translator.TranslateAsync(locale, "notifications", fullSubjectId, subsDictionary);
+            subject = HttpUtility.HtmlDecode(subject);
+            var body = await Translator.TranslateAsync(locale, "notifications", fullBodyId, subsDictionary);
+            contentModel.Message = body;
+
+            var email = new Email
+            {
+                To = emailAddress,
+                Subject = subject,
+                ContentTemplate = template,
+                ContentModel = contentModel
+            };
+            var result = await EmailRepository.CreateAsync(email);
 
         }
         protected string BuildLinks(Dictionary<string, object> paramsDictionary, ProductBuild build)
