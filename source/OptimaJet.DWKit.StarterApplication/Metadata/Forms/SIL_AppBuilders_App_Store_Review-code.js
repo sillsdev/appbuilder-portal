d86157e33c92f7bdbf876919a42142accbec077a
Chris Hubbard <chris@thehubbards.org>
2019-05-02 14:22:15 -0400

    Fix Workflow issues: update with grid & artifact filters
    * When we added updating comments to forms with ProductArtifacts grid,
      we started getting errors about property on ProductArtifacts. Finally
      figured out that the key on the grid needs to match the properties on
      the models.
    * This also fixed the artifact filters

--- source/OptimaJet.DWKit.StarterApplication/Metadata/Forms/SIL_AppBuilders_App_Store_Review-code.js
+++ source/OptimaJet.DWKit.StarterApplication/Metadata/Forms/SIL_AppBuilders_App_Store_Review-code.js
@@ -1,7 +1,7 @@
 {
     openArtifactLink: function({controlRef, parameters}){
         let selectedRow = controlRef.state.items[parameters.rowIdx];
-        var link = selectedRow.link;
+        var link = selectedRow.Url;
         var win = window.open(link, '_blank');
         win.focus();
     }
