ae4674ca46c44080905a2663557f8c0202dab2d2
David Moore <david_moore1@sil.org>
2019-05-21 16:24:10 -0400

    Restructure module to functional model Changed the withQueryParams to extract useQueryParams method

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/invite-organization/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/admin/invite-organization/index.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
-import { withRouter, RouteProps } from 'react-router';
-import { withData, WithDataProps } from 'react-orbitjs';
-import { compose } from 'recompose';
+import { useOrbit } from 'react-orbitjs';
 import * as toast from '@lib/toast';
 
 import { defaultOptions } from '@data';
 
 import { OrganizationInviteAttributes, TYPE_NAME } from '@data/models/organization-invite';
-import { withQueryParams, IProvidedQueryParams } from '@lib/query-string';
-import Header from '@ui/components/header/only-logo';
-import { withTranslations, i18nProps } from '@lib/i18n';
+import { useQueryParams } from '@lib/query-string';
+import { useTranslations } from '@lib/i18n';
 
 import Display from './display';
 
-export type IProps = {} & RouteProps & IProvidedQueryParams & WithDataProps & i18nProps;
-
-export class InviteOrganization extends React.Component<IProps> {
-  submit = async (payload: OrganizationInviteAttributes) => {
+export default function InviteOrganization() {
+  const { t } = useTranslations();
+  const { queryParams, updateQueryParams } = useQueryParams();
+  const { dataStore } = useOrbit();
+  const submit = async (payload: OrganizationInviteAttributes) => {
     try {
-      await this.create(payload);
+      await create(payload);
 
       toast.success(`Invitation sent`);
     } catch (e) {
@@ -26,11 +24,10 @@ export class InviteOrganization extends React.Component<IProps> {
     }
   };
 
-  create = async (payload: OrganizationInviteAttributes) => {
-    const { updateStore: update } = this.props;
+  const create = async (payload: OrganizationInviteAttributes) => {
     const { name, ownerEmail, expiresAt, url } = payload;
 
-    return await update(
+    return await dataStore.update(
       (t) =>
         t.addRecord({
           type: TYPE_NAME,
@@ -40,26 +37,15 @@ export class InviteOrganization extends React.Component<IProps> {
     );
   };
 
-  render() {
-    const { queryParams, t } = this.props;
-
-    return (
-      <div className='flex flex-column flex-grow'>
-        <div className='ui container p-t-lg'>
-          <h1 className='title page-heading-border p-b-md m-b-lg'>{t('newOrganization.title')}</h1>
-          <div>
-            <h3 className='m-b-lg'>{t('common.general')}</h3>
-            <Display onSubmit={this.submit} {...queryParams} />
-          </div>
+  return (
+    <div className='flex flex-column flex-grow'>
+      <div className='ui container p-t-lg'>
+        <h1 className='title page-heading-border p-b-md m-b-lg'>{t('newOrganization.title')}</h1>
+        <div>
+          <h3 className='m-b-lg'>{t('common.general')}</h3>
+          <Display onSubmit={submit} {...queryParams} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default compose(
-  withRouter,
-  withQueryParams,
-  withData({}),
-  withTranslations
-)(InviteOrganization);
