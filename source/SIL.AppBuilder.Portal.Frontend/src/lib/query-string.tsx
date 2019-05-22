7adba9870b2b11d516571ee1a563714e5e544e96
David Moore <david_moore1@sil.org>
2019-05-22 11:44:31 -0400

    Remove extra header on invite and restructure  (#650)
    * Two headers displayed for organization invites
    
    * Restructure module to functional model
    Changed the withQueryParams to extract useQueryParams method

--- source/SIL.AppBuilder.Portal.Frontend/src/lib/query-string.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/lib/query-string.tsx
@@ -3,39 +3,42 @@ import * as qs from 'querystring';
 import * as React from 'react';
 import { assert } from '@lib/debug';
 
+import { useRouter } from '~/lib/hooks';
+
 export interface IProvidedQueryParams<TQueryParams = {}> {
   queryParams: TQueryParams;
   updateQueryParams(newParams: object): void;
 }
+export function useQueryParams() {
+  const { location, history } = useRouter();
+  // search includes the '?' ...
+  let search = location.search;
 
-export function withQueryParams(InnerComponent) {
-  return (props) => {
-    const { location, history } = props;
+  if (search[0] === '?') {
+    search = search.substr(1);
+  }
 
-    assert(location, `location could not be found in props. Did you include withRouter?`);
+  const params = qs.parse(search);
 
-    // search includes the '?' ...
-    let search = location.search;
-
-    if (search[0] === '?') {
-      search = search.substr(1);
-    }
-
-    const params = qs.parse(search);
-
-    const updateQueryParams = (changedQPs: object) => {
-      const newQueryParams = {
-        ...params,
-        ...changedQPs,
-      };
-
-      history.push({
-        pathname: location.pathname,
-        search: qs.stringify(newQueryParams),
-      });
+  const updateQueryParams = (changedQPs: object) => {
+    const newQueryParams = {
+      ...params,
+      ...changedQPs,
     };
 
-    return <InnerComponent {...props} queryParams={params} updateQueryParams={updateQueryParams} />;
+    history.push({
+      pathname: location.pathname,
+      search: qs.stringify(newQueryParams),
+    });
+  };
+  return { queryParams: params, updateQueryParams };
+}
+export function withQueryParams(InnerComponent) {
+  return (props) => {
+    const { queryParams, updateQueryParams } = useQueryParams();
+    return (
+      <InnerComponent {...props} queryParams={queryParams} updateQueryParams={updateQueryParams} />
+    );
   };
 }
 
