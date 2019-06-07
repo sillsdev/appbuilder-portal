9bfd737dda1a5c8403d7f04aedb5ffad65990f8c
Chris Hubbard <chris@thehubbards.org>
2019-06-07 16:20:44 -0400

    Change the default sample data (#660)
    * Change the default email notification to off for sample data
    
    * Remove DT users and simplify user roles

--- scripts/DB/sample_data.sql
+++ scripts/DB/sample_data.sql
@@ -1,19 +1,11 @@
 INSERT INTO "Users" (
-  "Name",                   "Email",                     "ExternalId",                          "FamilyName",   "GivenName", "IsLocked", "Locale", "Phone", "Timezone") VALUES
-(	'Chris Hubbard',	        'chris_hubbard@sil.org',	   'google-oauth2|116747902156680384840',	'Hubbard',	    'Chris',	   '0',	NULL,	NULL,	NULL),
-(	'David Moore',	          'david_moore1@sil.org',	     'google-oauth2|114981819181509824425',	'Moore',	      'David',	   '0',	NULL,	NULL,	NULL),
-(	'Preston Sego (gmail)',	  'lt.sego@gmail.com',	       'google-oauth2|106685378104908547147',	'Sego (gmail)',	'Preston',	 '0',	NULL,	NULL,	NULL),
-(	'Giancarlo Corzo',	      'gian.corzo@gmail.com',	     'google-oauth2|105409413471872324118',	'Corzo',	      'Giancarlo', '0',	NULL,	NULL,	NULL),
-(	'Liz Tabor (dt)',         'ltabor@developertown.com',	 'google-oauth2|113420589826662049605',	'Tabor',	      'Liz',	     '0',	NULL,	NULL,	NULL),
-(	'Chris Hubbard (Kalaam)',	'chris.kalaam@gmail.com',	   'auth0|5b578f6197af652b19f9bb41',	    'Hubbard',	    'Chris',	   '0',	NULL,	NULL,	NULL),
-( 'Preston Sego (dt)',      'psego@developertown.com',   'google-oauth2|111802484969448690930', 'Sego (dt)',    'Preston',   '0', NULL, NULL, NULL),
-(	'Bill Dyck',              'bill_dyck@sil.org',	       'google-oauth2|102643649500459434996', 'Dyck',	        'Bill',	     '0',	NULL, NULL, NULL),
-(	'Loren Hawthorne',        'loren_hawthrone@sil.org',	 'google-oauth2|116603781884964961816', 'Loren',	      'Hawthorne', '0',	NULL, NULL, NULL),
-(	'liztabor19@gmail.com',   'liztabor19@gmail.com',	     'google-oauth2|104286798726238637666', 'Liz',	        'Tabor',	   '0',	NULL, NULL, NULL),
-(	'other@sil.org',          'other@sil.org',	           'auth0|5c07e78511bf6d2f2ce0fcff',      'Other'	,       'SIL',       '0',	NULL, NULL, NULL),
-(	'Nate Canada',            'ncanada@developertown.com', 'google-oauth2|107093754016949028816', 'Nate', 	      'Canada',	   '0',	NULL, NULL, NULL),
-(   'Jon Nolen',              'jnolen@developertown.com',  'google-oauth2|113544451794352875093', 'Jon',  	      'Nolen', 	   '0', NULL, NULL, NULL),
-(	'Alex Billingsley',       'abillingsley@developertown.com', 'google-oauth2|106243064761677479593', 'Alex', 	      'Billingsley',	   '0',	NULL, NULL, NULL);
+  "Name",                   "Email",                     "ExternalId",                          "FamilyName",   "GivenName", "IsLocked", "Locale", "Phone", "Timezone", "EmailNotification") VALUES
+(	'Chris Hubbard',	        'chris_hubbard@sil.org',	   'google-oauth2|116747902156680384840',	'Hubbard',	    'Chris',	   '0',	NULL,	NULL,	NULL, '0'),
+(	'David Moore',	          'david_moore1@sil.org',	     'google-oauth2|114981819181509824425',	'Moore',	      'David',	   '0',	NULL,	NULL,	NULL, '0'),
+(	'Preston Sego (gmail)',	  'lt.sego@gmail.com',	       'google-oauth2|106685378104908547147',	'Sego (gmail)',	'Preston',	 '0',	NULL,	NULL,	NULL, '0'),
+(	'Chris Hubbard (Kalaam)',	'chris.kalaam@gmail.com',	   'auth0|5b578f6197af652b19f9bb41',	    'Hubbard',	    'Chris',	   '0',	NULL,	NULL,	NULL, '0'),
+(	'Bill Dyck',              'bill_dyck@sil.org',	       'google-oauth2|102643649500459434996', 'Dyck',	        'Bill',	     '0',	NULL, NULL, NULL, '0'),
+(	'Loren Hawthorne',        'loren_hawthrone@sil.org',	 'google-oauth2|116603781884964961816', 'Loren',	      'Hawthorne', '0',	NULL, NULL, NULL, '0');
 
 INSERT INTO "Organizations" ("Id", "Name", "WebsiteUrl", "BuildEngineUrl", "BuildEngineApiAccessToken", "OwnerId", "UseDefaultBuildEngine") VALUES
 (1,	'SIL International',	'https://sil.org',	'https://buildengine.gtis.guru:8443',	'replace',	1,	false),
@@ -88,70 +80,37 @@ INSERT INTO "Groups" ("Name", "Abbreviation", "OwnerId") VALUES
 (	'KAL Asia',	'KAL_AS',	3);
 
 INSERT INTO "OrganizationMemberships" ("UserId", "OrganizationId") VALUES
-(	1,	1), -- chris_hubbard@sil.org - SIL
-(	1,	2), -- chris_hubbard@sil.org - DT
-(	1,	3), -- chris_hubbard@sil.org - Kalaam
-(	2,	1), -- david_moore1@sil.org - SIL
-(	2,  3), -- david_moore1@sil.org - Kalaam
-(	3,	2), -- lt.sego@gmail.com - DT
-(	4,	2), -- gian.corzo@gmail.com - DT
-(	5,	2), -- ltabor@developertown.com - DT
-(	6,	3), -- chris.kalaam@gmail.com - Kalaam
-(	7,  1), -- psego@developertown.com - SIL
-(	7,  2), -- psego@developertown.com - DT
-(	8,  1), -- bill_dyck@sil.org - SIL
-(	8,  3), -- bill_dyck@sil.org - Kalaam
-(	9,  1), -- loren_hawthorne@sil.org - SIL
-(	10,  1), -- liztabor19+1@gmail.com - SIL
-(	10,  2), -- liztabor19+1@gmail.com - DT
-(	11,  1), -- other@sil.org - SIL
-(	12,	2), -- ncanada@developertown.com - DT
-( 13, 2), -- jnolen@developertown.com - DT
-(	5,	1), -- ltabor@developertown.com - SIL
-(	14,	2); -- abillingsley@developertown.com - DT
-
-
-
-
+( 1, 1), -- chris_hubbard@sil.org - SIL
+( 1, 2), -- chris_hubbard@sil.org - DT
+( 1, 3), -- chris_hubbard@sil.org - Kalaam
+( 2, 1), -- david_moore1@sil.org - SIL
+( 2, 3), -- david_moore1@sil.org - Kalaam
+( 3, 2), -- lt.sego@gmail.com - DT
+( 4, 3), -- chris.kalaam@gmail.com - Kalaam
+( 3, 1), -- psego@developertown.com - SIL
+( 3, 2), -- psego@developertown.com - DT
+( 5, 1), -- bill_dyck@sil.org - SIL
+( 5, 3), -- bill_dyck@sil.org - Kalaam
+( 6, 1); -- loren_hawthorne@sil.org - SIL
 
 INSERT INTO "GroupMemberships" ("UserId", "GroupId") VALUES
-(	1,	1),
-(	2,	1),
-(	2,	2),
-(	2,	15),
-(	4,	14),
-( 3,  14),
-( 7,  14),
-( 7,   1),
-( 5,  14),
-( 12,  14),
-( 14, 14);
+( 1, 1), -- chris_hubbard@sil.org - LSDEV
+( 2, 1), -- david_moore1@sil.org - LSDEV
+( 2, 2), -- david_moore1@sil.org - CHB
+( 2, 15), -- david_moore1@sil.org - KAL_AF
+( 3, 14); -- lt.sego@gmail.com - Development (DT)
 
 INSERT INTO "UserRoles" ("UserId", "RoleId", "OrganizationId") VALUES
-(	1,	1,	1), -- chris_hubbard@sil.org - SuperAdmin - SIL
-(	1,	2,	1), -- chris_hubbard@sil.org - OrgAmin - SIL
-(	8,	1,	1), -- bill_dyck@sil.org - AppBuilder - SIL
-(	2,	3,	1), -- david_moore1@sil.org - AppBuilder - SIL
-(	7,	1,	1), -- psego@developertown.com - AppBuilder - SIL
-(	9,	3,	1), -- loren_hawthorne@sil.org - AppBuilder - SIL
-(	7,	1,	2), -- psego@developertown.com - SuperAdmin - DT
-(	5,	1,	2), -- ltabor@developertown.com - SuperAdmin - DT
-(	5,	2,	2), -- ltabor@developertown.com - OrgAdmin - DT
-(	1,	2,	2), -- chris_hubbard@sil.org - OrgAdmin - DT
-(	3,	3,	2), -- lt.sego@gmail.com - AppBuilder - DT
-(	4,	3,	2), -- gian.corzo@gmail.com - AppBuilder - DT
-(	1,	1,	3), -- chris_hubbard@sil.org - SuperAdmin - Kalaam
-(	8,	1,	3), -- bill_dyck@sil.org - AppBuilder - Kalaam
-(	2,	2,	3), -- david_moore1@sil.org - OrgAdmin - Kalaam
-(	6,	3,	3), -- chris.kalaam@gmail.com - AppBuilder - Kalaam
-(	10,	3,	1), -- liztabor19+1@gmail.com - AppBuilder - SIL
-(	10,	3,	2), -- liztabor19+1@gmail.com - AppBuilder - DT
-(	11,	3,	1), -- other@sil.org - AppBuilder - SIL
-(	12,	1,	2), -- ncanada@developertown.com - SuperAdmin - DT
-(	12,	2,	2), -- ncanada@developertown.com - OrgAdmin - DT
-(	13,	2,	2), -- jnolen@developertown.com - OrgAdmin - DT
-( 14,	1,	2), -- jnolen@developertown.com - SuperAdmin - DT
-( 14,	2,	2); -- jnolen@developertown.com - OrgAdmin - DT
+( 1, 1, 1), -- chris_hubbard@sil.org - SuperAdmin - SIL
+( 1, 2, 1), -- chris_hubbard@sil.org - OrgAmin - SIL
+( 5, 3, 1), -- bill_dyck@sil.org - AppBuilder - SIL
+( 2, 3, 1), -- david_moore1@sil.org - AppBuilder - SIL
+( 6, 3, 1), -- loren_hawthorne@sil.org - AppBuilder - SIL
+( 1, 2, 2), -- chris_hubbard@sil.org - OrgAdmin - DT
+( 3, 3, 2), -- lt.sego@gmail.com - AppBuilder - DT
+( 5, 3, 3), -- bill_dyck@sil.org - AppBuilder - Kalaam
+( 2, 2, 3), -- david_moore1@sil.org - OrgAdmin - Kalaam
+( 4, 3, 3); -- chris.kalaam@gmail.com - AppBuilder - Kalaam
 
 /* Fix sequences ids */
 SELECT SETVAL('"ApplicationTypes_Id_seq"', COALESCE(MAX("Id"), 1) )
