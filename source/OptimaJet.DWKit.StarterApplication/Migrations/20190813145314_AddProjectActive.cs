fec98dc7b50ecb702209211ad36509ed5b2f1542
Chris Hubbard <chris@thehubbards.org>
2019-08-14 15:31:47 -0400

    Add Project.DateActive

---
+++ source/OptimaJet.DWKit.StarterApplication/Migrations/20190813145314_AddProjectActive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OptimaJet.DWKit.StarterApplication.Migrations
+{
+    public partial class AddProjectActive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateActive",
+                table: "Projects",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateActive",
+                table: "Projects");
+        }
+    }
+}
