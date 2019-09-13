0f406707a45f1b7b1308678639f7d17b86d3c037
Chris Hubbard <chris@thehubbards.org>
2019-09-13 09:04:57 -0400

    Combine helper code from LogEmailSender and AmazonSender (#708)

--- source/OptimaJet.DWKit.StarterApplication/Utility/FluentEmailHelpers.cs
+++ source/OptimaJet.DWKit.StarterApplication/Utility/FluentEmailHelpers.cs
@@ -1,14 +1,19 @@
-﻿using FluentEmail.Core.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using FluentEmail.Core.Models;
 
 namespace OptimaJet.DWKit.StarterApplication.Utility
 {
     public static class FluentEmailHelpers
     {
-        public static string AddressToString(Address address)
-        {
-            return (string.IsNullOrWhiteSpace(address.Name))
-                ? address.EmailAddress
-                : $"{address.Name} <{address.EmailAddress}>";
-        }
+        public static string AddressToString(Address address) =>
+            new MailAddress(address.EmailAddress, address.Name).ToString();
+
+        public static string AddressesToString(List<Address> addresses) =>
+           string.Join(",", addresses.Select((Address arg) => AddressToString(arg)));
+
+        public static List<string> AddressesToStrings(List<Address> addresses) =>
+            addresses.ConvertAll(address => AddressToString(address));
     }
 }
