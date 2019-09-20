fb8c2ac7a01bb4d837b052864eda51b040682e0d
Chris Hubbard <chris@thehubbards.org>
2019-09-20 13:00:26 -0400

    Add WorkflowDefinition.Properties

---
+++ source/OptimaJet.DWKit.StarterApplication/Migrations/20190919193626_AddWorkflowDefinitionProperties.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OptimaJet.DWKit.StarterApplication.Migrations
+{
+    public partial class AddWorkflowDefinitionProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Properties",
+                table: "WorkflowDefinitions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Properties",
+                table: "WorkflowDefinitions");
+        }
+    }
+}
