de4b216a11babb44b317476e306d82b2252f116f
David Moore <david_moore1@sil.org>
2019-10-07 07:46:25 -0400

    Right Size Task Wait Control (#714)
    The wait box that appears while the app tries to retrieve the
    current task info is very large and then shrinks to one line
    once the task is found.  This fix adjusts the size of the box
    while it is finding the task info to something close to the final
    size.

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/loaders/page.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/loaders/page.tsx
@@ -4,15 +4,16 @@ import RectLoader from './rect-loader';
 
 interface IProps {
   loaderProps?: any;
+  sizeClass?: string;
 }
 
 export default class PageLoader extends React.PureComponent<IProps> {
   render() {
     const loaderProps = this.props.loaderProps || {};
-
+    const sizeClass = this.props.sizeClass || 'm-t-xxl m-b-xxl';
     return (
       <div className='flex-row flex-grow justify-content-center align-items-center'>
-        <RectLoader className='m-t-xxl m-b-xxl' {...loaderProps} />
+        <RectLoader className={sizeClass} {...loaderProps} />
       </div>
     );
   }
