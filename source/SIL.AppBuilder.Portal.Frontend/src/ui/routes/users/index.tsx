d05778b061ce39f08a2f8429eaca42580ea9792b
David Moore <david_moore1@sil.org>
2019-11-11 16:28:33 -0500

    Add missing routing file

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/users/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/users/index.tsx
@@ -8,6 +8,7 @@ import { NotFound } from '@ui/routes/errors';
 import ListRoute, { pathName as listPath } from './list';
 import ShowRoute, { pathName as showPath } from './show';
 import EditRoute, { pathName as editPath } from './edit';
+import NewEditRoute, { pathName as newEditPath } from './newedit';
 
 export default function UsersRoot() {
   return (
@@ -15,6 +16,7 @@ export default function UsersRoot() {
       <Route exact path={listPath} component={ListRoute} />
       <Route exact path={showPath} component={ShowRoute} />
       <Route path={editPath} component={EditRoute} />
+      <Route path={newEditPath} component={NewEditRoute} />
 
       <Route component={NotFound} />
     </Switch>
