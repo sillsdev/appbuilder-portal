f5b3c92915d2da67be89195044da711eb54e6367
David Moore <david_moore1@sil.org>
2019-11-12 16:01:24 -0500

    Display error on not active user login (#745)
    * Display error on not active user login
    
    * Change comment

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/errors/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/errors/index.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import NotFoundRoute, { pathName as notFoundPath } from './not-found';
 import OrgMembershipRequired, { pathName as orgRequiredPath } from './org-membership-required';
+import NotActiveUser, { pathName as notActiveUserPath } from './not-active-user';
 import VerifyEmail, { pathName as verifyEmailPath } from './verify-email';
 export const NotFound = NotFoundRoute;
 
@@ -12,6 +13,7 @@ export default class ErrorsRootRoute extends React.Component {
       <Switch>
         <Route exact path={notFoundPath} component={NotFoundRoute} />
         <Route exact path={orgRequiredPath} component={OrgMembershipRequired} />
+        <Route exact path={notActiveUserPath} component={NotActiveUser} />
         <Route exact path={verifyEmailPath} component={VerifyEmail} />
         <Route component={NotFoundRoute} />
       </Switch>
---
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/errors/not-active-user.tsx
@@ -0,0 +1,5 @@
+import NotActiveUser from '@ui/components/errors/not-active-user';
+
+export const pathName = '/not-active-user';
+
+export default NotActiveUser;
