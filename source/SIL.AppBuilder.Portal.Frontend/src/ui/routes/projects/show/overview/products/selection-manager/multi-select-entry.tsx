347bb3c72ac19ce7cef058ab5b27829ce00b1c6c
davidmoore1 <david_moore1@sil.org>
2020-01-14 09:32:57 -0500

    Removing  unnecessary import

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/overview/products/selection-manager/multi-select-entry.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/projects/show/overview/products/selection-manager/multi-select-entry.tsx
@@ -3,7 +3,6 @@ import { ResourceObject } from 'jsonapi-typescript';
 
 import { attributesFor, relationshipFor } from '@data';
 
-import { Checkbox } from 'semantic-ui-react';
 import ProductIcon from '@ui/components/product-icon';
 import { i18nProps } from '@lib/i18n';
 import { isEmpty } from '@lib/collection';
