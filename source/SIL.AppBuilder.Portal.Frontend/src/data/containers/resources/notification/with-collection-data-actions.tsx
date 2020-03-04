e9f12468d91325f8ee5956b20ed8f09064124fed
Chris Hubbard <chris@thehubbards.org>
2020-03-04 13:44:01 -0500

    Improve 'Clear All' notifications efficiency (#770)
    * Allow more efficient 'Clear All' notifications implementation
    
    * Fix tests to work
    
    * tweak travis config; remove errors and warnings
    
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

--- source/SIL.AppBuilder.Portal.Frontend/src/data/containers/resources/notification/with-collection-data-actions.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/data/containers/resources/notification/with-collection-data-actions.tsx
@@ -7,6 +7,7 @@ import { useCallback } from 'react';
 import { recordsThatStillExist } from '~/data/store-helpers';
 
 import { attributesFor } from 'react-orbitjs';
+import { destroy } from '@lib/fetch';
 
 export function useCollectionDataActions(notifications: NotificationResource[]) {
   const { pushData, dataStore } = useLiveData();
@@ -42,15 +43,18 @@ export function useCollectionDataActions(notifications: NotificationResource[]) 
       return;
     }
 
-    pushData((t) =>
-      records.map((notification) => {
-        return t.removeRecord(notification);
-      })
-    );
-
+    try {
+      const result = await destroy(`/api/notifications/all`, {
+        headers: {
+          Accept: 'application/vnd.api+json',
+          'Content-Type': 'application/vnd.api+json',
+        },
+      });
+    } catch (error) {
+      console.error('Caught error deleting notifications:', error);
+    }
     // the backend doesn't return records in the operations payload when they are removed
     dataStore.update((t) => records.map((n) => t.removeRecord(n)), { skipRemote: true });
-  }, [dataStore, notifications, pushData]);
-
+  }, [dataStore, notifications]);
   return { markAllAsViewed, clearAll };
 }
