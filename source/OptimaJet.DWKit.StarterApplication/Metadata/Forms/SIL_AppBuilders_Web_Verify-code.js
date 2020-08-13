d8a393e61e217e61c22ae010f488ab751274b75c
Chris Hubbard <chris@thehubbards.org>
2020-08-13 10:17:38 -0400

    openArtifactLink not showing up for SIL_AppBuilders_Web_Verify form

--- source/OptimaJet.DWKit.StarterApplication/Metadata/Forms/SIL_AppBuilders_Web_Verify-code.js
+++ source/OptimaJet.DWKit.StarterApplication/Metadata/Forms/SIL_AppBuilders_Web_Verify-code.js
@@ -1,5 +1,5 @@
 {
-    openArtifactLink: function({controlRef, parameters}){
+    openWebArtifactLink: function({controlRef, parameters}){
         let selectedRow = controlRef.state.items[parameters.rowIdx];
         var link = selectedRow.Url;
         var win = window.open(link, '_blank');
