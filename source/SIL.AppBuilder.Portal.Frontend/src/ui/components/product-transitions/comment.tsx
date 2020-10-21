b283f971470d464f119c9691b1d84c02eee17518
davidmoore1 <david_moore1@sil.org>
2020-10-21 13:57:45 -0400

    Eliminate unneeded import

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/components/product-transitions/comment.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/components/product-transitions/comment.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslations } from '@lib/i18n';
 
 export default function TransitionComment({ comment }) {
