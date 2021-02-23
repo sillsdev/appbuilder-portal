ee057470a02a0a3c5c670a52dfb6c2a229c44940
Chris Hubbard <chris@thehubbards.org>
2021-02-23 16:53:28 -0500

    Change OrganizationMembershipInvite processing to remove duplicate entries

--- source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/invitations/index.tsx
+++ source/SIL.AppBuilder.Portal.Frontend/src/ui/routes/invitations/index.tsx
@@ -8,9 +8,6 @@ import NotFoundRoute from '@ui/routes/errors/not-found';
 import CreateOrganizationRoute, { pathName as createPath } from './create-organization';
 import MissingTokenRoute, { pathName as missingTokenPath } from './missing-token';
 import JoinOrganizationRoute, { pathName as joinOrganizationPath } from './join-organization';
-import JoinOrganizationFinishedRoute, {
-  pathName as joinOrganizationFinishedPath,
-} from './join-organization/finished';
 
 export default class InvitationsRoute extends React.Component {
   render() {
@@ -19,11 +16,7 @@ export default class InvitationsRoute extends React.Component {
         <Route exact path={createPath} component={CreateOrganizationRoute} />
         <Route exact path={missingTokenPath} component={MissingTokenRoute} />
         <Route exact path={joinOrganizationPath} component={JoinOrganizationRoute} />
-        <Route
-          exact
-          path={joinOrganizationFinishedPath}
-          component={JoinOrganizationFinishedRoute}
-        />
+
         <Route component={NotFoundRoute} />
       </Switch>
     );
