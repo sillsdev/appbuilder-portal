dfc0190bafe4fe8c91b101ad602d74ffde52d301
Chris Hubbard <chris@thehubbards.org>
2021-09-08 14:00:07 -0400

    Increase deployment timeout

--- scripts/deploy-to-ecs.sh
+++ scripts/deploy-to-ecs.sh
@@ -5,5 +5,5 @@ source ./scripts/build-setup.sh
 echo "Attempting to deploy $CURRENT_VERSION to Cluster: '$ECS_CLUSTER'..."
 
 if [ -n "$ECS_CLUSTER" ]; then
-  ecs-deploy -c $ECS_CLUSTER -n portal -i ignore -to $CURRENT_VERSION --max-definitions 20 --timeout 300
+  ecs-deploy -c $ECS_CLUSTER -n portal -i ignore -to $CURRENT_VERSION --max-definitions 20 --timeout 600
 fi
