42523221e1f2b8e5281c4080a5e3adb3f00b7e06
Chris Hubbard <chris@thehubbards.org>
2023-10-18 08:51:50 -0400

    Initial UI
    * locale
    * navbar

---
+++ source/SIL.AppBuilder.Portal/src/lib/icons/ArrowBackIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    // Arrow Back from https://fonts.google.com/icons
+    // Filled = 0
+    // Weight = 400
+    // Grade = 0
+    // Optical size = 24px
+    export let color = 'black';
+</script>
+
+<svg fill={color} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"
+    ><path d="M480 896 160 576l320-320 57 56-224 224h487v80H313l224 224-57 56Z" /></svg
+>
