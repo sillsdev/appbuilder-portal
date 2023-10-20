d4f79cd161f155a494cf4159291e2311347b137a
Chris Hubbard <chris@thehubbards.org>
2023-10-20 09:37:42 -0400

    Stub out sidebar

--- portal/src/routes/+layout.svelte
+++ portal/src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script>
 	import { _ } from 'svelte-i18n';
+	import { base } from '$app/paths';
 	import '../app.css';
 	import { HamburgerIcon } from '$lib/icons';
 	import LanguageSelector from '$lib/components/LanguageSelector.svelte';
+
+	$: organization = 1;
 </script>
 
 <div class="navbar bg-[#1C3258]">
 	<div class="navbar-start">
-		<label for="sidebar" class="btn btn-ghost btn-circle p-1 drawer-button lg:hidden">
+		<label
+			for="primary-content-drawer"
+			class="btn btn-ghost btn-circle p-1 drawer-button lg:hidden"
+		>
 			<HamburgerIcon color="white" />
 		</label>
 		<p class="uppercase text-white lg:ps-4">{$_('appName')}</p>
@@ -17,7 +23,66 @@
 		<LanguageSelector />
 	</div>
 </div>
-<slot />
+
+<div class="drawer lg:drawer-open">
+	<input id="primary-content-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-row items-start justify-start">
+		<slot />
+	</div>
+
+	<div class="drawer-side">
+		<label for="primary-content-drawer" class="drawer-overlay" />
+		<ul
+			class="menu mt-16 lg:mt-0 rounded-r-xl p-4 w-1/3 lg:w-64 bg-base-100 text-base-content h-full"
+		>
+			<li>
+				<a href="{base}/tasks">
+					{$_('sidebar.myTasks')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/projects/own">
+					{$_('sidebar.myProjects')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/projects/organization">
+					{$_('sidebar.organizationProjects')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/projects/active">
+					{$_('sidebar.activeProjects')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/users">
+					{$_('sidebar.users')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/orgainizations/{organization}/settings">
+					{$_('sidebar.organizationSettings')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/admin/settings/orgainizations">
+					{$_('sidebar.adminSettings')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/directory">
+					{$_('sidebar.projectDirectory')}
+				</a>
+			</li>
+			<li>
+				<a href="{base}/open-source">
+					{$_('opensource')}
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
 
 <style>
 	.navbar {
