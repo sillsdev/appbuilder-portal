808b634425c61968502e1735e6ca7f37b1c4747d
Chris Hubbard <chris@thehubbards.org>
2023-10-27 18:35:08 -0400

    Page Header

--- portal/src/routes/projects/active/+page.svelte
+++ portal/src/routes/projects/active/+page.svelte
@@ -2,6 +2,4 @@
 	import { _ } from 'svelte-i18n';
 </script>
 
-<div class="max-w-screen-md mx-auto">
-	<h1>{$_('projects.switcher.dropdown.activeProjects')}</h1>
-</div>
+<h1>{$_('projects.switcher.dropdown.activeProjects')}</h1>
