a86fa58cb5010a39c74ba31edc9f8e2c8a36259f
Chris Hubbard <chris@thehubbards.org>
2023-10-31 21:20:12 -0400

    Start the admin UI
    * still needs sizing of boxes and buttons

--- source/SIL.AppBuilder.Portal/src/routes/admin/settings/organizations/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/organizations/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
+	import type { PageData } from './$types';
 	import { _ } from 'svelte-i18n';
+
+	export let data: PageData;
 </script>
 
-<h2>{$_('admin.settings.organizations.title')}</h2>
+<div class="flex flex-col">
+	<h2>{$_('admin.settings.organizations.title')}</h2>
+
+	<div class="btn btn-outline rounded-none m-4 mt-0">{$_('admin.settings.organizations.add')}</div>
+
+	<div class="flex flex-col w-full">
+		{#each data.organizations as organization}
+			<div class="flex flex-row border-2 p-2 w-full ml-4 m-2">
+				<div>
+					<h3>{organization.Name}</h3>
+					<p><b>{$_('admin.settings.organizations.owner')}: </b> {organization.Owner.Name}</p>
+					<p>
+						<b>{$_('admin.settings.organizations.buildEngineURL')}: </b>
+						{organization.WebsiteUrl}
+					</p>
+					<p>
+						<b>{$_('admin.settings.organizations.accessToken')}: </b>
+						{organization.BuildEngineApiAccessToken}
+					</p>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
