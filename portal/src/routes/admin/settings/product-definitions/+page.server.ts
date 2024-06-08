f10c65a21e7e6bb9fd161efcc42af2ab30b4907d
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-08 14:12:22 -0400

    Implement product definitions

---
+++ portal/src/routes/admin/settings/product-definitions/+page.server.ts
@@ -0,0 +1,12 @@
+// src/routes/+page.server.ts
+
+import prisma from '$lib/prisma';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	const productDefinitions = await prisma.productDefinitions.findMany({
+		include: { ApplicationTypes: true, Workflow: true, RebuildWorkflow: true, RepublishWorkflow: true }
+	});
+
+	return { productDefinitions };
+}) satisfies PageServerLoad;
--- portal/src/routes/admin/settings/product-definitions/+page.svelte
+++ portal/src/routes/admin/settings/product-definitions/+page.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
-</script>
+	import type { PageData } from './$types';
 
-<h2>{$_('admin.settings.productDefinitions.title')}</h2>
+	export let data: PageData;
+</script>
+<div class="flex grow flex-col">
+	<h2>{$_('admin.settings.productDefinitions.title')}</h2>
+
+	<div class="btn btn-outline rounded-none m-4 mt-0">
+		{$_('admin.settings.productDefinitions.add')}
+	</div>
+
+	<div class="flex flex-col w-full">
+		{#each data.productDefinitions.sort((a, b) => a.Name?.localeCompare(b.Name ?? "") ?? 0) as pD}
+			<div class="flex flex-row border-2 p-2 mx-4 m-2">
+				<div>
+					<h3>{pD.Name}</h3>
+					<p><b>{$_('admin.settings.productDefinitions.type')}: </b> {pD.ApplicationTypes.Name}</p>
+					<p><b>{$_('admin.settings.productDefinitions.workflow')}: </b> {pD.Workflow.Name}</p>
+					<p><b>{$_('admin.settings.productDefinitions.rebuildWorkflow')}: </b> {pD.RebuildWorkflow?.Name ?? ""}</p>
+					<p><b>{$_('admin.settings.productDefinitions.republishWorkflow')}: </b> {pD.RepublishWorkflow?.Name ?? ""}</p>
+					<p><b>{$_('admin.settings.productDefinitions.description')}: </b> {pD.Description}</p>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
