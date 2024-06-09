4e02621d4b26c696dd2ca485a44652d15bc93283
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-09 00:22:37 -0400

    Implement stores, storeTypes and Build Engine Statuses

--- source/SIL.AppBuilder.Portal/src/routes/admin/settings/store-types/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/store-types/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
+	import type { PageData } from './$types';
+	import InternationalizedDataBox from '$lib/components/InternationalizedDataBox.svelte';
+
+	export let data: PageData;
 </script>
 
-<h2>{$_('admin.settings.storeTypes.title')}</h2>
+
+<div class="flex grow flex-col">
+	<h2>{$_('admin.settings.storeTypes.title')}</h2>
+
+	<div class="btn btn-outline rounded-none m-4 mt-0">
+		{$_('admin.settings.storeTypes.add')}
+	</div>
+
+	<div class="flex flex-col w-full">
+		{#each data.storeTypes as storeType}
+			<InternationalizedDataBox title="{storeType.Name}" fields="{[
+				{ key: 'stores.attributes.description', value: storeType.Description },
+			]}" />
+		{/each}
+	</div>
+</div>
