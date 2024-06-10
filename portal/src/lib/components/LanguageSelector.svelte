dd73f592546daa50e3b2f0035c3e8e371858f105
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-10 14:24:51 -0400

    Fix IDE errors

--- portal/src/lib/components/LanguageSelector.svelte
+++ portal/src/lib/components/LanguageSelector.svelte
@@ -3,7 +3,7 @@
 	import { LanguageIcon } from '$lib/icons';
 	import { _, locale, locales } from 'svelte-i18n';
 
-	function setLocale(lang: string | null | undefined) {
+	function setLocale(lang: string | null | undefined): undefined {
 		locale.set(lang);
 		const elem = document.activeElement as HTMLElement;
 		if (elem) {
@@ -23,13 +23,14 @@
 
 {#key $_('lang')}
 	<div class="dropdown dropdown-end">
-		<label
-			for
+		<!-- When .dropdown is focused, .dropdown-content is revealed making this actually interactive -->
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<div
 			class="btn btn-ghost m-2 p-2 rounded-xl items-middle justify-center flex-nowrap"
-			tabindex="-1"
+			tabindex="0"
 		>
 			<LanguageIcon color="white" />
-		</label>
+	</div>
 		<div class="dropdown-content z-10 bg-base-200 w-48 rounded-md overflow-y-auto">
 			<ul class="menu menu-compact gap-1 p-2" tabindex="-1">
 				{#each $locales as lang}
@@ -38,6 +39,8 @@
 							class="btn flex {isActive(lang, $locale)}"
 							on:click={setLocale(lang)}
 							on:keypress={setLocale(lang)}
+							role="button"
+							tabindex="0"
 						>
 							<Icon icon="circle-flags:{lang}" color="white" width="24" />
 							{lang}
