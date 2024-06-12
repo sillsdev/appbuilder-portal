1a0fb6165e70a80cf5b00d643e2ce8792958d5d8
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-12 15:10:23 -0400

    Componentize input slots

--- portal/src/routes/admin/settings/product-definitions/+page.server.ts
+++ portal/src/routes/admin/settings/product-definitions/+page.server.ts
@@ -1,11 +1,7 @@
 // src/routes/+page.server.ts
 
-import * as v from 'valibot';
 import prisma from '$lib/prisma';
-import type { Actions, PageServerLoad } from './$types';
-import { superValidate } from 'sveltekit-superforms';
-import { valibot } from 'sveltekit-superforms/adapters';
-import { fail } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
   const productDefinitions = await prisma.productDefinitions.findMany({
@@ -16,56 +12,6 @@ export const load = (async () => {
       RepublishWorkflow: true
     }
   });
-  // productDefinitions[0].
 
   return { productDefinitions };
 }) satisfies PageServerLoad;
-
-// export const actions = {
-//   async edit({ cookies, request }) {
-//     // const data = Object.fromEntries(await request.formData());
-//     const form = await superValidate(request, valibot(editSchema));
-//     console.log(form);
-//     if (!form.valid) {
-//       return fail(400, { form });
-//     }
-//     return { form };
-//     // try {
-//     //   const {
-//     //     id,
-//     //     name,
-//     //     applicationType,
-//     //     workflow,
-//     //     rebuildWorkflow,
-//     //     republishWorkflow,
-//     //     description,
-//     //     properties
-//     //   } = form.data;
-//     //   await prisma.productDefinitions.update({
-//     //     where: {
-//     //       Id: id
-//     //     },
-//     //     data: {
-//     //       TypeId: applicationType,
-//     //       Name: name,
-//     //       WorkflowId: workflow,
-//     //       RebuildWorkflowId: rebuildWorkflow,
-//     //       RepublishWorkflowId: republishWorkflow,
-//     //       Description: description,
-//     //       Properties: properties
-//     //     }
-//     //   });
-//     // } catch (e) {
-//     //   if (e instanceof v.ValiError) return { errors: e.issues };
-//     //   throw e;
-//     // }
-//     // const Id = data.get('id');
-//     // const name = data.get('name');
-//     // const applicationType = data.get('applicationType');
-//     // const workflow = data.get('workflow');
-//     // const rebuildWorkflow = data.get('rebuildWorkflow');
-//     // const republishWorkflow = data.get('republishWorkflow');
-//     // const description = data.get('description');
-//     // const properties = data.get('properties');
-//   }
-// } satisfies Actions;
--- portal/src/routes/admin/settings/product-definitions/edit/+page.svelte
+++ portal/src/routes/admin/settings/product-definitions/edit/+page.svelte
@@ -3,6 +3,7 @@
   import { _ } from 'svelte-i18n';
   import type { ActionData, PageData } from './$types';
   import { goto } from '$app/navigation';
+  import InternationalizedInput from '$lib/components/InternationalizedInput.svelte';
 
   export let data: PageData;
   export let form: ActionData;
@@ -17,105 +18,70 @@
 <!-- <SuperDebug data={superForm} /> -->
 <form class="m-4" method="post" action="?/edit" use:enhance>
   <input type="hidden" name="id" value={$superFormData.id} />
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.name')}</span>
-      </div>
-      <input
-        class="input w-full input-bordered"
-        type="text"
-        name="name"
-        bind:value={$superFormData.name}
-      />
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.type')}</span>
-      </div>
-      <select
-        class="select select-bordered"
-        name="applicationType"
-        bind:value={$superFormData.applicationType}
-      >
-        {#each data.options.applicationTypes as type}
-          <option value={type.Id}>{type.Name}</option>
-        {/each}
-      </select>
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.workflow')}</span>
-      </div>
-      <select class="select select-bordered" name="workflow" bind:value={$superFormData.workflow}>
-        {#each workflows.filter((w) => w.Type) as workflow}
-          <option value={workflow.Id}>{workflow.Name}</option>
-        {/each}
-      </select>
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.rebuildWorkflow')}</span>
-      </div>
-      <select
-        class="select select-bordered"
-        name="rebuildWorkflow"
-        bind:value={$superFormData.rebuildWorkflow}
-      >
-        <option value={null}>{$_('admin.settings.productDefinitions.noWorkflow')}</option>
-        {#each rebuildWorkflows.filter((w) => w.Type) as workflow}
-          <option value={workflow.Id}>{workflow.Name}</option>
-        {/each}
-      </select>
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.republishWorkflow')}</span>
-      </div>
-      <select
-        class="select select-bordered"
-        name="republishWorkflow"
-        bind:value={$superFormData.republishWorkflow}
-      >
-        <option value={null}>{$_('admin.settings.productDefinitions.noWorkflow')}</option>
-        {#each republishWorkflows.filter((w) => w.Type) as workflow}
-          <option value={workflow.Id}>{workflow.Name}</option>
-        {/each}
-      </select>
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.description')}</span>
-      </div>
-      <textarea
-        name="description"
-        class="textarea textarea-bordered w-full"
-        bind:value={$superFormData.description}
-      />
-    </label>
-  </div>
-  <div>
-    <label>
-      <div class="label">
-        <span class="label-text">{$_('admin.settings.productDefinitions.properties')}</span>
-      </div>
-      <textarea
-        name="properties"
-        class="textarea textarea-bordered w-full"
-        bind:value={$superFormData.properties}
-      />
-    </label>
-  </div>
+  <InternationalizedInput name="admin.settings.productDefinitions.name">
+    <input
+      class="input w-full input-bordered"
+      type="text"
+      name="name"
+      bind:value={$superFormData.name}
+    />
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.type">
+    <select
+      class="select select-bordered"
+      name="applicationType"
+      bind:value={$superFormData.applicationType}
+    >
+      {#each data.options.applicationTypes as type}
+        <option value={type.Id}>{type.Name}</option>
+      {/each}
+    </select>
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.workflow">
+    <select class="select select-bordered" name="workflow" bind:value={$superFormData.workflow}>
+      {#each workflows.filter((w) => w.Type) as workflow}
+        <option value={workflow.Id}>{workflow.Name}</option>
+      {/each}
+    </select>
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.rebuildWorkflow">
+    <select
+      class="select select-bordered"
+      name="rebuildWorkflow"
+      bind:value={$superFormData.rebuildWorkflow}
+    >
+      <option value={null}>{$_('admin.settings.productDefinitions.noWorkflow')}</option>
+      {#each rebuildWorkflows.filter((w) => w.Type) as workflow}
+        <option value={workflow.Id}>{workflow.Name}</option>
+      {/each}
+    </select>
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.republishWorkflow">
+    <select
+      class="select select-bordered"
+      name="republishWorkflow"
+      bind:value={$superFormData.republishWorkflow}
+    >
+      <option value={null}>{$_('admin.settings.productDefinitions.noWorkflow')}</option>
+      {#each republishWorkflows.filter((w) => w.Type) as workflow}
+        <option value={workflow.Id}>{workflow.Name}</option>
+      {/each}
+    </select>
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.description">
+    <textarea
+      name="description"
+      class="textarea textarea-bordered w-full"
+      bind:value={$superFormData.description}
+    />
+  </InternationalizedInput>
+  <InternationalizedInput name="admin.settings.productDefinitions.properties">
+    <textarea
+      name="properties"
+      class="textarea textarea-bordered w-full"
+      bind:value={$superFormData.properties}
+    />
+  </InternationalizedInput>
   <div>
     <input type="submit" class="btn btn-primary" value="Submit" />
     <a class="btn" href="/admin/settings">Cancel</a>
