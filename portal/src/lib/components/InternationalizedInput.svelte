15ba76180934cffb69a4861fa7160bcfa5133ef7
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-12 15:10:23 -0400

    Componentize input slots

---
+++ portal/src/lib/components/InternationalizedInput.svelte
@@ -0,0 +1,20 @@
+<!--
+    @component
+    An input slot for a form (stylized)
+    
+-->
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  export let name: string;
+</script>
+
+<div>
+  <label>
+    <div class="label">
+      <span class="label-text">
+        {$_(name)}
+      </span>
+    </div>
+    <slot />
+  </label>
+</div>
