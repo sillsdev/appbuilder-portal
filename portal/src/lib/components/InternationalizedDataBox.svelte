b41e37975fc66a9cb41c40f2ab29c827c09312ff
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-11 22:13:59 -0400

    First version of admin settings edit

--- portal/src/lib/components/InternationalizedDataBox.svelte
+++ portal/src/lib/components/InternationalizedDataBox.svelte
@@ -4,19 +4,31 @@
     
 -->
 <script lang="ts">
-    import { _ } from "svelte-i18n";
-    export let title;
-    export let fields: {key: string, value?: string | null}[];
+	import Icon from '@iconify/svelte';
+	import { createEventDispatcher } from 'svelte';
+	import { _ } from 'svelte-i18n';
+
+	export let title;
+	export let fields: { key: string; value?: string | null }[];
+	export let editable = false;
+	const dispatch = createEventDispatcher<{
+		edit: null;
+	}>();
 </script>
 
 <div class="flex flex-row border border-slate-600 p-2 mx-4 m-1 rounded-md">
-    <div>
-        <h3>{title}</h3>
-        {#each fields as field}
-            <p>
-                <b>{$_(field.key)}: </b>
-                {field.value ?? ""}
-            </p>
-        {/each}
-    </div>
+	<div class="relative">
+		<h3>{title}</h3>
+		{#if editable}
+			<button class="absolute right-2 top-2" on:click={() => dispatch('edit')}
+				><Icon icon="mdi:pencil" /></button
+			>
+		{/if}
+		{#each fields as field}
+			<p>
+				<b>{$_(field.key)}: </b>
+				{field.value ?? ''}
+			</p>
+		{/each}
+	</div>
 </div>
