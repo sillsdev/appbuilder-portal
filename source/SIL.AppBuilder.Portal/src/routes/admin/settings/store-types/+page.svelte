2aff29fd0f498f640b756f638d779988d6636065
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-12 13:40:40 -0400

    Reduce dup code

--- source/SIL.AppBuilder.Portal/src/routes/admin/settings/store-types/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/store-types/+page.svelte
@@ -6,19 +6,17 @@
   export let data: PageData;
 </script>
 
-<div class="flex grow flex-col">
-  <h2>{$_('admin.settings.storeTypes.title')}</h2>
+<h2>{$_('admin.settings.storeTypes.title')}</h2>
 
-  <div class="btn btn-outline rounded-none m-4 mt-0">
-    {$_('admin.settings.storeTypes.add')}
-  </div>
+<div class="btn btn-outline rounded-none m-4 mt-0">
+  {$_('admin.settings.storeTypes.add')}
+</div>
 
-  <div class="flex flex-col w-full">
-    {#each data.storeTypes as storeType}
-      <InternationalizedDataBox
-        title={storeType.Name}
-        fields={[{ key: 'stores.attributes.description', value: storeType.Description }]}
-      />
-    {/each}
-  </div>
+<div class="flex flex-col w-full">
+  {#each data.storeTypes as storeType}
+    <InternationalizedDataBox
+      title={storeType.Name}
+      fields={[{ key: 'stores.attributes.description', value: storeType.Description }]}
+    />
+  {/each}
 </div>
--- source/SIL.AppBuilder.Portal/src/routes/admin/settings/stores/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/stores/+page.svelte
@@ -6,22 +6,20 @@
   export let data: PageData;
 </script>
 
-<div class="flex grow flex-col">
-  <h2>{$_('admin.settings.navigation.stores')}</h2>
+<h2>{$_('admin.settings.navigation.stores')}</h2>
 
-  <div class="btn btn-outline rounded-none m-4 mt-0">
-    {$_('models.add', { values: { name: $_('stores.name') } })}
-  </div>
+<div class="btn btn-outline rounded-none m-4 mt-0">
+  {$_('models.add', { values: { name: $_('stores.name') } })}
+</div>
 
-  <div class="flex flex-col w-full">
-    {#each data.stores as store}
-      <InternationalizedDataBox
-        title={store.Name}
-        fields={[
-          { key: 'stores.attributes.description', value: store.Description },
-          { key: 'storeTypes.name', value: store.StoreType.Name }
-        ]}
-      />
-    {/each}
-  </div>
+<div class="flex flex-col w-full">
+  {#each data.stores as store}
+    <InternationalizedDataBox
+      title={store.Name}
+      fields={[
+        { key: 'stores.attributes.description', value: store.Description },
+        { key: 'storeTypes.name', value: store.StoreType.Name }
+      ]}
+    />
+  {/each}
 </div>
