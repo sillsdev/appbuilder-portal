a48a8440cf503352c902a73ee4d715700bcae4e6
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-12 23:28:21 -0400

    Make admin store types editable

--- portal/src/routes/admin/settings/store-types/+page.svelte
+++ portal/src/routes/admin/settings/store-types/+page.svelte
@@ -2,11 +2,10 @@
   import { _ } from 'svelte-i18n';
   import type { PageData } from './$types';
   import DataDisplayBox from '$lib/components/settings/DataDisplayBox.svelte';
+  import { goto } from '$app/navigation';
 
   export let data: PageData;
 </script>
-
-<h2>{$_('admin.settings.storeTypes.title')}</h2>
 
 <div class="btn btn-outline rounded-none m-4 mt-0">
   {$_('admin.settings.storeTypes.add')}
@@ -15,6 +14,8 @@
 <div class="flex flex-col w-full">
   {#each data.storeTypes as storeType}
     <DataDisplayBox
+      editable
+      on:edit={() => goto('/admin/settings/store-types/edit?id=' + storeType.Id)}
       title={storeType.Name}
       fields={[{ key: 'stores.attributes.description', value: storeType.Description }]}
     />
