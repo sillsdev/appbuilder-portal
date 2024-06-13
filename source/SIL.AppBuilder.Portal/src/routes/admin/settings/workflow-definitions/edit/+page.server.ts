e98c4d6c7d21d001f594f5a0b358ebe6e52beedf
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-12 22:49:48 -0400

    Add Workflow Definition edits

---
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/workflow-definitions/edit/+page.server.ts
@@ -0,0 +1,88 @@
+import prisma from '$lib/prisma';
+import { superValidate } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import * as v from 'valibot';
+import type { Actions, PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
+
+const editSchema = v.object({
+  id: v.pipe(v.number(), v.minValue(0), v.integer()),
+  name: v.nullable(v.string()),
+  storeType: v.pipe(v.number(), v.minValue(0), v.integer()),
+  workflowType: v.pipe(v.number(), v.minValue(0), v.integer()),
+  workflowScheme: v.nullable(v.string()),
+  workflowBusinessFlow: v.nullable(v.string()),
+  description: v.nullable(v.string()),
+  properties: v.nullable(v.string()),
+  enabled: v.boolean()
+});
+export const load = (async ({ url }) => {
+  const id = parseInt(url.searchParams.get('id') ?? '');
+  if (isNaN(id)) {
+    return redirect(302, base + '/admin/settings/workflow-definitions');
+  }
+  const data = await prisma.workflowDefinitions.findFirst({
+    where: {
+      Id: id
+    }
+  });
+  if (!data) return redirect(302, base + '/admin/settings/workflow-definitions');
+  const options = await prisma.storeTypes.findMany();
+  const form = await superValidate(
+    {
+      id: data.Id,
+      name: data.Name,
+      storeType: data.StoreTypeId!,
+      workflowType: data.Type,
+      workflowScheme: data.WorkflowScheme,
+      workflowBusinessFlow: data.WorkflowBusinessFlow,
+      description: data.Description,
+      properties: data.Properties,
+      enabled: data.Enabled
+    },
+    valibot(editSchema)
+  );
+  return { form, options };
+}) satisfies PageServerLoad;
+
+export const actions = {
+  async edit({ cookies, request }) {
+    const form = await superValidate(request, valibot(editSchema));
+    if (!form.valid) {
+      return fail(400, { form, ok: false, errors: form.errors });
+    }
+    try {
+      const {
+        id,
+        name,
+        description,
+        properties,
+        enabled,
+        storeType,
+        workflowBusinessFlow,
+        workflowScheme,
+        workflowType
+      } = form.data;
+      await prisma.workflowDefinitions.update({
+        where: {
+          Id: id
+        },
+        data: {
+          Type: workflowType,
+          Name: name,
+          WorkflowScheme: workflowScheme,
+          WorkflowBusinessFlow: workflowBusinessFlow,
+          StoreTypeId: storeType,
+          Description: description,
+          Properties: properties,
+          Enabled: enabled
+        }
+      });
+      return { ok: true, form };
+    } catch (e) {
+      if (e instanceof v.ValiError) return { form, ok: false, errors: e.issues };
+      throw e;
+    }
+  }
+} satisfies Actions;
