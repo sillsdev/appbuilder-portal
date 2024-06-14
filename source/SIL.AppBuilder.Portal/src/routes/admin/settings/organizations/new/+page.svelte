a8e4ed056a649a3a17c931acb8749a85841957e0
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-14 15:34:08 -0400

    Add errors to forms

--- source/SIL.AppBuilder.Portal/src/routes/admin/settings/organizations/new/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/admin/settings/organizations/new/+page.svelte
@@ -7,7 +7,7 @@
 
   export let data: PageData;
 
-  const { form, enhance, message } = superForm(data.form, {
+  const { form, enhance, allErrors } = superForm(data.form, {
     onUpdated(event) {
       if (event.form.valid) {
         goto('/admin/settings/organizations');
@@ -81,6 +81,16 @@
       </div>
     </label>
   </div>
+  {#if $allErrors.length}
+    <ul>
+      {#each $allErrors as error}
+        <li class="text-red-500">
+          <b>{error.path}:</b>
+          {error.messages.join('. ')}
+        </li>
+      {/each}
+    </ul>
+  {/if}
   <div class="my-4">
     <input type="submit" class="btn btn-primary" value="Submit" />
     <a class="btn" href="/admin/settings/organizations">Cancel</a>
