dc4ac39e89e38647ec8e1b2c6890d35729fbd3ba
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-19 09:56:17 -0400

    Implement working auth0 auth

---
+++ source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/(auth)/logout/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import { SignOut } from '@auth/sveltekit/components';
+</script>
+
+<div class="card bg-gray-900 p-4">
+  <!-- <h2 class="text-center">{$_('header.signOut')}</h2> -->
+  <SignOut signOutPage="logout" class="mx-auto">
+    <div slot="submitButton" class="btn w-full mx-auto">{$_('header.signOut')}</div>
+  </SignOut>
+</div>
