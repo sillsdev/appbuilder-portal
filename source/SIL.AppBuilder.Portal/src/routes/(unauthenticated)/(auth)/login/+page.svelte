59bfafae2929ca8ccec017221220e477004854a5
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-19 13:46:10 -0400

    Add ability to login with a new oauth session

--- source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
@@ -4,9 +4,17 @@
 </script>
 
 <div class="card bg-slate-50 dark:bg-slate-800 border p-4">
-  <h2 class="text-center">{$_('welcome')}</h2>
-  <SignIn provider="auth0" signInPage="login" class="mx-auto">
-    <div slot="submitButton" class="btn w-full mx-auto">Sign In with Auth0</div>
+  <h2 class="text-center m-4">{$_('welcome')}</h2>
+  <SignIn provider="auth0" signInPage="login" class="mx-auto my-4">
+    <div slot="submitButton" class="btn btn-primary w-full mx-auto">Sign In</div>
+  </SignIn>
+  <SignIn
+    provider="auth0"
+    signInPage="login"
+    class="mx-auto"
+    authorizationParams={{ prompt: 'login', screen_hint: 'signup' }}
+  >
+    <div slot="submitButton" class="btn w-full mx-auto">Sign In with new session</div>
   </SignIn>
 </div>
 <br />
