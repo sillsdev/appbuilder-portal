048ed59f4db5cdbb3e644be0a3ed71160155e2fd
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-06-28 12:47:46 -0400

    Opensource page

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/organizations/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/organizations/+page.svelte
@@ -6,32 +6,40 @@
   export let data: PageData;
 </script>
 
-<h1>{org_settingsTitle()}</h1>
-<div class="w-full px-4">
-  <table class="w-full">
-    <thead>
-      <tr class="text-left">
-        <th>Organization</th>
-        <th>Owner</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each data.organizations as org}
-        <tr
-          class="h-16 border-y"
-          on:click={() => goto('/organizations/' + org.Id + '/settings/info')}
-        >
-          <td>
-            <img class="inline-block p-2 h-16 w-16" src={org.LogoUrl} alt="Logo" />
-            <span>
-              {org.Name}
-            </span>
-          </td>
-          <td>
-            {org.Owner.Name}
-          </td>
+<div class="w-full max-w-6xl mx-auto">
+  <h1 class="pl-7">{org_settingsTitle()}</h1>
+  <div class="w-full px-4">
+    <table class="w-full">
+      <thead>
+        <tr class="text-left">
+          <th>Organization</th>
+          <th>Owner</th>
         </tr>
-      {/each}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {#each data.organizations as org}
+          <tr
+            class="h-16 border-y hover:bg-gray-100 hover:dark:bg-gray-800"
+            on:click={() => goto('/organizations/' + org.Id + '/settings/info')}
+          >
+            <td>
+              {#if org.LogoUrl}
+                <img class="inline-block p-2 h-16 w-16" src={org.LogoUrl} alt="Logo" />
+              {:else}
+                <div class="inline-block p-2 h-16 w-16 align-middle">
+                  <div class="bg-white w-full h-full" />
+                </div>
+              {/if}
+              <span>
+                {org.Name}
+              </span>
+            </td>
+            <td>
+              {org.Owner.Name}
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
