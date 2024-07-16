597917ddffd7e46144e70b90d76cfe9f07f20768
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-07-16 09:37:26 -0400

    Restyle login page

--- portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
+++ portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
@@ -1,30 +1,41 @@
 <script>
+  import ScriptoriaIcon from '$lib/icons/ScriptoriaIcon.svelte';
+  import * as m from '$lib/paraglide/messages';
   import { SignIn } from '@auth/sveltekit/components';
-  import * as m from "$lib/paraglide/messages";
 </script>
 
-<div class="card bg-slate-50 dark:bg-slate-800 border p-4">
-  <h2 class="text-center m-4">{m.welcome()}</h2>
-  <SignIn provider="auth0" signInPage="login" class="mx-auto my-4">
-    <div slot="submitButton" class="btn btn-primary w-full mx-auto">Sign In</div>
-  </SignIn>
-  <SignIn
-    provider="auth0"
-    signInPage="login"
-    class="mx-auto"
-    authorizationParams={{ prompt: 'login', screen_hint: 'signup' }}
-  >
-    <div slot="submitButton" class="btn w-full mx-auto">Sign In with new session</div>
-  </SignIn>
+<div class="card shadow-xl bg-base-100 border p-4">
+  <div class="w-full flex justify-center">
+    <div class="w-10" />
+    <ScriptoriaIcon size="128" />
+  </div>
+  <h1 class="text-center mx-4 mt-0 pt-2">{m.welcome()}</h1>
+  <div class="flex flex-row justify-evenly">
+    <SignIn provider="auth0" signInPage="login" class="inline-block">
+      <div slot="submitButton" class="btn btn-primary w-full mx-auto">Sign In</div>
+    </SignIn>
+    <SignIn
+      provider="auth0"
+      signInPage="login"
+      class="inline-block"
+      authorizationParams={{ prompt: 'login', screen_hint: 'signup' }}
+    >
+      <div slot="submitButton" class="btn w-full mx-auto btn-secondary">
+        Sign In with new session
+      </div>
+    </SignIn>
+  </div>
 </div>
 <br />
-<img
-  src="https://cdn.auth0.com/oss/badges/a0-badge-light.png"
-  alt="Auth0 badge"
-  class="w-1/2 mx-auto dark:hidden"
-/>
-<img
-  src="https://cdn.auth0.com/oss/badges/a0-badge-dark.png"
-  alt="Auth0 badge"
-  class="w-1/2 mx-auto hidden dark:block"
-/>
+<a href="https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss">
+  <img
+    src="https://cdn.auth0.com/oss/badges/a0-badge-light.png"
+    alt="Auth0 badge"
+    class="w-1/2 mx-auto dark:hidden shadow-xl rounded-xl"
+  />
+  <img
+    src="https://cdn.auth0.com/oss/badges/a0-badge-dark.png"
+    alt="Auth0 badge"
+    class="w-1/2 mx-auto hidden dark:block shadow-xl rounded"
+  />
+</a>
