a907c146779837830231a0b50b88c779d5526ed5
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-07-17 20:25:06 -0400

    Begin Project Directory

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/directory/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/directory/+page.server.ts
@@ -1,16 +1,14 @@
 import prisma, { getOrganizationsForUser } from '$lib/prisma';
 import { error } from '@sveltejs/kit';
-import { pruneProjects } from '../common';
+import { pruneProjects } from '../projects/common';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params, locals }) => {
-  const selector = params.filter as 'all' | 'own';
+export const load = (async ({ locals }) => {
   const userId = (await locals.auth())?.user.userId;
   if (!userId) return error(402);
   const orgs = await getOrganizationsForUser(userId);
   const projects = await prisma.projects.findMany({
     where: {
-      OwnerId: selector === 'all' ? undefined : userId,
       OrganizationId: {
         in: orgs.map((org) => org.Id)
       }
