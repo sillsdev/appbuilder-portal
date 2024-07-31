144d67e4a7ba3cad282cdeb7939420b5ec1057ca
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-07-31 10:40:14 -0400

    Restyle checkboxes

---
+++ source/SIL.AppBuilder.Portal/src/lib/components/settings/MultiselectBox.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let header: string;
+</script>
+
+<div class="border border-opacity-15 border-gray-50 rounded-lg p-2">
+  <div>
+    <span>{header}</span>
+  </div>
+  <slot />
+</div>
