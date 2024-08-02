9e25ad07a4dbdae67ebb954558a41b50bf9e2fa7
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-08-01 23:54:17 -0400

    Start user settings page

---
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/users/[id=idNumber]/settings/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+  import { page } from '$app/stores';
+  import TabbedMenu from '$lib/components/settings/TabbedMenu.svelte';
+  import * as m from '$lib/paraglide/messages';
+  import type { LayoutData } from './$types';
+
+  let userSettingsLinks = [
+    { text: m.users_userProfile(), route: 'profile' },
+    { text: m.users_userRoles(), route: 'roles' },
+    { text: m.users_userGroups(), route: 'groups' }
+  ];
+
+  export let data: LayoutData;
+  // TODO: route must be permission protected
+</script>
+
+<TabbedMenu
+  routeId="/(authenticated)/users/[id=idNumber]/settings"
+  base="{base}/users/{$page.params.id}/settings"
+  title={m.users_settingsTitle() + ': ' + data.username}
+  menuItems={userSettingsLinks}
+>
+  <slot />
+</TabbedMenu>
