40e5bc57c68cd6cdb08dbf873b1e06189ea5377b
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-08-10 17:12:31 -0400

    User groups

---
+++ portal/src/routes/(authenticated)/users/[id=idNumber]/settings/groups/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages';
+  import { superForm } from 'sveltekit-superforms';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+  const { form, enhance } = superForm(data.form, {
+    dataType: 'json',
+    resetForm: false
+  });
+</script>
+
+<form action="" method="post" use:enhance>
+  <div class="flex flex-col pr-4">
+    {#each $form.organizations as org}
+      <h3>{org.name}</h3>
+      <!-- TODO i18n -->
+      <div class="flex w-full">
+        <div class="shrink space-y-2">
+          {#each data.groups.filter((g) => g.orgId === org.id) as group}
+            <div class="flex space-x-2">
+              <input
+                type="checkbox"
+                class="toggle toggle-info"
+                value={group.id}
+                bind:group={org.groups}
+              />
+              <span>{group.name}</span>
+            </div>
+          {/each}
+        </div>
+        <div class="grow" />
+      </div>
+    {/each}
+    <div class="flex my-2">
+      <button type="submit" class="btn btn-primary">{m.common_save()}</button>
+    </div>
+  </div>
+</form>
