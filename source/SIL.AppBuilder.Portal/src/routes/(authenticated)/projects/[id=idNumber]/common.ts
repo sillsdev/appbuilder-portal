43d4c7c9b33859ef2709f2438d7d389248c86434
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-08-12 10:52:01 -0400

    Implement project toggles Add authorization checks to settings changes

---
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/projects/[id=idNumber]/common.ts
@@ -0,0 +1,22 @@
+import type { Session } from '@auth/sveltekit';
+import { prisma } from 'sil.appbuilder.portal.common';
+import { RoleId } from 'sil.appbuilder.portal.common/prismaTypes';
+
+export async function verifyCanView(user: Session, projectId: number) {
+  // Editing is allowed if the user owns the project, or if the user is an organization
+  // admin for the project's organization, or if the user is a super admin
+  const orgId = await prisma.projects.findUnique({
+    where: {
+      Id: projectId
+    }
+  });
+  if (orgId?.OwnerId === user.user.userId) return true;
+  if (
+    user.user.roles.find(
+      (r) =>
+        r[1] === RoleId.SuperAdmin || (r[1] === RoleId.OrgAdmin && r[0] === orgId?.OrganizationId)
+    )
+  )
+    return true;
+  return false;
+}
