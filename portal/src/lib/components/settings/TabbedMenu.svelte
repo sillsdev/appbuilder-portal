51606ca53eaa269b4cfdce3758b6b82a28e90afe
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-08-20 13:43:52 -0400

    Phone responsiveness

--- portal/src/lib/components/settings/TabbedMenu.svelte
+++ portal/src/lib/components/settings/TabbedMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import IconContainer from '../IconContainer.svelte';
 
   export let menuItems: {
     route: string;
@@ -16,12 +17,36 @@
 </script>
 
 <div class="w-full max-w-6xl mx-auto">
-  <div class="flex flex-row">
-    <div class="p-4 pr-0 sticky top-0 self-start">
+  <div class="flex sm:flex-row flex-col">
+    <div class="p-4 sm:pr-0 sm:sticky top-0 sm:self-start">
       <slot name="title">
         <h1 class="p-4" class:text-nowrap={!allowTitleWrap}>{title}</h1>
       </slot>
-      <ul class="menu p-0 rounded border border-slate-600">
+      <div class="rounded border-slate-600 bg-base-200 mx-auto sm:hidden">
+        <div class="slidedown p-3" role="button" tabindex="0">
+          <div class="flex place-content-between">
+            <span>
+              {menuItems.find((item) =>
+                $page.route.id?.split(routeId + '/')[1].startsWith(item.route)
+              )?.text}
+            </span>
+            <IconContainer icon="gridicons:dropdown" width="24" />
+          </div>
+        </div>
+        <div class="relative h-0">
+          <div class="slidedown-content w-full z-10">
+            {#each menuItems as item}
+              <a
+                class="bg-base-200 border-t border-slate-600 p-3 w-full block"
+                href="{base}/{item.route}"
+              >
+                {item.text}
+              </a>
+            {/each}
+          </div>
+        </div>
+      </div>
+      <ul class="menu p-0 rounded border border-slate-600 sm:flex hidden">
         {#key $page.route.id}
           {#each menuItems as item}
             <li class="w-60 border-t border-slate-600 w-full [top:-1px]">
@@ -37,10 +62,26 @@
         {/key}
       </ul>
     </div>
-    <div class="flex grow mt-16">
+    <div class="flex grow sm:mt-16">
       <div class="flex grow flex-col">
         <slot />
       </div>
     </div>
   </div>
 </div>
+
+<style>
+  .slidedown {
+    position: relative;
+  }
+  .slidedown:focus ~ .relative > .slidedown-content {
+    transform: none;
+  }
+  .slidedown-content {
+    transform-origin: top;
+    transform: scaleY(0);
+    transition: transform 0.5s;
+    position: absolute;
+    overflow: hidden;
+  }
+</style>
