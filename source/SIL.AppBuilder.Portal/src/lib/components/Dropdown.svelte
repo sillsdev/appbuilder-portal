e3f91f008c94ef07c5cdb9c9ec607c43adda0f3d
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-08-20 15:10:33 -0400

    Use role="button" for dropdown divs

--- source/SIL.AppBuilder.Portal/src/lib/components/Dropdown.svelte
+++ source/SIL.AppBuilder.Portal/src/lib/components/Dropdown.svelte
@@ -9,18 +9,19 @@ A simple dropdown menu from DaisyUI.
 </script>
 
 <div class="dropdown">
+  <!-- When .dropdown is focused, .dropdown-content is revealed making this actually interactive -->
   <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
   <label tabindex="0" class="btn btn-ghost p-0.5 no-animation flex-nowrap">
     <slot name="label" />
+    <div
+      role="button"
+      tabindex="0"
+      class="dropdown-content menu drop-shadow-lg mt-2.5 bg-base-100 z-10"
+      class:min-w-[21rem]={cols == 6}
+      class:min-w-[17.25rem]={cols == 5}
+      on:blur={() => dispatch('nav-end')}
+    >
+      <slot name="content" />
+    </div>
   </label>
-  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-  <div
-    tabindex="0"
-    class="dropdown-content menu drop-shadow-lg mt-2.5 bg-base-100 z-10"
-    class:min-w-[21rem]={cols == 6}
-    class:min-w-[17.25rem]={cols == 5}
-    on:blur={() => dispatch('nav-end')}
-  >
-    <slot name="content" />
-  </div>
 </div>
