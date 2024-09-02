e10b985494e54d1591e3b5cab9ac9572b1fc3ebb
Aidan Jones <aejones4gm@gmail.com>
2024-09-02 11:26:15 -0400

    Add comments noting which database field

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/+page.server.ts
@@ -2,18 +2,19 @@ import type { PageServerLoad } from './$types';
 import { prisma } from 'sil.appbuilder.portal.common';
 
 type Fields = {
-  user?: string;
-  email?: string;
-  name: string;
-  description: string;
-  store?: string;
-  listingLanguage?: string;
-  projectURL?: string;
-  product?: string;
-  appType?: string;
-  langCode?: string;
+  user?: string;            //Product.Project.Owner.Name
+  email?: string;           //Product.Project.Owner.Email
+  name: string;             //Product.Project.Name
+  description: string;      //Product.Project.Description
+  store?: string;           //Product.Store.Name
+  listingLanguage?: string; //Product.StoreLanguage.Name
+  projectURL?: string;      //Product.Project.WorkflowAppProjectURL?
+  product?: string;         //Product.ProductDefinition.Name?
+  appType?: string;         //Product.ProductDefinition.ApplicationTypes.Name
+  langCode?: string;        //Product.Project.Language?
 }
 
+//replace with Product.ProductArtifacts
 type File = {
   BuildId: number;
   Type: string;
@@ -22,6 +23,7 @@ type File = {
   Id: number;
 };
 
+//replace with Product.Project.Reviewers
 type User = {
   Id: number;
   Name: string;
