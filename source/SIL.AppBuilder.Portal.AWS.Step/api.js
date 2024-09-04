246964871a524ca9602b639900e8081a87cc8f67
Aidan Jones <aejones4gm@gmail.com>
2024-09-04 15:13:32 -0400

    Beginnings of Callback flow

--- source/SIL.AppBuilder.Portal.AWS.Step/api.js
+++ source/SIL.AppBuilder.Portal.AWS.Step/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('node:fs');
 
-const { SFNClient, ListStateMachinesCommand } = require("@aws-sdk/client-sfn"); // CommonJS import
+const { 
+  SFNClient, 
+  CreateStateMachineCommand,
+  DescribeStateMachineCommand,
+  ListStateMachinesCommand,
+  StartExecutionCommand
+} = require("@aws-sdk/client-sfn"); // CommonJS import
 
 const client = new SFNClient({
   endpoint: `http://localhost:8083`,
@@ -12,6 +19,8 @@ const client = new SFNClient({
     secretAccessKey: 'dummy'
   }
 });
+
+const machines = {};
 
 const app = express();
 
@@ -31,15 +40,66 @@ app.post('/echo', (req, res) => {
   console.log(req.body);
 });
 
-app.get('/machines', async (req, res) => {
+async function listMachines() {
   try {
-    const response = await client.send(new ListStateMachinesCommand());
-    res.send(response);
+    return await client.send(new ListStateMachinesCommand());
   }
   catch (e) {
     console.log(e);
-    res.send({error: e});
+    return {error: e};
   }  
-})
+}
+
+app.get('/machines', async (req, res) => {
+  res.send(await listMachines());
+});
+
+app.get('/machines/:name', async (req, res) => {
+  const name = req.params.name;
+
+  if (machines[name]) {
+    res.send(await client.send(new DescribeStateMachineCommand({stateMachineArn: machines[name]})));
+    return;
+  }
+
+  // Sending 404 when not found something is a good practice
+  res.status(404).send('State Machine not found');
+});
+
+app.get('/flow/start/:name', async (req, res) => {
+  const name = req.params.name;
+
+  if (!machines[name]) {
+    res.status(404).send('State Machine not found');
+    return;
+  }
+
+  res.send(await client.send(new StartExecutionCommand({stateMachineArn: machines[name]})));
+});
+
+app.post('/token', (req, res) => {
+  console.log(req.body);
+});
+
+const creation = setInterval(async () => {
+  const res = await listMachines();
+  if (!res.error) {
+    console.log("Creating State Machine");
+    fs.readFile("flow.asl.json", (err, data) => {
+      client.send(new CreateStateMachineCommand({
+        name: "workflow",
+        definition: data.toString('utf8'),
+        roleArn: "arn:aws:iam::128716708097:role/StepFunctionsLambdaRole"
+      })).then((r) => {
+        const key = r.stateMachineArn.split(":").pop();
+        machines[key] = r.stateMachineArn;
+        console.log(machines);
+      }).catch((e) => {
+        console.log(e);
+      })
+    });
+    clearInterval(creation);
+  }
+}, 5000);
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
