e124bf5b1d25a590d55689a10c04ac661675a0dc
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-09-12 14:48:10 -0400

    Add per-route auth framework

--- source/SIL.AppBuilder.Portal/src/auth.ts
+++ source/SIL.AppBuilder.Portal/src/auth.ts
@@ -1,8 +1,10 @@
+import type { Session } from '@auth/express';
 import { SvelteKitAuth, type DefaultSession, type SvelteKitAuthConfig } from '@auth/sveltekit';
 import Auth0Provider from '@auth/sveltekit/providers/auth0';
-import { redirect, type Handle } from '@sveltejs/kit';
+import { error, redirect, type Handle } from '@sveltejs/kit';
 import { DatabaseWrites, prisma } from 'sil.appbuilder.portal.common';
 import { RoleId } from 'sil.appbuilder.portal.common/prisma';
+import { verifyCanView } from './routes/(authenticated)/projects/[id=idNumber]/common';
 
 declare module '@auth/sveltekit' {
   interface Session {
@@ -75,6 +77,66 @@ export const localRouteHandle: Handle = async ({ event, resolve }) => {
   ) {
     const session = await event.locals.auth();
     if (!session) return redirect(302, '/login');
+    if (!(await validateRouteForAuthenticatedUser(session, event.route.id, event.params)))
+      return error(403);
   }
   return resolve(event);
 };
+
+async function validateRouteForAuthenticatedUser(
+  session: Session,
+  route: string,
+  params: Partial<Record<string, string>>
+): Promise<boolean> {
+  const path = route.split('/').filter((r) => !!r);
+  // Only guarding authenticated routes
+  if (path[0] === '(authenticated)') {
+    if (path[1] === 'admin') return !!session.user.roles.find((r) => r[1] === RoleId.SuperAdmin);
+    else if (path[1] === 'directory' || path[1] === 'open-source')
+      // Always allowed. Open pages
+      return true;
+    else if (path[1] === 'organizations') {
+      // Must be org admin or super admin for some organization
+      if (!session.user.roles.find((r) => r[1] === RoleId.SuperAdmin || r[1] === RoleId.OrgAdmin))
+        return false;
+      // Must be org admin or super admin for this organization
+      if (params.id)
+        return !!session.user.roles.find(
+          (r) =>
+            r[1] === RoleId.SuperAdmin ||
+            (r[0] === parseInt(params.id!) && r[1] === RoleId.OrgAdmin)
+        );
+      return true;
+    } else if (path[1] === 'products') {
+      // TODO not sure, probably based on ownership of the project
+      const projectId = (
+        await prisma.products.findFirst({
+          where: {
+            Id: params.id
+          }
+        })
+      )?.ProjectId;
+      if (!projectId) return false;
+      return verifyCanView(session, projectId);
+    } else if (path[1] === 'projects') {
+      if (path[2] === '[filter=projectSelector]') return true;
+      // TODO: what are the conditions for viewing a project
+      // I imagine either own it or be organization admin
+      else if (path[2] === '[id=idNumber]') {
+        return await verifyCanView(session, parseInt(params.id!));
+      }
+      return true;
+    } else if (path[1] === 'tasks') {
+      // Own task list always allowed, and specific products checked manually
+      return true;
+    } else if (path[1] === 'users') {
+      // Checked manually in the users route
+      return true;
+    } else {
+      // Unknown route. We'll assume it's a legal route
+      return true;
+    }
+  } else {
+    return true;
+  }
+}
