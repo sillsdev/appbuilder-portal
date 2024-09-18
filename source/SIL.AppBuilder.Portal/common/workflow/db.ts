67761ef4c8273fffef093354235edc54af336066
Aidan Jones <aejones4gm@gmail.com>
2024-09-18 16:00:18 -0400

    Update UserTasks from state machine

--- source/SIL.AppBuilder.Portal/common/workflow/db.ts
+++ source/SIL.AppBuilder.Portal/common/workflow/db.ts
@@ -2,6 +2,7 @@ import DatabaseWrites from '../databaseProxy/index.js';
 import { prisma } from '../index.js';
 import { WorkflowContext } from '../public/workflow.js';
 import { AnyStateMachine } from 'xstate';
+import { RoleId } from '../public/prisma.js';
 
 export type Snapshot = {
   value: string;
@@ -37,4 +38,84 @@ export async function getSnapshot(productId: string, machine: AnyStateMachine) {
     return machine.resolveState(snap);
   }
   return undefined;
+}
+
+/**
+ * Delete all tasks for a product.
+ * Then create new tasks based on the provided user roles:
+ * - OrgAdmin for administrative tasks (Product.Project.Organization.UserRoles.User where Role === OrgAdmin)
+ * - AppBuilder for project owner tasks (Product.Project.Owner)
+ * - Author for author tasks (Product.Project.Authors)
+ */
+export async function updateUserTasks(
+  productId: string,
+  roles: RoleId[],
+  state: string,
+  comment?: string
+) {
+  // Delete all tasks for a product
+  await DatabaseWrites.userTasks.deleteMany({
+    where: {
+      ProductId: productId
+    }
+  });
+
+  const product = await prisma.products.findUnique({
+    where: {
+      Id: productId
+    },
+    select: {
+      Project: {
+        select: {
+          Organization: {
+            select: {
+              UserRoles: {
+                where: {
+                  RoleId: RoleId.OrgAdmin
+                },
+                select: {
+                  UserId: true
+                }
+              }
+            }
+          },
+          OwnerId: true,
+          Authors: {
+            select: {
+              UserId: true
+            }
+          }
+        }
+      }
+    }
+  });
+
+  const uids = roles
+    .map((r) => {
+      switch (r) {
+        case RoleId.OrgAdmin:
+          return product.Project.Organization.UserRoles.map((u) => u.UserId);
+        case RoleId.AppBuilder:
+          return [product.Project.OwnerId];
+        case RoleId.Author:
+          return product.Project.Authors.map((a) => a.UserId);
+        default:
+          break;
+      }
+    })
+    .reduce((p, c) => p.concat(c), []);
+
+  const timestamp = new Date();
+
+  return DatabaseWrites.userTasks.createMany({
+    data: uids.map((u) => ({
+      UserId: u,
+      ProductId: productId,
+      ActivityName: state,
+      Status: state,
+      Comment: comment,
+      DateCreated: timestamp,
+      DateUpdated: timestamp
+    }))
+  });
 }
