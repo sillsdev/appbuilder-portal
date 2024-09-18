f108e61604c40f59b261590236aa2f795a90fb7d
Aidan Jones <aejones4gm@gmail.com>
2024-09-18 09:55:18 -0400

    Move workflow to server-only

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/workflows/[product_id]/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/workflows/[product_id]/+page.server.ts
@@ -1,7 +1,26 @@
-import { prisma } from 'sil.appbuilder.portal.common';
-import type { PageServerLoad } from './$types';
+import { prisma, NoAdminS3 } from 'sil.appbuilder.portal.common';
+import { transform } from 'sil.appbuilder.portal.common/workflow';
+import { createActor, type Snapshot } from 'xstate';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async ({ params, url, locals }) => {
+  const actor = createActor(NoAdminS3, {
+    snapshot:
+      (JSON.parse(
+        (
+          await prisma.workflowInstances.findUnique({
+            where: {
+              ProductId: params.product_id
+            },
+            select: {
+              Snapshot: true
+            }
+          })
+        )?.Snapshot || 'null'
+      ) as Snapshot<unknown>) ?? undefined,
+    input: {}
+  });
+
   const instance = await prisma.workflowInstances.findUnique({
     where: {
       ProductId: params.product_id
@@ -32,9 +51,24 @@ export const load: PageServerLoad = async ({ params, url, locals }) => {
             take: 1
           }
         }
-      },
-      Snapshot: true
+      }
     }
   });
-  return { instance };
+
+  const snap = actor.getSnapshot();
+
+  return {
+    instance: instance,
+    snapshot: { value: snap.value },
+    machine: transform(NoAdminS3.toJSON())
+  };
 };
+
+export const actions = {
+  default: async ({ request }) => {
+    // TODO: permission check
+    const data = await request.formData();
+
+    console.log(data.get('state'));
+  }
+} satisfies Actions;
