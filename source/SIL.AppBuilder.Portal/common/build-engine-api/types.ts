ad35026d727204fc345785716b137ef21bef2fcc
Aidan Jones <aejones4gm@gmail.com>
2024-09-30 11:55:27 -0400

    Methods to interact with BuildEngine

---
+++ source/SIL.AppBuilder.Portal/common/build-engine-api/types.ts
@@ -0,0 +1,105 @@
+export type Response =
+  | ErrorResponse
+  | ProjectResponse
+  | TokenResponse
+  | BuildResponse
+  | ReleaseResponse;
+
+export type ErrorResponse = {
+  responseType: 'error';
+  name: string;
+  message: string;
+  code: number;
+  status: number;
+  type: string;
+};
+
+type SuccessResponse = {
+  responseType: 'project' | 'token' | 'job' | 'build' | 'release';
+  id: number;
+  created: Date;
+  updated: Date;
+  _links: {
+    self?: {
+      href: string;
+    };
+    job?: {
+      href: string;
+    };
+  };
+};
+
+export type ProjectConfig = {
+  user_id: string;
+  group_id: string;
+  app_id: string;
+  project_name: string;
+  language_code: string;
+  publishing_key: string;
+  storage_type: string;
+};
+export type ProjectResponse = SuccessResponse &
+  ProjectConfig & {
+    responseType: 'project';
+    status: 'initialized' | 'accepted' | 'complete' | 'delete' | 'deleting';
+    result: 'SUCCESS' | 'FAILURE' | null;
+    error: string | null;
+    url: string;
+  };
+
+export type TokenConfig = {
+  name: string;
+};
+export type TokenResponse = {
+  responseType: 'token';
+  session_token: string;
+  secret_access_key: string;
+  access_key_id: string;
+  expiration: string;
+  region: string;
+};
+
+export type JobConfig = {
+  request_id: string;
+  git_url: string;
+  app_id: string;
+  publisher_id: string;
+};
+export type JobResponse = SuccessResponse &
+  JobConfig & {
+    responseType: 'job';
+  };
+
+type BuildCommon = {
+  targets: string;
+};
+export type BuildConfig = BuildCommon & {
+  environment: { [key: string]: string };
+};
+export type BuildResponse = SuccessResponse &
+  BuildCommon & {
+    responseType: 'build';
+    job_id: number;
+    status: 'initialized' | 'accepted' | 'active' | 'expired' | 'postprocessing' | 'completed';
+    result: 'SUCCESS' | 'FAILURE' | 'ABORTED' | null;
+    error: string | null;
+    artifacts: { [key: string]: string };
+  };
+
+type ReleaseCommon = {
+  channel: 'production' | 'beta' | 'alpha';
+  targets: string;
+};
+export type ReleaseConfig = ReleaseCommon & {
+  environment: { [key: string]: string };
+};
+export type ReleaseResponse = SuccessResponse &
+  ReleaseCommon & {
+    responseType: 'release';
+    buildId: number;
+    status: 'initialized' | 'accepted' | 'active' | 'expired' | 'completed' | 'postprocessing';
+    result: 'SUCCESS' | 'FAILURE' | 'EXCEPTION' | null;
+    error: string | null;
+    console_text: string;
+    artifacts: { [key: string]: string };
+  };
