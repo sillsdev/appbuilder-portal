59debe5c96ab2ce5b3d0bc39f79ff52d114d0f69
Aidan Jones <aejones4gm@gmail.com>
2024-09-30 11:55:27 -0400

    Methods to interact with BuildEngine

--- source/SIL.AppBuilder.Portal/common/index.ts
+++ source/SIL.AppBuilder.Portal/common/index.ts
@@ -3,3 +3,4 @@ export { scriptoriaQueue } from './bullmq.js';
 export { default as DatabaseWrites } from './databaseProxy/index.js';
 export { readonlyPrisma as prisma } from './prisma.js';
 export { Workflow } from './workflow/index.js';
+export * as BuildEngine from './build-engine-api/index.js';
