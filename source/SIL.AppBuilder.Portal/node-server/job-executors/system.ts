0ad81a3ec6c467cd05457b058c081a44fcd4a2a2
Aidan Jones <aejones4gm@gmail.com>
2024-10-03 11:20:31 -0400

    Add stub for recurring system status check job

---
+++ source/SIL.AppBuilder.Portal/node-server/job-executors/system.ts
@@ -0,0 +1,16 @@
+import { Job } from 'bullmq';
+import { BullMQ, prisma } from 'sil.appbuilder.portal.common';
+
+export async function checkStatuses(job: Job<BullMQ.System.CheckStatuses>): Promise<unknown> {
+  // TODO: Do I use the `SystemStatus` table? Why does this table even exist? It mostly duplicates data from `Organizations` but is completely disconnected from `Organizations`. Can these be consolidated?
+  const systems = await prisma.systemStatuses.findMany();
+  job.updateProgress(10);
+  //const timestamp = new Date();
+  systems.forEach((s, i) => {
+    // TODO: Not doing anything here until above TODO is resolved
+    job.updateProgress(10 + ((i + 1) * 80) / systems.length);
+  });
+  //await prisma.$transaction(systems.map());
+  job.updateProgress(100);
+  return systems.length;
+}
