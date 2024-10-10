57c041614c994f3bd0487ecc2ae73d118057310b
Aidan Jones <aejones4gm@gmail.com>
2024-10-10 14:18:07 -0400

    Add some permission checks

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-definitions/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-definitions/+page.svelte
@@ -3,13 +3,16 @@
   import DataDisplayBox from '$lib/components/settings/DataDisplayBox.svelte';
   import * as m from '$lib/paraglide/messages';
   import type { PageData } from './$types';
+  import { RoleId } from 'sil.appbuilder.portal.common/prisma';
 
   export let data: PageData;
 </script>
 
+{#if data.session?.user.roles.find((role) => role[1] === RoleId.SuperAdmin)}
 <a href="/admin/workflows" class="btn btn-outline m-4 mt-0">
   {m.admin_workflowInstances_title()}
 </a>
+{/if}
 
 <a href="workflow-definitions/new" class="btn btn-outline m-4 mt-0">
   {m.admin_settings_workflowDefinitions_add()}
