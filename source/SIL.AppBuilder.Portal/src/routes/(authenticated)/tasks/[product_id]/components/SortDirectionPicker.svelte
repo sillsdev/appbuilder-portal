4106ba256ea70bc3a928fba4f30d5f84ce2aef7e
Aidan Jones <aejones4gm@gmail.com>
2024-10-10 15:35:26 -0400

    Make sort direction picker less visually jarring
    Fixed width bug
    Couldn't fix vertical position bug, so made it look less jarring by adding transparency.

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/components/SortDirectionPicker.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/components/SortDirectionPicker.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
-  import { ArrowDownIcon, ArrowUpIcon } from "$lib/icons";
+  import { ArrowDownIcon, ArrowUpIcon } from '$lib/icons';
 
   export let active: boolean;
   export let descending: boolean;
-
-  // TODO: selecting one of these causes the relative sizes of the table headers to change, which looks really janky. I've tried fixing this, but have been unsuccessful. For now, at least it functions correctly, even if it doesn't look quite right.
 </script>
 
 <div class="form-control">
   <label>
-    <span class="{active? "":"hidden"}">
-      {#if descending}
-      <ArrowDownIcon />
+    <span class="label-text {active ? 'active' : ''}"><slot /></span>
+    <div class="arrow">
+      {#if active}
+        {#if descending}
+          <ArrowDownIcon />
+        {:else}
+          <ArrowUpIcon />
+        {/if}
       {:else}
-      <ArrowUpIcon />
+        &nbsp;
       {/if}
-    </span>
-    <span class="label-text"><slot></slot></span>
+    </div>
   </label>
 </div>
 
-<style lang=postcss>
+<style lang="postcss">
   label {
     @apply select-none cursor-pointer;
+    min-height: 24px;
+  }
+  .label-text {
+    display: inline-block;
+    position: relative;
+    bottom: 0;
+    height: 100%;
+  }
+  .arrow {
+    display: inline-block;
+    width: 24px;
+  }
+  .active {
+    @apply text-neutral-content text-opacity-75;
   }
 </style>
