a6a5c6fa2b0c8ac92fd84b38ad36dccdb1b378cd
Aidan Jones <aejones4gm@gmail.com>
2024-10-10 11:34:05 -0400

    Add new queue

--- source/SIL.AppBuilder.Portal/common/index.ts
+++ source/SIL.AppBuilder.Portal/common/index.ts
@@ -1,5 +1,5 @@
 export * as BullMQ from './BullJobTypes.js';
-export { scriptoriaQueue } from './bullmq.js';
+export * as queues from './bullmq.js';
 export { default as DatabaseWrites } from './databaseProxy/index.js';
 export { readonlyPrisma as prisma } from './prisma.js';
 export { Workflow } from './workflow/index.js';
