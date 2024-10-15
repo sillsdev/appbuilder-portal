40aeba1908ce52faf53d4b92b1c2fc406f66b11c
Aidan Jones <aejones4gm@gmail.com>
2024-10-15 10:38:44 -0400

    Convert svelte-headless-table into TableWrapper component

---
+++ source/SIL.AppBuilder.Portal/src/lib/components/AnchorWrapper.svelte
@@ -0,0 +1,8 @@
+<!-- This was added solely so a link could be passed into TableWrapper -->
+<script lang="ts">
+  export let href: string;
+  export let content: string;
+  export let className: string = '';
+</script>
+
+<a href={href} class={className}>{content}</a>
--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/workflows/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/workflows/+page.server.ts
@@ -1,5 +1,10 @@
 import { prisma } from 'sil.appbuilder.portal.common';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
+import { tableSchema } from '$lib/table';
+import { superValidate, fail } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import { RoleId } from 'sil.appbuilder.portal.common/prisma';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async (event) => {
   const instances = await prisma.workflowInstances.findMany({
@@ -26,7 +31,133 @@ export const load: PageServerLoad = async (event) => {
           }
         }
       }
-    }
+    },
+    take: 20
   });
-  return { instances };
+
+  const count = await prisma.productTransitions.count();
+  return {
+    instances,
+    count,
+    form: await superValidate(
+      {
+        page: 0,
+        size: 20
+      },
+      valibot(tableSchema)
+    )
+  };
+};
+
+export const actions: Actions = {
+  page: async function ({ request, locals }) {
+    const session = await locals.auth();
+    if ((session?.user.roles.filter(([org, role]) => role === RoleId.SuperAdmin) ?? []).length < 1)
+      return error(403);
+    const form = await superValidate(request, valibot(tableSchema));
+    if (!form.valid) return fail(400, { form, ok: false });
+
+    const search = form.data.search.text
+      ? new RegExp(
+          // escape regex special characters: https://stackoverflow.com/a/6969486
+          form.data.search.text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), //
+          'i'
+        )
+      : null;
+
+    let instances = await prisma.workflowInstances.findMany({
+      orderBy: form.data.sort?.find((s) => s.field === 'product')
+        ? { ProductId: form.data.sort?.find((s) => s.field === 'product')?.direction }
+        : undefined,
+      select: {
+        Product: {
+          select: {
+            Id: true,
+            ProductTransitions: {
+              where: {
+                DateTransition: {
+                  not: null
+                }
+              },
+              select: {
+                DateTransition: true,
+                DestinationState: true
+              },
+              orderBy: [
+                {
+                  DateTransition: 'desc'
+                }
+              ],
+              take: 1
+            }
+          }
+        }
+      }
+    });
+
+    // I don't like that I have to do it this way, but there does not appear to be a good way to do this on the database side
+    if (search) {
+      if (!form.data.search.field) {
+        instances = instances.filter(
+          (i) =>
+            search.test(i.Product.Id) ||
+            search.test(i.Product.ProductTransitions[0].DestinationState ?? '')
+        );
+      } else if (form.data.search.field === 'product') {
+        instances = instances.filter((i) => search.test(i.Product.Id));
+      } else if (form.data.search.field === 'state') {
+        instances = instances.filter((i) =>
+          search.test(i.Product.ProductTransitions[0].DestinationState ?? '')
+        );
+      }
+    }
+
+    instances = form.data.sort?.find((s) => s.field === 'date')
+      ? instances.sort(
+          form.data.sort?.find((s) => s.field === 'date')?.direction === 'asc'
+            ? (a, b) => {
+                return (
+                  a.Product.ProductTransitions[0].DateTransition?.getTime()! -
+                  b.Product.ProductTransitions[0].DateTransition?.getTime()!
+                );
+              }
+            : (a, b) => {
+                return (
+                  b.Product.ProductTransitions[0].DateTransition?.getTime()! -
+                  a.Product.ProductTransitions[0].DateTransition?.getTime()!
+                );
+              }
+        )
+      : instances;
+
+    instances = form.data.sort?.find((s) => s.field === 'state')
+      ? instances.sort(
+          form.data.sort?.find((s) => s.field === 'state')?.direction === 'asc'
+            ? (a, b) => {
+                return a.Product.ProductTransitions[0].DestinationState!.localeCompare(
+                  b.Product.ProductTransitions[0].DestinationState!
+                );
+              }
+            : (a, b) => {
+                return b.Product.ProductTransitions[0].DestinationState!.localeCompare(
+                  a.Product.ProductTransitions[0].DestinationState!
+                );
+              }
+        )
+      : instances;
+
+    const count = instances.length;
+
+    return {
+      form,
+      ok: true,
+      query: {
+        data: instances.slice(
+          form.data.page * form.data.size,
+          (form.data.page + 1) * form.data.size
+        ),
+        count
+      }
+    };
+  }
 };
