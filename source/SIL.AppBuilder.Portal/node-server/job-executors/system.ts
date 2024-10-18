e3f99ed87e9a9c85d0822f321faf2a11c58439f5
Aidan Jones <aejones4gm@gmail.com>
2024-10-18 17:35:00 -0400

    SystemStatusCheck Job

--- source/SIL.AppBuilder.Portal/node-server/job-executors/system.ts
+++ source/SIL.AppBuilder.Portal/node-server/job-executors/system.ts
@@ -1,18 +1,84 @@
-import { BullMQ, prisma } from 'sil.appbuilder.portal.common';
+import { BullMQ, prisma, DatabaseWrites, BuildEngine } from 'sil.appbuilder.portal.common';
 import { Job } from 'bullmq';
 import { ScriptoriaJobExecutor } from './base.js';
 
 export class CheckStatuses extends ScriptoriaJobExecutor<BullMQ.ScriptoriaJobType.System_CheckStatuses> {
   async execute(job: Job<BullMQ.System.CheckStatuses, number, string>): Promise<number> {
-    // TODO: Do I use the `SystemStatus` table? Why does this table even exist? It mostly duplicates data from `Organizations` but is completely disconnected from `Organizations`. Can these be consolidated?
-    const systems = await prisma.systemStatuses.findMany();
-    job.updateProgress(10);
-    //const timestamp = new Date();
-    systems.forEach((s, i) => {
-      // TODO: Not doing anything here until above TODO is resolved
-      job.updateProgress(10 + ((i + 1) * 80) / systems.length);
+    const organizations = await prisma.organizations.findMany({
+      where: {
+        OR: [
+          {
+            UseDefaultBuildEngine: null
+          },
+          {
+            UseDefaultBuildEngine: false
+          }
+        ]
+      },
+      select: {
+        BuildEngineUrl: true,
+        BuildEngineApiAccessToken: true
+      }
     });
-    //await prisma.$transaction(systems.map());
+    // Add defaults
+    if (process.env.DEFAULT_BUILDENGINE_URL && process.env.DEFAULT_BUILDENGINE_API_ACCESS_TOKEN) {
+      organizations.push({
+        BuildEngineUrl: process.env.DEFAULT_BUILDENGINE_URL,
+        BuildEngineApiAccessToken: process.env.DEFAULT_BUILDENGINE_API_ACCESS_TOKEN
+      });
+    }
+    job.updateProgress(10);
+    // remove statuses that do not correspond to organizations
+    await DatabaseWrites.systemStatuses.deleteMany({
+      where: {
+        BuildEngineUrl: {
+          notIn: organizations.map((o) => o.BuildEngineUrl)
+        },
+        BuildEngineApiAccessToken: {
+          notIn: organizations.map((o) => o.BuildEngineApiAccessToken)
+        }
+      }
+    });
+    job.updateProgress(20);
+    const systems = await prisma.systemStatuses.findMany({
+      select: {
+        BuildEngineUrl: true,
+        BuildEngineApiAccessToken: true
+      }
+    });
+    const filteredOrgs = organizations.filter(
+      (o) =>
+        !systems.find(
+          (s) =>
+            s.BuildEngineUrl === o.BuildEngineUrl &&
+            s.BuildEngineApiAccessToken === o.BuildEngineApiAccessToken
+        )
+    );
+    job.updateProgress(30);
+    await DatabaseWrites.systemStatuses.createMany({
+      data: filteredOrgs.map((o) => ({ ...o, SystemAvailable: false, DateCreated: new Date() }))
+    });
+    job.updateProgress(50);
+    await Promise.all(
+      (
+        await prisma.systemStatuses.findMany()
+      ).map(async (s) => {
+        const res = await BuildEngine.Requests.systemCheck({
+          type: 'provided',
+          url: s.BuildEngineUrl,
+          token: s.BuildEngineApiAccessToken
+        });
+        await DatabaseWrites.systemStatuses.update({
+          where: {
+            Id: s.Id
+          },
+          data: {
+            SystemAvailable: res.status === 200,
+            DateUpdated: new Date()
+          }
+        })
+      })
+    );
     job.updateProgress(100);
     return systems.length;
   }
