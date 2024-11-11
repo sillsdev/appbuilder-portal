f846520f27eca2fc2483d03d259888e695181170
Aidan Jones <aejones4gm@gmail.com>
2024-11-11 17:01:33 -0500

    Improved UI for WorkflowDefinition Creation/Editing (#1029)
    * Edit workflow definition
    
    * Break common elements of WorkflowDefinition to base valibot schema
    
    * Replicate edit process for WorkflowDefinition creation
    
    * Add dropdown select for WorkflowScheme
    
    I know/assume we will eventually be getting rid of this field entirely, but adding this for now because it is a nice feature.
    
    * Also add dropdown for WorkflowBusinessFlow
    
    * Switch options to toggle, add warning color
    
    * Add separation and coloring to options
    
    * Size inputs to be mobile-friendly
    
    * Add i18n for workflow definitions

--- portal/src/lib/components/settings/LabeledFormInput.svelte
+++ portal/src/lib/components/settings/LabeledFormInput.svelte
@@ -7,9 +7,10 @@
   import type { ValidI13nKey } from '$lib/i18n';
   import * as m from '$lib/paraglide/messages';
   export let name: ValidI13nKey;
+  export let className: string = '';
 </script>
 
-<div>
+<div class={className}>
   <label>
     <div class="label">
       <span class="label-text">
