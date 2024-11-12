4587d9f8ecf0cb535a8fc34f1bc008bd206f08d9
Aidan Jones <aejones4gm@gmail.com>
2024-11-12 11:08:35 -0500

    Break search bar into component

---
+++ source/SIL.AppBuilder.Portal/src/lib/components/SearchBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import * as m from '../paraglide/messages';
+  import IconContainer from './IconContainer.svelte';
+
+  export let className: string = '';
+  export let value: string;
+</script>
+
+<div class="input input-bordered flex items-center gap-2 {className}">
+  <input type="text" placeholder={m.search()} class="flex-grow" bind:value={value} />
+  <IconContainer icon="mdi:search" class="ml-auto" width={24} />
+</div>
