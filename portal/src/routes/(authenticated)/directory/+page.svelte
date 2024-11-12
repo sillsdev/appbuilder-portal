837f9bcfd4772d323ae011bd488edc80fe49566f
Aidan Jones <aejones4gm@gmail.com>
2024-11-12 12:44:18 -0500

    Split ProjectSelector functionality into ProjectCard (#1025)
    * Rework ProjectSelector into separate component
    
    * Remove extraneous query
    
    Organizations is already queried by the root +layout.server.ts
    
    * Add more i18n to project directory
    
    * Move ProjectCards and other common to lib
    
    Moved ProjectCard, ProjectFilterSelector, and the two common.ts scripts below `routes/projects` to `lib/projects`
    
    * Add missing search bar to directory
    
    * Shorten text search call
    
    * Fix button layout to be mobile friendly

--- portal/src/routes/(authenticated)/directory/+page.svelte
+++ portal/src/routes/(authenticated)/directory/+page.svelte
@@ -3,8 +3,10 @@
   import LanguageCodeTypeahead from '$lib/components/LanguageCodeTypeahead.svelte';
   import * as m from '$lib/paraglide/messages';
   import 'flatpickr/dist/flatpickr.css';
-  import ProjectSelector from '../projects/components/ProjectSelector.svelte';
+  import ProjectCard from '$lib/projects/components/ProjectCard.svelte';
+  import IconContainer from '$lib/components/IconContainer.svelte';
   import type { PageData } from './$types';
+  import { languageTag } from '$lib/paraglide/runtime';
 
   export let data: PageData;
 
@@ -17,46 +19,81 @@
   let productDefinitionFilter: string;
   let organizationFilter: string;
   let updateDates: [Date, Date] | null;
-</script>
 
-<ProjectSelector
-  projects={data.projects.filter((proj) => {
+  let searchTerm: string = '';
+
+  $: filteredProjects = data.projects.filter((project) => {
+    const searchTermLower = searchTerm.toLowerCase();
     return (
+      [
+        project.Name,
+        project.Language,
+        project.OwnerName,
+        project.OrganizationName,
+        project.GroupName
+      ].some((field) => field?.toLowerCase().includes(searchTermLower)) &&
       (!updateDates ||
         !updateDates[1] ||
-        !proj.DateUpdated ||
-        (proj.DateUpdated > updateDates[0] && proj.DateUpdated < updateDates[1])) &&
-      (proj.Language ?? '').includes(langCode) &&
+        !project.DateUpdated ||
+        (project.DateUpdated > updateDates[0] && project.DateUpdated < updateDates[1])) &&
+      (project.Language ?? '').includes(langCode) &&
       (!productDefinitionFilter ||
-        proj.Products.some((prod) => prod.ProductDefinitionName === productDefinitionFilter)) &&
-      (!organizationFilter || proj.OrganizationName === organizationFilter)
+        project.Products.some((prod) => prod.ProductDefinitionName === productDefinitionFilter)) &&
+      (!organizationFilter || project.OrganizationName === organizationFilter)
     );
-  })}
->
-  <h1 slot="header" class="p-4 pl-6">{m.sidebar_projectDirectory()}</h1>
-  <div
-    slot="options"
-    class="w-full flex flex-row place-content-start p-4 pb-0 space-between-4 flex-wrap gap-1"
-  >
-    <select class="select select-bordered" bind:value={organizationFilter}>
-      <option value="">{m.org_allOrganizations()}</option>
-      {#each new Set(data.projects.map((p) => p.OrganizationName)).values() as org}
-        <option value={org}>{org}</option>
-      {/each}
-    </select>
+  });
+</script>
 
-    <LanguageCodeTypeahead bind:langCode />
-    <select class="select select-bordered max-w-full" bind:value={productDefinitionFilter}>
-      <option value="" selected>{m.productDefinitions_filterAllProjects()}</option>
-      {#each data.productDefinitions as pD}
-        <option value={pD.Name}>{pD.Name}</option>
-      {/each}
-    </select>
-    <DateRangePicker bind:chosenDates={updateDates} placeholder={m.directory_filters_dateRange()} />
+<div class="w-full max-w-6xl mx-auto relative px-2">
+  <div class="flex flex-row place-content-between w-full pt-4 flex-wrap">
+    <div class="inline-block">
+      <h1 class="p-4 pl-6">{m.sidebar_projectDirectory()}</h1>
+    </div>
+    <div
+      class="flex flex-row flex-wrap md:flex-nowrap place-content-end items-center mx-4 gap-1 mobile-sizing"
+    >
+      <select class="select select-bordered mobile-sizing" bind:value={organizationFilter}>
+        <option value="">{m.org_allOrganizations()}</option>
+        {#each new Set(data.projects.map((p) => p.OrganizationName)).values() as org}
+          <option value={org}>{org}</option>
+        {/each}
+      </select>
+      <div class="input input-bordered flex items-center gap-2 mobile-sizing">
+        <input type="text" placeholder={m.search()} class="flex-grow" bind:value={searchTerm} />
+        <IconContainer icon="mdi:search" class="ml-auto" width={24} />
+      </div>
+    </div>
+    <div class="w-full flex flex-row place-content-start mt-1 px-4 pb-0 flex-wrap gap-1">
+      <div class="mobile-sizing">
+        <LanguageCodeTypeahead bind:langCode inputClasses="w-full max-w-xs" />
+      </div>
+      <select
+        class="select select-bordered mobile-sizing max-w-full"
+        bind:value={productDefinitionFilter}
+      >
+        <option value="" selected>{m.productDefinitions_filterAllProjects()}</option>
+        {#each data.productDefinitions as pD}
+          <option value={pD.Name}>{pD.Name}</option>
+        {/each}
+      </select>
+      <DateRangePicker
+        bind:chosenDates={updateDates}
+        placeholder={m.directory_filters_dateRange()}
+      />
+    </div>
   </div>
-</ProjectSelector>
+  {#if filteredProjects.length > 0}
+    <div class="w-full relative p-4">
+      {#each filteredProjects.sort( (a, b) => (a.Name ?? '').localeCompare(b.Name ?? '', languageTag()) ) as project}
+        <ProjectCard {project} />
+      {/each}
+    </div>
+  {:else}
+    <p class="m-8">{m.projectTable_empty()}</p>
+  {/if}
+</div>
 
-<style>
+<style lang="postcss">
   :global(.highlight) {
     background-color: oklch(var(--a));
   }
@@ -69,5 +106,13 @@
   }
   :global(li[aria-selected='true'] .listElement) {
     background-color: oklch(var(--b2));
+  }
+  .mobile-sizing {
+    @apply w-full max-w-xs;
+  }
+  @media screen(md) {
+    .mobile-sizing {
+      @apply w-auto max-w-none;
+    }
   }
 </style>
--- portal/src/routes/(authenticated)/projects/[filter=projectSelector]/[id]/+page.svelte
+++ portal/src/routes/(authenticated)/projects/[filter=projectSelector]/[id]/+page.svelte
@@ -1,8 +1,112 @@
 <script lang="ts">
-  import ProjectSelector from '../../components/ProjectSelector.svelte';
   import type { PageData } from './$types';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
+  import IconContainer from '$lib/components/IconContainer.svelte';
+  import * as m from '$lib/paraglide/messages';
+  import ProjectFilterSelector from '$lib/projects/components/ProjectFilterSelector.svelte';
+  import ProjectCard from '$lib/projects/components/ProjectCard.svelte';
+  import { languageTag } from '$lib/paraglide/runtime';
 
   export let data: PageData;
+
+  let selectedProjects: number[] = [];
+  let selectedOrg = parseInt($page.params.id);
+  let searchTerm: string = '';
+
+  $: filteredProjects = data.projects.filter((project) => {
+    const searchTermLower = searchTerm.toLowerCase();
+    return [
+      project.Name,
+      project.Language,
+      project.OwnerName,
+      project.OrganizationName,
+      project.GroupName
+    ].some((field) => field?.toLowerCase().includes(searchTermLower));
+  });
 </script>
 
-<ProjectSelector projects={data.projects} />
+<div class="w-full max-w-6xl mx-auto relative px-2">
+  <div class="flex flex-row place-content-between w-full pt-4 flex-wrap">
+    <div class="inline-block">
+      <ProjectFilterSelector />
+    </div>
+    <div
+      class="flex flex-row flex-wrap md:flex-nowrap place-content-end items-center mx-4 mobile-sizing gap-1"
+    >
+      <select
+        class="select select-bordered mobile-sizing"
+        bind:value={selectedOrg}
+        on:change={() => goto(selectedOrg + '')}
+      >
+        {#each data.organizations as organization}
+          <option value={organization.Id}>{organization.Name}</option>
+        {/each}
+      </select>
+      <div class="input input-bordered flex items-center gap-2 mobile-sizing">
+        <input type="text" placeholder={m.search()} class="flex-grow" bind:value={searchTerm} />
+        <IconContainer icon="mdi:search" class="ml-auto" width={24} />
+      </div>
+    </div>
+  </div>
+  <div class="w-full flex flex-row flex-wrap place-content-between gap-1 mt-4">
+    <div class="flex flex-row flex-wrap mobile-sizing gap-1 mx-4">
+      <button
+        class="action"
+        disabled={!selectedProjects.length}
+        on:click={() => alert(selectedProjects.join(', '))}
+      >
+        {m.common_archive()}
+      </button>
+      <button class="action" disabled={!selectedProjects.length}>
+        {m.common_rebuild()}
+      </button>
+    </div>
+    <div class="flex flex-row flex-wrap mobile-sizing gap-1 mx-4">
+      <button class="action" on:click={() => alert('TODO api proxy')}>
+        {m.project_importProjects()}
+      </button>
+      <button class="action" on:click={() => alert('TODO api proxy')}>
+        {m.sidebar_addProject()}
+      </button>
+    </div>
+  </div>
+  {#if filteredProjects.length > 0}
+    <div class="w-full relative p-4">
+      {#each filteredProjects.sort( (a, b) => (a.Name ?? '').localeCompare(b.Name ?? '', languageTag()) ) as project}
+        <ProjectCard {project}>
+          <span slot="checkbox">
+            <input
+              type="checkbox"
+              class="mr-2 checkbox checkbox-info"
+              bind:group={selectedProjects}
+              value={project.Id}
+            />
+          </span>
+        </ProjectCard>
+      {/each}
+    </div>
+  {:else}
+    <p class="m-8">{m.projectTable_empty()}</p>
+  {/if}
+</div>
+
+<style lang="postcss">
+  .mobile-sizing {
+    @apply w-full max-w-xs;
+  }
+  @media screen(md) {
+    .mobile-sizing {
+      @apply w-auto max-w-none;
+    }
+  }
+  .action {
+    @apply form-control w-full max-w-xs btn btn-outline;
+  }
+  /* This is perfectly valid. I don't have a way to make the error disappear */
+  @media screen(sm) {
+    .action {
+      @apply w-auto max-w-none;
+    }
+  }
+</style>
