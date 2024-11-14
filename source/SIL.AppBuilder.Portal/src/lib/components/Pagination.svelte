c00a2f3a740f685e9e86f1c4da4a0aa66d3aa819
Aidan Jones <aejones4gm@gmail.com>
2024-11-14 10:13:55 -0500

    Pagination UI component (#1039)
    * Add pagination component
    
    * Create pagination schema in $lib/table
    
    * Improve layout
    
    * Move number input after page buttons
    
    * Change size from number to select
    
    * Remove border from input wrapper
    
    * Change to Total, put dropdown right
    
    * Add Total to locale JSON

---
+++ source/SIL.AppBuilder.Portal/src/lib/components/Pagination.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages';
+  export let size: number;
+  export let total: number;
+  export let page: number = 0;
+
+  $: pageCount = Math.ceil(total / size);
+  $: collapse = pageCount > 6;
+  $: hasPreviousPage = page > 0;
+  $: hasNextPage = page < pageCount - 1;
+
+  function index(i: number, page: number): number {
+    if (page <= 3) return i + 2;
+    else if (page > pageCount - 5) return pageCount + i - 5;
+    else return page + i - 1;
+  }
+</script>
+
+<div class="flex flex-row flex-wrap gap-1 w-full">
+  {#if pageCount > 1}
+    <div class="join max-w-xs overflow-x-auto md:max-w-none">
+      <label class="join-item btn btn-square form-control {hasPreviousPage ? '' : 'btn-disabled'}">
+        <span>«</span>
+        <input class="hidden" type="radio" bind:group={page} name="page" value={page - 1} />
+      </label>
+      <label class="join-item btn btn-square form-control {page === 0 ? 'bg-primary' : ''}">
+        <span>{1}</span>
+        <input class="hidden" type="radio" bind:group={page} name="page" value={0} />
+      </label>
+      {#if collapse}
+        {#if page > 3}
+          <button class="join-item btn btn-disabled">...</button>
+        {:else}
+          <label class="join-item btn btn-square form-control {page === 1 ? 'bg-primary' : ''}">
+            <span>{2}</span>
+            <input class="hidden" type="radio" bind:group={page} name="page" value={1} />
+          </label>
+        {/if}
+        {#each Array.from({ length: 3 }) as _, i}
+          <label
+            class="join-item btn btn-square form-control {page === index(i, page)
+              ? 'bg-primary'
+              : ''}"
+          >
+            <span>{index(i, page) + 1}</span>
+            <input
+              class="hidden"
+              type="radio"
+              bind:group={page}
+              name="page"
+              value={index(i, page)}
+            />
+          </label>
+        {/each}
+        {#if page < pageCount - 4}
+          <button class="join-item btn btn-disabled">...</button>
+        {:else}
+          <label
+            class="join-item btn btn-square form-control {page === pageCount - 2
+              ? 'bg-primary'
+              : ''}"
+          >
+            <span>{pageCount - 1}</span>
+            <input
+              class="hidden"
+              type="radio"
+              bind:group={page}
+              name="page"
+              value={pageCount - 2}
+            />
+          </label>
+        {/if}
+      {:else}
+        {#each Array.from({ length: pageCount - 2 }) as _, i}
+          <label class="join-item btn btn-square form-control {page === i + 1 ? 'bg-primary' : ''}">
+            <span>{i + 2}</span>
+            <input class="hidden" type="radio" bind:group={page} name="page" value={i + 1} />
+          </label>
+        {/each}
+      {/if}
+      <label
+        class="join-item btn btn-square form-control {page === pageCount - 1 ? 'bg-primary' : ''}"
+      >
+        <span>{pageCount}</span>
+        <input class="hidden" type="radio" bind:group={page} name="page" value={pageCount - 1} />
+      </label>
+      <label class="join-item btn btn-square form-control {hasNextPage ? '' : 'btn-disabled'}">
+        <span>»</span>
+        <input class="hidden" type="radio" bind:group={page} name="page" value={page + 1} />
+      </label>
+    </div>
+  {/if}
+  <div class="flex flex-row grow">
+    <div class="input flex items-center w-fit">
+      {m.common_total({ total })}
+    </div>
+    <div class="hidden grow md:inline-block">&nbsp;</div>
+    <select class="select select-bordered" name="size" bind:value={size}>
+      <option value={10}>10</option>
+      <option value={25}>25</option>
+      <option value={50}>50</option>
+      <option value={100}>100</option>
+    </select>
+  </div>
+</div>
