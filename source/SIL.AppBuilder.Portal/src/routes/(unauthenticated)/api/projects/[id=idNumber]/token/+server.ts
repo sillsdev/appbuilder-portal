aaaa7625ce92a60853f16f8aded4c51cbdbf37ff
Aidan Jones <aejones4gm@gmail.com>
2024-11-13 19:51:18 -0500

    Attempt redirect

--- source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/api/projects/[id=idNumber]/token/+server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/api/projects/[id=idNumber]/token/+server.ts
@@ -1,33 +1,14 @@
-import { error } from '@sveltejs/kit';
-import { encode, getToken } from'@auth/core/jwt';
-import { jwtVerify } from 'jose';
+import { error, redirect } from '@sveltejs/kit';
 
-export async function POST({ params, request, fetch }) {
+export async function POST({ request }) {
   if (!request.headers.get('Authorization')) {
     return error(401, `Unauthorized`);
   }
 
-  const token = await getToken({ req: request, raw: true, salt: '', secret: [] });
-
-  console.log(token);
-
-  console.log(jwtVerify(token));
-
-  console.log(await encode({ token, secret: import.meta.env.VITE_AUTH0_CLIENT_SECRET, salt: ''}))
-
-  
-  return new Response();
-  const res = await fetch(`/projects/${params.id}/token`, {
-    method: 'POST',
-    headers: {
-      ...Object.fromEntries(request.headers.entries()),
-      Cookie: `authjs.session-token=${request.headers
-        .get('Authorization')
-        ?.replace('Bearer ', '')}`
-    }
-  });
-
-  console.log(res);
-
-  return res;
+  return redirect(
+    302,
+    `/auth/callback/auth0?code=${request.headers
+      .get('Authorization')
+      ?.replace('Bearer ', '')}&redirectUrl=/projects/id/token`
+  );
 }
