7413e54cf9166c89b78c8f2df971b6f53c8f1bff
Aidan Jones <aejones4gm@gmail.com>
2024-11-15 14:01:36 -0500

    Better configuration and styling for SortTable

--- source/SIL.AppBuilder.Portal/src/lib/components/SortTable.svelte
+++ source/SIL.AppBuilder.Portal/src/lib/components/SortTable.svelte
@@ -2,88 +2,117 @@
   import { languageTag } from '$lib/paraglide/runtime';
   import { ArrowDownIcon, ArrowUpIcon } from '$lib/icons';
   export let items: { [key: string]: any }[];
+  export let columns: {
+    header: string;
+    data: (i: any) => any;
+    render?: (d: any) => string;
+    sortable?: boolean;
+  }[];
+  export let className: string = '';
+  export let maxh_class: string = 'max-h-96';
 
-  let current = ''; //current field being sorted
+  let current = columns.find((c) => c.sortable)!; //current field being sorted
   let descending = false;
 
-  const handleClick = (key: string) => {
-    if (current !== key) {
+  const handleClick = (key: (typeof columns)[0]) => {
+    if (current.header !== key.header) {
       // change current field
       descending = false;
       current = key;
     } else {
       if (descending) {
         // reset to sort default field
-        handleClick('');
-        return; //don't sort twice
+        if (current !== columns.find((c) => c.sortable)!) {
+          handleClick(columns.find((c) => c.sortable)!);
+          return; //don't sort twice
+        } else {
+          descending = false;
+        }
       } else {
         descending = true;
       }
     }
     // sort based on current field
     // if blank, sort first field
-    const field = current || Object.keys(items[0])[0];
+    const data = current.data || columns.find((c) => c.sortable)!.data;
     const langTag = languageTag();
     items =
-      typeof items[0][field] === 'string'
+      typeof data(items[0]) === 'string'
         ? // sort strings
           items.sort((a, b) => {
             return descending
-              ? b[field].localeCompare(a[field], langTag)
-              : a[field].localeCompare(b[field], langTag);
+              ? data(b).localeCompare(data(a), langTag)
+              : data(a).localeCompare(data(b), langTag);
           })
         : // sort non-strings (i.e. numbers)
           items.sort((a, b) => {
-            return descending ? b[field] - a[field] : a[field] - b[field];
+            if (data(a) === data(b)) {
+              return 0;
+            } else if (data(a) > data(b)) {
+              return descending ? -1 : 1;
+            } else {
+              return descending ? 1 : -1;
+            }
           });
   };
 </script>
 
-<table class="table">
-  <thead>
-    <tr>
-      {#each Object.keys(items[0]) as key}
-        <th on:click={() => handleClick(key)}>
-          <!-- svelte-ignore a11y-label-has-associated-control -->
-          <label class="form-control flex-row">
-            <span class="label-text">{key}</span>
-            <span class="direction-arrow">
-              {#if current === key}
-                {#if descending}
-                  <ArrowDownIcon />
-                {:else}
-                  <ArrowUpIcon />
-                {/if}
-              {:else}
-                &nbsp;
-              {/if}
-            </span>
-          </label>
-        </th>
-      {/each}
-    </tr>
-  </thead>
-  <tbody>
-    {#each items as item}
+<div class="overflow-y {maxh_class}">
+  <table class="table">
+    <thead>
       <tr>
-        {#each Object.values(item) as val}
-          <td>{val}</td>
+        {#each columns as c}
+          <th
+            on:click={() => {
+              if (c.sortable) {
+                handleClick(c);
+              }
+            }}
+          >
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label class="form-control flex-row">
+              <span class="label-text">{c.header}</span>
+              <span class="direction-arrow">
+                {#if current.header === c.header && c.sortable}
+                  {#if descending}
+                    <ArrowDownIcon />
+                  {:else}
+                    <ArrowUpIcon />
+                  {/if}
+                {:else}
+                  &nbsp;
+                {/if}
+              </span>
+            </label>
+          </th>
         {/each}
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody class={className}>
+      {#each items as item}
+        <tr>
+          {#each columns as c}
+            <td>{@html c.render ? c.render(c.data(item)) : c.data(item)}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
 
 <style lang="postcss">
   tr {
     @apply cursor-pointer select-none;
   }
   tbody > tr:hover {
-    @apply bg-info;
+    @apply bg-neutral;
   }
   thead {
     /* this helps prevent the vertical jankiness */
     line-height: inherit;
+    position: sticky; 
+    top: 0;
+    @apply bg-base-100;
   }
   .direction-arrow {
     display: inline-block;
