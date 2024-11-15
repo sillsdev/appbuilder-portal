3ce36d7d2be9add58466f0ccee44b9dbcb4c370a
Aidan Jones <aejones4gm@gmail.com>
2024-11-15 11:51:45 -0500

    Fix sort direction picker

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/components/SortTable.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/tasks/[product_id]/components/SortTable.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { languageTag } from "$lib/paraglide/runtime";
-  import SortDirectionPicker from "./SortDirectionPicker.svelte";
-  export let items: {[key: string]: any}[];
+  import { languageTag } from '$lib/paraglide/runtime';
+  import { ArrowDownIcon, ArrowUpIcon } from '$lib/icons';
+  export let items: { [key: string]: any }[];
 
-  let current = ""; //current field being sorted
+  let current = ''; //current field being sorted
   let descending = false;
 
   const handleClick = (key: string) => {
@@ -11,14 +11,12 @@
       // change current field
       descending = false;
       current = key;
-    }
-    else {
+    } else {
       if (descending) {
         // reset to sort default field
-        handleClick("");
+        handleClick('');
         return; //don't sort twice
-      }
-      else {
+      } else {
         descending = true;
       }
     }
@@ -26,41 +24,74 @@
     // if blank, sort first field
     const field = current || Object.keys(items[0])[0];
     const langTag = languageTag();
-    items = (typeof items[0][field] === 'string')?
-      // sort strings
-      items.sort((a, b) => {
-        return descending ?
-          b[field].localeCompare(a[field], langTag):
-          a[field].localeCompare(b[field], langTag);
-      }):
-      // sort non-strings (i.e. numbers)
-      items.sort((a, b) => {
-        return descending?
-          b[field] - a[field]:
-          a[field] - b[field];
-      });
-  }
+    items =
+      typeof items[0][field] === 'string'
+        ? // sort strings
+          items.sort((a, b) => {
+            return descending
+              ? b[field].localeCompare(a[field], langTag)
+              : a[field].localeCompare(b[field], langTag);
+          })
+        : // sort non-strings (i.e. numbers)
+          items.sort((a, b) => {
+            return descending ? b[field] - a[field] : a[field] - b[field];
+          });
+  };
 </script>
 
 <table class="table">
   <thead>
     <tr>
       {#each Object.keys(items[0]) as key}
-      <th on:click={() => handleClick(key)}>
-        <SortDirectionPicker active={current === key} bind:descending={descending}>
-          {key}
-        </SortDirectionPicker>
-      </th>
+        <th on:click={() => handleClick(key)}>
+          <!-- svelte-ignore a11y-label-has-associated-control -->
+          <label class="form-control flex-row">
+            <span class="label-text">{key}</span>
+            <span class="direction-arrow">
+              {#if current === key}
+                {#if descending}
+                  <ArrowDownIcon />
+                {:else}
+                  <ArrowUpIcon />
+                {/if}
+              {:else}
+                &nbsp;
+              {/if}
+            </span>
+          </label>
+        </th>
       {/each}
     </tr>
   </thead>
   <tbody>
     {#each items as item}
-    <tr>
-      {#each Object.values(item) as val}
-      <td>{val}</td>
-      {/each}
-    </tr>
+      <tr>
+        {#each Object.values(item) as val}
+          <td>{val}</td>
+        {/each}
+      </tr>
     {/each}
   </tbody>
 </table>
+
+<style lang="postcss">
+  tr {
+    @apply cursor-pointer select-none;
+  }
+  tbody > tr:hover {
+    @apply bg-info;
+  }
+  thead {
+    /* this helps prevent the vertical jankiness */
+    line-height: inherit;
+  }
+  .direction-arrow {
+    display: inline-block;
+    /* these help mitigate some of the visual jankiness */
+    min-width: 24px;
+    min-height: 26px;
+  }
+  label {
+    @apply select-none cursor-pointer;
+  }
+</style>
