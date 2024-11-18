99ae8ff58f3a4a5b7c0fd51e0ae362c7a0303f35
Aidan Jones <aejones4gm@gmail.com>
2024-11-18 08:40:40 -0500

    BuildEngine API methods (#1040)
    * Methods to interact with BuildEngine
    
    * Get default url and token from env
    
    * Change Content-Type header
    
    That was the problem? Seriously???
    
    * Consolidate response status types
    
    * Return DeleteResponse from BuildEngine delete functions
    
    * Change BuildEngine token method
    
    * Return ErrorResponse from try-catch in request
    
    * Split channels into separate type
    
    * Fix ProjectConfig and ProjectResponse composition
    
    * Change type of TokenConfig/Response to reflect DWKit
    
    * Break result into CommonResult

---
+++ portal/common/build-engine-api/types.ts
@@ -0,0 +1,136 @@
+export type Auth =
+  | {
+      type: 'query';
+      organizationId: number;
+    }
+  | {
+      type: 'provided';
+      url: string;
+      token: string;
+    };
+
+export type Response =
+  | ErrorResponse
+  | ProjectResponse
+  | TokenResponse
+  | BuildResponse
+  | ReleaseResponse
+  | StatusResponse;
+
+export type ErrorResponse = {
+  responseType: 'error';
+  name: string;
+  message: string;
+  code: number;
+  status: number;
+  type: string;
+};
+
+export type StatusResponse = {
+  responseType: 'status';
+  status: number;
+};
+
+export type DeleteResponse = {
+  responseType: 'delete';
+  status: number;
+};
+
+type SuccessResponse = {
+  responseType: 'project' | 'token' | 'job' | 'build' | 'release';
+  id: number;
+  created: Date;
+  updated: Date;
+  _links: {
+    self?: {
+      href: string;
+    };
+    job?: {
+      href: string;
+    };
+  };
+};
+
+type CommonStatus = 'initialized' | 'accepted' | 'completed';
+type CommonResult = 'SUCCESS' | 'FAILURE' | null;
+
+export type ProjectConfig = {
+  app_id: string;
+  project_name: string;
+  language_code: string;
+  storage_type: string;
+};
+export type ProjectResponse = SuccessResponse &
+  ProjectConfig & {
+    responseType: 'project';
+    status: CommonStatus | 'delete' | 'deleting';
+    result: CommonResult;
+    error: string | null;
+    url: string;
+    publishing_key: string;
+    user_id: string;
+    group_id: string;
+  };
+
+export type TokenConfig = {
+  name: string;
+  ReadOnly: boolean;
+};
+export type TokenResponse = {
+  responseType: 'token';
+  SessionToken: string;
+  SecretAccessKey: string;
+  AccessKeyId: string;
+  Expiration: string;
+  Region: string;
+  ReadOnly: boolean;
+};
+
+export type JobConfig = {
+  request_id: string;
+  git_url: string;
+  app_id: string;
+  publisher_id: string;
+};
+export type JobResponse = SuccessResponse &
+  JobConfig & {
+    responseType: 'job';
+  };
+
+type BuildOrReleaseStatus = 'active' | 'expired' | 'postprocessing';
+
+type BuildCommon = {
+  targets: string;
+};
+export type BuildConfig = BuildCommon & {
+  environment: { [key: string]: string };
+};
+export type BuildResponse = SuccessResponse &
+  BuildCommon & {
+    responseType: 'build';
+    job_id: number;
+    status: CommonStatus | BuildOrReleaseStatus;
+    result: CommonResult | 'ABORTED';
+    error: string | null;
+    artifacts: { [key: string]: string };
+  };
+
+export type Channels = 'production' | 'beta' | 'alpha';
+
+type ReleaseCommon = {
+  channel: Channels;
+  targets: string;
+};
+export type ReleaseConfig = ReleaseCommon & {
+  environment: { [key: string]: string };
+};
+export type ReleaseResponse = SuccessResponse &
+  ReleaseCommon & {
+    responseType: 'release';
+    buildId: number;
+    status: CommonStatus | BuildOrReleaseStatus;
+    result: CommonResult | 'EXCEPTION';
+    error: string | null;
+    console_text: string;
+    artifacts: { [key: string]: string };
+  };
