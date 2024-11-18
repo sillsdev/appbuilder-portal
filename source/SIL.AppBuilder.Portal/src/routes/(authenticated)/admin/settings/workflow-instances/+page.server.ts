b3fdcafb991326da000f8c7b6411410eb4fa9d2f
Aidan Jones <aejones4gm@gmail.com>
2024-11-18 10:23:02 -0500

    Server-side sort and search

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-instances/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-instances/+page.server.ts
@@ -7,85 +7,56 @@ import { RoleId } from 'sil.appbuilder.portal.common/prisma';
 import { error } from '@sveltejs/kit';
 import * as v from 'valibot';
 import * as m from '$lib/paraglide/messages';
+import type { Prisma } from '@prisma/client';
+import { idSchema } from '$lib/valibot';
 
 const tableSchema = v.object({
   page: paginateSchema,
   sort: v.nullable(
-    v.array(
-      v.object({
-        field: v.string(),
-        direction: v.picklist(['asc', 'desc'])
-      })
-    )
+    v.object({
+      field: v.string(),
+      direction: v.picklist(['asc', 'desc'])
+    })
   ),
-  search: v.object({
-    field: v.nullable(v.string()),
-    text: v.nullable(v.string())
-  })
+  search: v.string(),
+  workflowDefinitionId: v.nullable(idSchema),
+  dateUpdatedRange: v.nullable(v.tuple([v.date(), v.nullable(v.date())]))
 });
 
-const typeNames = [
-  '',
-  m.admin_settings_workflowDefinitions_workflowTypes_1(),
-  m.admin_settings_workflowDefinitions_workflowTypes_2(),
-  m.admin_settings_workflowDefinitions_workflowTypes_3()
-];
-
 export const load: PageServerLoad = async (event) => {
-  const instances = (
-    await prisma.workflowInstances.findMany({
-      select: {
-        Product: {
-          select: {
-            Id: true,
-            ProductTransitions: {
-              where: {
-                DateTransition: {
-                  not: null
-                }
-              },
-              select: {
-                DateTransition: true,
-                DestinationState: true,
-                InitialState: true,
-                WorkflowType: true
-              },
-              orderBy: [
-                {
-                  DateTransition: 'desc'
-                }
-              ],
-              take: 1
-            }
-          }
+  const instances = await prisma.workflowInstances.findMany({
+    select: {
+      ProductId: true,
+      WorkflowDefinition: {
+        select: {
+          Name: true
         }
       },
-      take: 50
-    })
-  ).map((wi) => ({
-    Id: wi.Product.Id,
-    Transition: {
-      State:
-        wi.Product.ProductTransitions[0].DestinationState ??
-        wi.Product.ProductTransitions[0].InitialState ??
-        typeNames[wi.Product.ProductTransitions[0].WorkflowType ?? 1],
-      Date: wi.Product.ProductTransitions[0].DateTransition
-    }
-  }));
+      State: true,
+      DateUpdated: true
+    },
+    take: 50
+  });
 
-  const count = await prisma.productTransitions.count();
   return {
     instances,
-    count,
+    count: await prisma.workflowInstances.count(),
     form: await superValidate(
       {
         page: {
           page: 0,
           size: 50
-        }
+        },
+        workflowDefinitionId: null
       },
       valibot(tableSchema)
-    )
+    ),
+    workflowDefinitions: await prisma.workflowDefinitions.findMany({
+      select: {
+        Id: true,
+        Name: true
+      }
+    })
   };
 };
 
@@ -97,105 +68,82 @@ export const actions: Actions = {
     const form = await superValidate(request, valibot(tableSchema));
     if (!form.valid) return fail(400, { form, ok: false });
 
-    const search = form.data.search.text
+    const search = form.data.search
       ? new RegExp(
           // escape regex special characters: https://stackoverflow.com/a/6969486
-          form.data.search.text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), //
+          form.data.search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), //
           'i'
         )
       : null;
 
-    let instances = (
-      await prisma.workflowInstances.findMany({
-        orderBy: form.data.sort?.find((s) => s.field === 'product')
-          ? { ProductId: form.data.sort?.find((s) => s.field === 'product')?.direction }
-          : undefined,
-        select: {
-          Product: {
-            select: {
-              Id: true,
-              ProductTransitions: {
-                where: {
-                  DateTransition: {
-                    not: null
-                  }
-                },
-                select: {
-                  DateTransition: true,
-                  DestinationState: true,
-                  InitialState: true,
-                  WorkflowType: true
-                },
-                orderBy: [
-                  {
-                    DateTransition: 'desc'
-                  }
-                ],
-                take: 1
-              }
+    const pids = (await prisma.products.findMany({ select: { Id: true } }))
+      .filter((p) => search?.test(p.Id))
+      .map((p) => p.Id);
+
+    const where: Prisma.WorkflowInstancesWhereInput = {
+      OR: [
+        {
+          ProductId: { in: pids }
+        },
+        {
+          WorkflowDefinition: {
+            Name: {
+              contains: form.data.search,
+              mode: 'insensitive'
             }
           }
+        },
+        {
+          State: {
+            contains: form.data.search,
+            mode: 'insensitive'
+          }
+        },
+        {
+          DateUpdated:
+            form.data.dateUpdatedRange && form.data.dateUpdatedRange[1]
+              ? {
+                  gt: form.data.dateUpdatedRange[0],
+                  lt: form.data.dateUpdatedRange[1]
+                }
+              : undefined
         }
-      })
-    ).map((wi) => ({
-      Id: wi.Product.Id,
-      Transition: {
-        State:
-          wi.Product.ProductTransitions[0].DestinationState ??
-          wi.Product.ProductTransitions[0].InitialState ??
-          typeNames[wi.Product.ProductTransitions[0].WorkflowType ?? 1],
-        Date: wi.Product.ProductTransitions[0].DateTransition
-      }
-    }));
+      ],
+      WorkflowDefinitionId: form.data.workflowDefinitionId ?? undefined
+    };
 
-    // I don't like that I have to do it this way, but there does not appear to be a good way to do this on the database side
-    if (search) {
-      if (!form.data.search.field) {
-        instances = instances.filter(
-          (i) => search.test(i.Id) || search.test(i.Transition.State ?? '')
-        );
-      } else if (form.data.search.field === 'product') {
-        instances = instances.filter((i) => search.test(i.Id));
-      } else if (form.data.search.field === 'state') {
-        instances = instances.filter((i) => search.test(i.Transition.State ?? ''));
-      }
-    }
-
-    instances = form.data.sort?.find((s) => s.field === 'date')
-      ? instances.sort(
-          form.data.sort?.find((s) => s.field === 'date')?.direction === 'asc'
-            ? (a, b) => {
-                return a.Transition.Date?.getTime()! - b.Transition.Date?.getTime()!;
-              }
-            : (a, b) => {
-                return b.Transition.Date?.getTime()! - a.Transition.Date?.getTime()!;
-              }
-        )
-      : instances;
-
-    instances = form.data.sort?.find((s) => s.field === 'state')
-      ? instances.sort(
-          form.data.sort?.find((s) => s.field === 'state')?.direction === 'asc'
-            ? (a, b) => {
-                return a.Transition.State!.localeCompare(b.Transition.State!);
-              }
-            : (a, b) => {
-                return b.Transition.State!.localeCompare(a.Transition.State!);
-              }
-        )
-      : instances;
-
-    const count = instances.length;
+    const instances = await prisma.workflowInstances.findMany({
+      orderBy:
+        form.data.sort?.field === 'product'
+          ? { ProductId: form.data.sort.direction }
+          : form.data.sort?.field === 'workflow'
+          ? { WorkflowDefinition: { Name: form.data.sort.direction } }
+          : form.data.sort?.field === 'state'
+          ? { State: form.data.sort.direction }
+          : form.data.sort?.field === 'date'
+          ? { DateUpdated: form.data.sort.direction }
+          : undefined,
+      where: where,
+      select: {
+        ProductId: true,
+        WorkflowDefinition: {
+          select: {
+            Name: true
+          }
+        },
+        State: true,
+        DateUpdated: true
+      },
+      skip: form.data.page.page * form.data.page.size,
+      take: form.data.page.size
+    });
 
     return {
       form,
       ok: true,
       query: {
-        data: instances.slice(
-          form.data.page.page * form.data.page.size,
-          (form.data.page.page + 1) * form.data.page.size
-        ),
-        count
+        data: instances,
+        count: await prisma.workflowInstances.count({ where: where })
       }
     };
   }
--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-instances/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/admin/settings/workflow-instances/+page.svelte
@@ -2,44 +2,138 @@
   import * as m from '$lib/paraglide/messages';
   import type { PageData } from './$types';
   import SortTable from '$lib/components/SortTable.svelte';
+  import DateRangePicker from '$lib/components/DateRangePicker.svelte';
+  import SearchBar from '$lib/components/SearchBar.svelte';
+  import Pagination from '$lib/components/Pagination.svelte';
+  import { superForm } from 'sveltekit-superforms';
+  import type { FormResult } from 'sveltekit-superforms';
+  import { writable } from 'svelte/store';
 
   export let data: PageData;
+
+  const instances = writable(data.instances);
+  const count = writable(data.count);
+
+  const { form, enhance, submit } = superForm(data.form, {
+    dataType: 'json',
+    resetForm: false,
+    onChange(event) {
+      if (!(event.paths.includes('page.size') || event.paths.includes('search'))) {
+        submit();
+      }
+    },
+    onUpdate(event) {
+      const data = event.result.data as FormResult<{
+        query: { data: any[]; count: number };
+      }>;
+      if (event.form.valid && data.query) {
+        instances.set(data.query.data);
+        count.set(data.query.count);
+      }
+    }
+  });
 </script>
 
 <div class="w-full">
-  <h2>{m.admin_settings_workflowInstances_title()}</h2>
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <form
+    method="POST"
+    action="?/page"
+    use:enhance
+    on:keydown={(event) => {
+      if (event.key === 'Enter') submit();
+    }}
+  >
+    <div
+      class="flex flex-row flex-wrap md:flex-nowrap place-content-end items-center px-4 gap-1 mobile-sizing"
+    >
+      <div class="inline-block grow mobile-sizing">
+        <h1 class="py-4 px-2">{m.admin_settings_workflowInstances_title()}</h1>
+      </div>
+      <div
+        class="flex flex-row flex-wrap md:flex-nowrap place-content-end items-center gap-1 mobile-sizing"
+      >
+        <SearchBar bind:value={$form.search} className="w-full max-w-xs md:w-auto md:max-w-none" />
+      </div>
+    </div>
+    <div class="flex flex-row flex-wrap gap-1 place-content-start px-4 pt-1 mobile-sizing">
+      <select class="select select-bordered mobile-sizing" bind:value={$form.workflowDefinitionId}>
+        <option value={null} selected>{m.productDefinitions_filterAllProjects()}</option>
+        {#each data.workflowDefinitions as pD}
+          <option value={pD.Id}>{pD.Name}</option>
+        {/each}
+      </select>
+      <DateRangePicker
+        bind:chosenDates={$form.dateUpdatedRange}
+        placeholder={m.directory_filters_dateRange()}
+      />
+    </div>
+  </form>
   <div class="m-4 relative mt-0">
-    {#if data.instances.length > 0}
+    {#if $instances.length > 0}
       <SortTable
-        data={data.instances}
+        data={$instances}
         columns={[
           {
             id: 'product',
             header: m.tasks_product(),
-            data: (i) => i.Id,
+            data: (i) => i.ProductId,
             render: (c) =>
-              `<a class="link" href="/admin/settings/workflow-instances/${c}">${c}</a>`,
+              `<a class="link" href="/admin/settings/workflow-instances/${c}">${
+                c.slice(0, 3) + '...' + c.slice(-3)
+              }</a>`,
+            sortable: true
+          },
+          {
+            id: 'workflow',
+            header: m.admin_settings_workflowDefinitions_name(),
+            data: (i) => i.WorkflowDefinition.Name,
             sortable: true
           },
           {
             id: 'state',
             header: m.project_products_transitions_state(),
-            data: (i) => i.Transition.State,
+            data: (i) => i.State,
             sortable: true
           },
           {
             id: 'date',
-            header: m.project_products_transitions_date(),
-            data: (i) => i.Transition.Date,
+            header: m.admin_settings_buildEngines_lastUpdated(),
+            data: (i) => i.DateUpdated,
             render: (c) => c?.toLocaleString() ?? '',
             sortable: true
           }
         ]}
         serverSide={true}
         maxh_class="max-h-full"
+        on:sort={(e) => form.update((data) => ({ ...data, sort: e.detail }))}
       />
     {:else}
       <p class="m-8">{m.admin_settings_workflowInstances_empty()}</p>
     {/if}
   </div>
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <form
+    method="POST"
+    action="?/page"
+    use:enhance
+    on:keydown={(event) => {
+      if (event.key === 'Enter') submit();
+    }}
+  >
+    <div class="w-full flex flex-row place-content-start p-4 space-between-4 flex-wrap gap-1">
+      <Pagination bind:size={$form.page.size} total={$count} bind:page={$form.page.page} />
+    </div>
+  </form>
 </div>
+
+<style lang="postcss">
+  .mobile-sizing {
+    @apply w-full max-w-xs;
+  }
+  @media screen(md) {
+    .mobile-sizing {
+      @apply w-auto max-w-none;
+    }
+  }
+</style>
