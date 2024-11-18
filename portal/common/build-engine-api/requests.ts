d2055b5465b9c1c5f74e04bbb155a449b0107e4b
Aidan Jones <aejones4gm@gmail.com>
2024-11-18 08:40:40 -0500

    BuildEngine API methods (#1040)
    * Methods to interact with BuildEngine
    
    * Get default url and token from env
    
    * Change Content-Type header
    
    That was the problem? Seriously???
    
    * Consolidate response status types
    
    * Return DeleteResponse from BuildEngine delete functions
    
    * Change BuildEngine token method
    
    * Return ErrorResponse from try-catch in request
    
    * Split channels into separate type
    
    * Fix ProjectConfig and ProjectResponse composition
    
    * Change type of TokenConfig/Response to reflect DWKit
    
    * Break result into CommonResult

---
+++ portal/common/build-engine-api/requests.ts
@@ -0,0 +1,223 @@
+import prisma from '../prisma.js';
+import * as Types from './types.js';
+
+export async function request(
+  resource: string,
+  auth: Types.Auth,
+  method: string = 'GET',
+  body?: any
+) {
+  try {
+    const { url, token } = auth.type === 'query' ? await getURLandToken(auth.organizationId) : auth;
+    return await fetch(`${url}/${resource}`, {
+      method: method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': body ? 'application/json' : undefined
+      },
+      body: body ? JSON.stringify(body) : undefined
+    });
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        responseType: 'error',
+        name: '',
+        status: 500,
+        code: 500,
+        message: typeof e === 'string' ? e.toUpperCase() : e instanceof Error ? e.message : e,
+        type: ''
+      } as Types.ErrorResponse),
+      {
+        status: 500,
+        statusText: 'Internal Server Error'
+      }
+    );
+  }
+}
+export async function getURLandToken(organizationId: number) {
+  const org = await prisma.organizations.findUnique({
+    where: {
+      Id: organizationId
+    },
+    select: {
+      BuildEngineUrl: true,
+      BuildEngineApiAccessToken: true,
+      UseDefaultBuildEngine: true
+    }
+  });
+
+  return org.UseDefaultBuildEngine
+    ? {
+        url: process.env.DEFAULT_BUILDENGINE_URL,
+        token: process.env.DEFAULT_BUILDENGINE_API_ACCESS_TOKEN
+      }
+    : {
+        url: org.BuildEngineUrl,
+        token: org.BuildEngineApiAccessToken
+      };
+}
+
+export async function systemCheck(auth: Types.Auth) {
+  const res = await request('system/check', auth);
+  return res.ok
+    ? ({ status: res.status} as Types.StatusResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+
+export async function createProject(
+  auth: Types.Auth,
+  project: Types.ProjectConfig
+): Promise<Types.ProjectResponse | Types.ErrorResponse> {
+  const res = await request('project', auth, 'POST', project);
+  return res.ok
+    ? ((await res.json()) as Types.ProjectResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getProjects(
+  auth: Types.Auth
+): Promise<Types.ProjectResponse[] | Types.ErrorResponse> {
+  const res = await request('project', auth);
+  return res.ok
+    ? ((await res.json()) as Types.ProjectResponse[])
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getProject(
+  auth: Types.Auth,
+  projectId: number
+): Promise<Types.ProjectResponse | Types.ErrorResponse> {
+  const res = await request(`project/${projectId}`, auth);
+  return res.ok
+    ? ((await res.json()) as Types.ProjectResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function deleteProject(
+  auth: Types.Auth,
+  projectId: number
+): Promise<Types.DeleteResponse | Types.ErrorResponse> {
+  const res = await request(`project/${projectId}`, auth, 'DELETE');
+  return res.ok
+    ? { responseType: 'delete', status: res.status }
+    : ((await res.json()) as Types.ErrorResponse);
+}
+
+export async function getProjectAccessToken(
+  auth: Types.Auth,
+  projectId: number,
+  token: Types.TokenConfig
+): Promise<Types.TokenResponse | Types.ErrorResponse> {
+  const res = await request(`project/${projectId}/token`, auth, 'POST', token);
+  return res.ok
+    ? ((await res.json()) as Types.TokenResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+
+export async function createJob(
+  auth: Types.Auth,
+  job: Types.JobConfig
+): Promise<Types.JobResponse | Types.ErrorResponse> {
+  const res = await request('job', auth, 'POST', job);
+  return res.ok
+    ? ((await res.json()) as Types.JobResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getJobs(
+  auth: Types.Auth
+): Promise<Types.JobResponse[] | Types.ErrorResponse> {
+  const res = await request('job', auth);
+  return res.ok
+    ? ((await res.json()) as Types.JobResponse[])
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getJob(
+  auth: Types.Auth,
+  jobId: number
+): Promise<Types.JobResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}`, auth);
+  return res.ok
+    ? ((await res.json()) as Types.JobResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function deleteJob(
+  auth: Types.Auth,
+  jobId: number
+): Promise<Types.DeleteResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}`, auth, 'DELETE');
+  return res.ok
+    ? { responseType: 'delete', status: res.status }
+    : ((await res.json()) as Types.ErrorResponse);
+}
+
+export async function createBuild(
+  auth: Types.Auth,
+  jobId: number,
+  build: Types.BuildConfig
+): Promise<Types.BuildResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build`, auth, 'POST', build);
+  return res.ok
+    ? ((await res.json()) as Types.BuildResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getBuild(
+  auth: Types.Auth,
+  jobId: number,
+  buildId: number
+): Promise<Types.BuildResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build/${buildId}`, auth);
+  return res.ok
+    ? ((await res.json()) as Types.BuildResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getBuilds(
+  auth: Types.Auth,
+  jobId: number
+): Promise<Types.BuildResponse[] | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build`, auth);
+  return res.ok
+    ? ((await res.json()) as Types.BuildResponse[])
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function deleteBuild(
+  auth: Types.Auth,
+  jobId: number,
+  buildId: number
+): Promise<Types.DeleteResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build/${buildId}`, auth, 'DELETE');
+  return res.ok
+    ? { responseType: 'delete', status: res.status }
+    : ((await res.json()) as Types.ErrorResponse);
+}
+
+export async function createRelease(
+  auth: Types.Auth,
+  jobId: number,
+  buildId: number,
+  release: Types.ReleaseConfig
+): Promise<Types.ReleaseResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build/${buildId}`, auth, 'PUT', release);
+  return res.ok
+    ? ((await res.json()) as Types.ReleaseResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function getRelease(
+  auth: Types.Auth,
+  jobId: number,
+  buildId: number,
+  releaseId: number
+): Promise<Types.ReleaseResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build/${buildId}/release/${releaseId}`, auth);
+  return res.ok
+    ? ((await res.json()) as Types.ReleaseResponse)
+    : ((await res.json()) as Types.ErrorResponse);
+}
+export async function deleteRelease(
+  auth: Types.Auth,
+  jobId: number,
+  buildId: number,
+  releaseId: number
+): Promise<Types.DeleteResponse | Types.ErrorResponse> {
+  const res = await request(`job/${jobId}/build/${buildId}/release/${releaseId}`, auth, 'DELETE');
+  return res.ok
+    ? { responseType: 'delete', status: res.status }
+    : ((await res.json()) as Types.ErrorResponse);
+}
