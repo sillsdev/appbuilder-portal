9e1c9427c51f72f5c8e9a016f85f06f3f3f7f97a
Aidan Jones <aejones4gm@gmail.com>
2024-11-18 13:16:12 -0500

    Add tooltip to search bar (#1044)

--- source/SIL.AppBuilder.Portal/src/lib/components/SearchBar.svelte
+++ source/SIL.AppBuilder.Portal/src/lib/components/SearchBar.svelte
@@ -4,9 +4,23 @@
 
   export let className: string = '';
   export let value: string;
+  export let tooltip: string = '';
 </script>
 
-<label class="input input-bordered flex items-center gap-2 {className}">
-  <input type="text" placeholder={m.search()} class="flex-grow" bind:value={value} />
-  <IconContainer icon="mdi:search" class="ml-auto" width={24} />
-</label>
+<div
+  class="{className} {tooltip ? 'tooltip tooltip-bottom' : ''}"
+  data-html="true"
+  data-tip={tooltip}
+>
+  <label class="input input-bordered flex items-center gap-2 w-full">
+    <input type="text" placeholder={m.search()} class="flex-grow" bind:value />
+    <IconContainer icon="mdi:search" class="ml-auto" width={24} />
+  </label>
+</div>
+
+<style>
+  .tooltip:before {
+    white-space: pre-wrap;
+    text-align: start;
+  }
+</style>
