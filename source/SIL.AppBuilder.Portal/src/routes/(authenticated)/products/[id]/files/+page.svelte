575c7b837c41a1e187e75944fe5287b805aa5306
Aidan Jones <aejones4gm@gmail.com>
2024-11-21 11:19:17 -0500

    Show multiple, collapsable builds

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.svelte
@@ -5,27 +5,12 @@
   import { getRelativeTime } from '$lib/timeUtils';
   import type { PageData } from './$types';
   import { bytesToHumanSize } from '$lib/utils';
+  import BuildArtifacts from './components/BuildArtifacts.svelte';
 
   export let data: PageData;
-  let selectedBuildId: number;
-  $: selectedBuild = data.builds.find((b) => b.Id === selectedBuildId);
   // TODO: test this page with artifacts
 
-  function versionString(build: (typeof data.builds)[0]): string {
-    let version = build.Version;
-    if (version === null) {
-      if (build.Success === false) {
-        version = ` ${m.projects_buildFailed()} `;
-      } else {
-        version = ` ${m.projects_buildPending()} `;
-      }
-    }
-
-    if (build.BuildId === data.product?.WorkflowBuildId) {
-      return m.projects_latestBuild({ version });
-    }
-    return version;
-  }
+  
 </script>
 
 <div class="w-full max-w-6xl mx-auto p-4">
@@ -34,62 +19,14 @@
       <li>
         <a class="link" href="/projects/{data.product?.Project.Id}">{data.product?.Project.Name}</a>
       </li>
-      <li>{data.product?.ProductDefinition.Name}</li>
+      <li>
+        <IconContainer icon={getIcon(data.product?.ProductDefinition.Name ?? '')} width="24" />{data
+          .product?.ProductDefinition.Name}
+      </li>
     </ul>
   </div>
   <h1 class="pl-4">{m.products_files_title()}</h1>
-  <select class="select font-bold text-lg text-info" bind:value={selectedBuildId}>
-    {#if data.builds.length === 0}
-      <option disabled selected value="-1">
-        {m.projects_noBuilds()}
-      </option>
-    {/if}
-    {#each data.builds as build}
-      <option value={build.Id} class="font-bold text-lg">
-        {versionString(build)}
-      </option>
-    {/each}
-  </select>
-  <div class="rounded-md border border-slate-400 w-full my-2">
-    <div class="bg-base-300 p-2 flex flex-row rounded-t-md place-content-between">
-      <span>
-        <IconContainer icon={getIcon(data.product?.ProductDefinition.Name ?? '')} width="24" />
-        {data.product?.ProductDefinition.Name}
-      </span>
-      <span>
-        <!-- TODO i18n (requires pluralization support) -->
-        {m.project_products_numArtifacts({ amount: selectedBuild?.ProductArtifacts.length })}
-      </span>
-    </div>
-    <div class="p-2">
-      {#if !selectedBuild?.ProductArtifacts.length}
-        {m.project_products_noArtifacts()}
-      {:else}
-        <table class="table table-auto">
-          <thead>
-            <tr>
-              <th>{m.project_products_filename()}</th>
-              <th>{m.project_products_updated()}</th>
-              <th class="text-right">{m.project_products_size()}</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {#each selectedBuild?.ProductArtifacts as artifact}
-              <tr>
-                <td><IconContainer icon="mdi:file" width="20" /> {artifact.ArtifactType}</td>
-                <td>{getRelativeTime(artifact.DateUpdated)}</td>
-                <td class="text-right">{bytesToHumanSize(artifact.FileSize)}</td>
-                <td class="text-right">
-                  <a href={artifact.Url} download>
-                    <IconContainer icon="mdi:download" width="20" />
-                  </a>
-                </td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      {/if}
-    </div>
-  </div>
+  {#each data.builds as build}
+    <BuildArtifacts {build} latestBuildId={data.product?.WorkflowBuildId}/>
+  {/each}
 </div>
---
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/components/BuildArtifacts.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages';
+  import IconContainer from '$lib/components/IconContainer.svelte';
+  import { getRelativeTime } from '$lib/timeUtils';
+  import { bytesToHumanSize } from '$lib/utils';
+  import ArrowDownIcon from '$lib/icons/ArrowDownIcon.svelte';
+  import ArrowUpIcon from '$lib/icons/ArrowUpIcon.svelte';
+
+  export let build: {
+    Version: string | null;
+    Success: boolean | null;
+    BuildId: number;
+    ProductArtifacts: {
+      ArtifactType: string | null;
+      FileSize: bigint | null;
+      Url: string | null;
+      DateUpdated: Date | null;
+    }[];
+  };
+
+  export let latestBuildId: number | undefined;
+
+  let hidden = false;
+
+  function versionString(b: typeof build): string {
+    let version = b.Version;
+    if (version === null) {
+      if (b.Success === false) {
+        version = ` ${m.projects_buildFailed()} `;
+      } else {
+        version = ` ${m.projects_buildPending()} `;
+      }
+    }
+
+    if (b.BuildId === latestBuildId) {
+      return m.projects_latestBuild({ version });
+    }
+    return version;
+  }
+</script>
+
+<div class="rounded-md border border-slate-400 w-full my-2">
+  <div class="bg-base-300 p-2 flex flex-row rounded-t-md place-content-between">
+    <span class="font-bold text-lg text-info grow">
+      {versionString(build)}
+    </span>
+    <span>
+      <!-- TODO i18n (requires pluralization support) -->
+      {m.project_products_numArtifacts({ amount: build.ProductArtifacts.length })}
+    </span>
+    <button on:click={() => hidden = !hidden}>
+      {#if hidden}
+        <ArrowUpIcon />
+      {:else}
+        <ArrowDownIcon />
+      {/if}
+    </button>
+  </div>
+  <div class="p-2" class:hidden>
+    {#if !build.ProductArtifacts.length}
+      {m.project_products_noArtifacts()}
+    {:else}
+      <table class="table table-auto">
+        <thead>
+          <tr>
+            <th>{m.project_products_filename()}</th>
+            <th>{m.project_products_updated()}</th>
+            <th class="text-right">{m.project_products_size()}</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {#each build.ProductArtifacts as artifact}
+            <tr>
+              <td><IconContainer icon="mdi:file" width="20" /> {artifact.ArtifactType}</td>
+              <td>{getRelativeTime(artifact.DateUpdated)}</td>
+              <td class="text-right">{bytesToHumanSize(artifact.FileSize)}</td>
+              <td class="text-right">
+                <a href={artifact.Url} download>
+                  <IconContainer icon="mdi:download" width="20" />
+                </a>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    {/if}
+  </div>
+</div>
