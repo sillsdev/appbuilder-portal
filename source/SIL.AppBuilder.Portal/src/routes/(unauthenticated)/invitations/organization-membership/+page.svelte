70ed89e60f4c9daa1c96951049c7d043b7ade5fd
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2024-12-01 15:47:52 -0500

    First version of user creation and organization invites

---
+++ source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/invitations/organization-membership/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import * as m from '$lib/paraglide/messages';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+  console.log('client', data);
+</script>
+
+<div class="grid w-full h-full place-items-center place-content-center">
+  <div class="card shadow-xl bg-base-100 border p-4 flex flex-column items-center">
+    {#if data.error === 'not found'}
+      <h2>{m.organizationMembership_invite_error_notFound()}</h2>
+    {:else if data.error === 'redeemed'}
+      <h2>{m.organizationMembership_invite_error_redeemed()}</h2>
+    {:else if data.error === 'expired'}
+      <h2>{m.organizationMembership_invite_error_expired()}</h2>
+    {:else}
+      <!-- TODO: i18n -->
+      <h2>Organization joined!</h2>
+      <!-- Organization info -->
+      <div class="flex flex-row">
+        {#if data.joinedOrganization?.logoUrl}
+          <img src={data.joinedOrganization.logoUrl} alt="Organization logo" />
+        {:else}
+          <div class="inline-block p-2 h-16 w-16 align-middle">
+            <div class="bg-white w-full h-full" />
+          </div>
+        {/if}
+        <div>
+          <h3>{data.joinedOrganization?.name}</h3>
+        </div>
+      </div>
+      <!-- TODO: i18n -->
+      <a href="/tasks" class="btn btn-primary">Go to dashboard</a>
+    {/if}
+  </div>
+</div>
