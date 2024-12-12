a6a1f698243fd8ed34af7a2d7bb4eaea4c3be249
Aidan Jones <aejones4gm@gmail.com>
2024-12-12 13:45:33 -0500

    Paginate /product/[id]/files (#1051)
    * Break bytesToHumanSize to lib/utils
    
    * Display artifacts in product files
    
    * Order builds correctly in display
    
    * Break versionString into local function
    
    * Show multiple, collapsable builds
    
    * Close all but latest build by default
    
    * Fix scrollbar
    
    * Fix vertical layout
    
    * X scroll for tables on mobile
    
    * Paginate product files
    
    * Show three builds per page by default
    
    * Lighten active background on Pagination
    
    * Collapse with click anywhere on header
    
    * Don't collapse BuildArtifacts
    
    * Add temporary fix for pluralization of product files
    
    3 cheers for RegEx

--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.server.ts
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.server.ts
@@ -1,23 +1,97 @@
+import { paginateSchema } from '$lib/table';
+import { error, fail } from '@sveltejs/kit';
 import { prisma } from 'sil.appbuilder.portal.common';
-import type { PageServerLoad } from './$types';
+import { superValidate } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
   const builds = await prisma.productBuilds.findMany({
+    orderBy: [
+      {
+        DateUpdated: 'desc'
+      }
+    ],
     where: {
       ProductId: params.id
     },
-    include: {
-      ProductArtifacts: true
-    }
+    select: {
+      Id: true,
+      Version: true,
+      BuildId: true,
+      Success: true,
+      ProductArtifacts: {
+        select: {
+          ArtifactType: true,
+          Url: true,
+          FileSize: true,
+          DateUpdated: true
+        }
+      }
+    },
+    take: 3
   });
   const product = await prisma.products.findUnique({
     where: {
       Id: params.id
     },
-    include: {
-      ProductDefinition: true,
-      Project: true
+    select: {
+      WorkflowBuildId: true,
+      ProductDefinition: {
+        select: {
+          Name: true
+        }
+      },
+      Project: {
+        select: {
+          Id: true,
+          Name: true
+        }
+      }
     }
   });
-  return { product, builds };
+  return {
+    product,
+    builds,
+    form: await superValidate({ page: 0, size: 3 }, valibot(paginateSchema)),
+    count: await prisma.productBuilds.count({ where: { ProductId: params.id } })
+  };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  page: async ({ request, params, locals }) => {
+    const session = await locals.auth();
+    if (!session) return error(403);
+    const form = await superValidate(request, valibot(paginateSchema));
+    if (!form.valid) return fail(400, { form, ok: false });
+
+    const builds = await prisma.productBuilds.findMany({
+      orderBy: [
+        {
+          DateUpdated: 'desc'
+        }
+      ],
+      where: {
+        ProductId: params.id
+      },
+      select: {
+        Id: true,
+        Version: true,
+        BuildId: true,
+        Success: true,
+        ProductArtifacts: {
+          select: {
+            ArtifactType: true,
+            Url: true,
+            FileSize: true,
+            DateUpdated: true
+          }
+        }
+      },
+      skip: form.data.page * form.data.size,
+      take: form.data.size
+    });
+
+    return { form, ok: true, query: { data: builds }}
+  }
+} satisfies Actions;
--- source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(authenticated)/products/[id]/files/+page.svelte
@@ -1,63 +1,66 @@
 <script lang="ts">
   import IconContainer from '$lib/components/IconContainer.svelte';
+  import Pagination from '$lib/components/Pagination.svelte';
   import { getIcon } from '$lib/icons/productDefinitionIcon';
   import * as m from '$lib/paraglide/messages';
-  import { getTimeDateString } from '$lib/timeUtils';
+  import { writable } from 'svelte/store';
+  import { superForm, type FormResult } from 'sveltekit-superforms';
   import type { PageData } from './$types';
-  import { bytesToHumanSize } from '$lib/utils';
+  import BuildArtifacts from './components/BuildArtifacts.svelte';
 
   export let data: PageData;
-  let selectedBuildId: number;
-  $: selectedBuild = data.builds.find((b) => b.Id === selectedBuildId);
-  // TODO: test this page with artifacts
+
+  const builds = writable(data.builds);
+
+  const { form, enhance, submit } = superForm(data.form, {
+    resetForm: false,
+    onChange(event) {
+      submit();
+    },
+    onUpdate(event) {
+      const data = event.result.data as FormResult<{
+        query: { data: any[] };
+      }>;
+      if (event.form.valid && data.query) {
+        builds.set(data.query.data);
+      }
+    }
+  });
 </script>
 
-<div class="w-full max-w-6xl mx-auto p-4">
-  <h1>{m.products_files_title()}: {data.product?.Project.Name}</h1>
-  <select class="select" bind:value={selectedBuildId}>
-    {#if data.builds.length === 0}
-      <option disabled selected value="-1">
-        {m.projects_noBuilds()}
-      </option>
-    {/if}
-    {#each data.builds as build}
-      <option value={build.Id}>{build.BuildId}</option>
-    {/each}
-  </select>
-  <div class="rounded-md border border-slate-400 w-full my-2">
-    <div class="bg-base-300 p-2 flex flex-row rounded-t-md place-content-between">
-      <span>
-        <IconContainer icon={getIcon(data.product?.ProductDefinition.Name ?? '')} width="24" />
-        {data.product?.ProductDefinition.Name}
-      </span>
-      <span>
-        <!-- TODO i18n (requires pluralization support) -->
-        {m.project_products_numArtifacts({ amount: selectedBuild?.ProductArtifacts.length })}
-      </span>
+<div class="w-full h-full max-w-6xl mx-auto p-4 flex flex-col">
+  <div>
+    <div class="breadcrumbs text-sm pl-4">
+      <ul>
+        <li>
+          <a class="link" href="/projects/{data.product?.Project.Id}">
+            {data.product?.Project.Name}
+          </a>
+        </li>
+        <li>
+          <IconContainer
+            icon={getIcon(data.product?.ProductDefinition.Name ?? '')}
+            width="24"
+          />{data.product?.ProductDefinition.Name}
+        </li>
+      </ul>
     </div>
-    <div class="p-2">
-      {#if selectedBuild?.ProductArtifacts.length ?? 0 === 0}
-        {m.project_products_noArtifacts()}
-      {:else}
-        <table>
-          <thead>
-            <tr>
-              <th>{m.project_products_filename()}</th>
-              <th>{m.project_products_updated}</th>
-              <th>{m.project_products_size()}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each selectedBuild?.ProductArtifacts ?? [] as artifact}
-              <tr>
-                <td><IconContainer icon="mdi:file" width="20" /> {artifact.ArtifactType}</td>
-                <td>{getTimeDateString(artifact.DateUpdated)}</td>
-                <td>{bytesToHumanSize(artifact.FileSize ?? BigInt(0))}</td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      {/if}
-    </div>
+    <h1 class="pl-4">{m.products_files_title()}</h1>
   </div>
+  <div id="files" class="overflow-y-auto grow">
+    {#each $builds as build}
+      <BuildArtifacts
+        {build}
+        latestBuildId={data.product?.WorkflowBuildId}
+      />
+    {/each}
+  </div>
+  <form method="POST" action="?/page" use:enhance>
+    <Pagination
+      bind:page={$form.page}
+      bind:size={$form.size}
+      total={data.count}
+      extraSizeOptions={[3]}
+    />
+  </form>
 </div>
