50e442ae66df9cd641ca5171b3ca21fd0f55a1bf
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2025-02-01 16:16:28 -0500

    Remove landing page in favor of login page

--- portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
+++ portal/src/routes/(unauthenticated)/(auth)/login/+page.svelte
@@ -1,44 +1,5 @@
 <script>
-  import ScriptoriaIcon from '$lib/icons/ScriptoriaIcon.svelte';
-  import { m } from '$lib/paraglide/messages';
-  import { SignIn } from '@auth/sveltekit/components';
+  import LoginScreen from './LoginScreen.svelte';
 </script>
 
-<div class="card shadow-xl bg-base-100 border p-4">
-  <div class="w-full flex justify-center">
-    <div class="w-10"></div>
-    <ScriptoriaIcon size="128" />
-  </div>
-  <h1 class="text-center mx-4 mt-0 pt-2">{m.welcome()}</h1>
-  <div class="flex flex-row justify-evenly">
-    <SignIn provider="auth0" signInPage="login" class="inline-block">
-      {#snippet submitButton()}
-        <!-- TODO: i18n? -->
-        <div class="btn btn-primary w-full mx-auto">Sign In</div>
-      {/snippet}
-    </SignIn>
-    <SignIn
-      provider="auth0"
-      signInPage="login"
-      class="inline-block"
-      authorizationParams={{ prompt: 'login', screen_hint: 'signup' }}
-    >
-      {#snippet submitButton()}
-        <div class="btn w-full mx-auto btn-secondary">Sign In with new session</div>
-      {/snippet}
-    </SignIn>
-  </div>
-</div>
-<br />
-<a href="https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss">
-  <img
-    src="https://cdn.auth0.com/oss/badges/a0-badge-light.png"
-    alt="Auth0 badge"
-    class="w-1/2 mx-auto dark:hidden shadow-xl rounded-xl"
-  />
-  <img
-    src="https://cdn.auth0.com/oss/badges/a0-badge-dark.png"
-    alt="Auth0 badge"
-    class="w-1/2 mx-auto hidden dark:block shadow-xl rounded-sm"
-  />
-</a>
+<LoginScreen />
---
+++ portal/src/routes/(unauthenticated)/(auth)/login/LoginScreen.svelte
@@ -0,0 +1,28 @@
+<script>
+  import ScriptoriaIcon from '$lib/icons/ScriptoriaIcon.svelte';
+  import * as m from '$lib/paraglide/messages';
+  import { SignIn } from '@auth/sveltekit/components';
+</script>
+
+<div class="card shadow-xl bg-base-100 border p-4">
+  <div class="w-full flex justify-center">
+    <div class="w-10" />
+    <ScriptoriaIcon size="128" />
+  </div>
+  <h1 class="text-center mx-4 mt-0 pt-2">{m.welcome()}</h1>
+  <div class="flex flex-row justify-evenly">
+    <SignIn provider="auth0" signInPage="login" class="inline-block">
+      <div slot="submitButton" class="btn btn-primary w-full mx-auto">{m.auth_login()}</div>
+    </SignIn>
+    <SignIn
+      provider="auth0"
+      signInPage="login"
+      class="inline-block"
+      authorizationParams={{ prompt: 'login', screen_hint: 'signup' }}
+    >
+      <div slot="submitButton" class="btn w-full mx-auto btn-secondary">
+        {m.auth_loginNewSession()}
+      </div>
+    </SignIn>
+  </div>
+</div>
