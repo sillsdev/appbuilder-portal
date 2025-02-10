ac26d4642cd27ff7639ede5ce9c77545215204ae
Aidan Jones <aejones4gm@gmail.com>
2025-02-10 16:11:12 -0500

    Replace svelte-flatpickr with Pikaday
    Pikaday will be supported by DaisyUI 5. In the meantime, the styling here is not ideal, but is at least doable.
    
    As best as I can tell, this works mostly as expected. Pikaday does not support single-calendar date range selection out of the box like flatpickr does, but an attempt was made here to approximate the same functionality.

--- portal/src/lib/components/DateRangePicker.svelte
+++ portal/src/lib/components/DateRangePicker.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
-  import Flatpickr from 'svelte-flatpickr';
+  import Pikaday from '@handsontable/pikaday';
   // This component is here because the range mode of the Flatpickr svelte package
   // is a bit broken. Essentially, the 'value' property is updated when the second
   // date is clicked but not reassigned, only when the first is and it is set to a new value.
@@ -8,15 +7,7 @@
   // TODO: add an obvious clear button to the date picker. Right now the best way to clear
   // the filter is reloading the page, or choosing a first date and not a second one
 
-  // Note: At the moment, the site always follows prefered color scheme
-  // If at some point in the future a manual toggle is added, this will need to be changed
-  let isDarkMode: boolean = $state(browser
-    ? window.matchMedia('(prefers-color-scheme: dark)').matches
-    : false);
-  browser &&
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-      isDarkMode = event.matches;
-    });
+  // based on: https://codepen.io/melenie/pen/gObPeGx
 
   interface Props {
     chosenDates?: [Date, Date | null] | null;
@@ -24,45 +15,147 @@
   }
 
   let { chosenDates = $bindable(null), placeholder = '' }: Props = $props();
+
+  let startInput: HTMLInputElement | undefined = undefined;
+  let endInput: HTMLInputElement | undefined = undefined;
+  // svelte-ignore non_reactive_update
+  let startPicker: Pikaday | undefined = undefined;
+  let endPicker: Pikaday | undefined = undefined;
+  let pickContainer: HTMLDivElement | undefined = undefined;
+  let targeting: boolean = false;
+  let open: boolean = $state(false);
+  function buildDatePicker(startInput: HTMLInputElement, endInput: HTMLInputElement) {
+    startPicker = new Pikaday({
+      bound: false,
+      field: startInput,
+      container: pickContainer,
+      maxDate: new Date(),
+      onSelect: () => {
+        updateStartDate(startPicker?.getDate() ?? null);
+      }
+    });
+    startPicker.hide();
+    endPicker = new Pikaday({
+      bound: false,
+      field: endInput,
+      container: pickContainer,
+      maxDate: new Date(),
+      onSelect: () => {
+        updateEndDate(endPicker?.getDate() ?? null);
+      }
+    });
+    endPicker.hide();
+    return () => {
+      startPicker?.destroy();
+      endPicker?.destroy();
+    };
+  }
+  function updateStartDate(selectedDate: Date | null) {
+    if (startPicker && endPicker && selectedDate) {
+      startPicker.hide();
+      endPicker.setMinDate(selectedDate);
+      endPicker.setStartRange(selectedDate);
+      endPicker.gotoDate(selectedDate);
+      setEndRange(selectedDate);
+      endPicker.show();
+    }
+  }
+  function updateEndDate(selectedDate: Date | null) {
+    if (startPicker && endPicker && selectedDate) {
+      setEndRange(selectedDate);
+      endPicker.hide();
+      open = false;
+      chosenDates = [
+        startPicker?.getDate() ?? new Date(),
+        // Add one day to include the final day
+        endPicker?.getDate()
+          ? new Date(endPicker!.getDate()!.getTime() + 1000 * 60 * 60 * 24)
+          : null
+      ];
+    }
+  }
+  function setEndRange(endDate: Date | null) {
+    endPicker?.setEndRange(endDate);
+    endPicker?.draw(true);
+  }
+
+  $effect(() => {
+    if (startInput && endInput && pickContainer) {
+      return buildDatePicker(startInput, endInput);
+    }
+  });
 </script>
 
 <svelte:head>
   <!-- NOTE: this is an external cdn... probably ok -->
-  {#if isDarkMode}
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://npmcdn.com/flatpickr/dist/themes/dark.css"
-    />
-  {/if}
-</svelte:head>
-<div class="dateRangePicker">
-  <Flatpickr
-    options={{
-      mode: 'range',
-      altInput: true,
-      altFormat: 'M j, Y',
-      static: true
-    }}
-    class="input input-bordered w-full min-w-40"
-    {placeholder}
-    on:change={(v) =>
-      (chosenDates = [
-        v.detail[0][0],
-        // Add one day to include the final day
-        v.detail[0][1] ? new Date(v.detail[0][1].getTime() + 1000 * 60 * 60 * 24) : null
-      ])}
+  <!-- TODO: remove once upgraded to DaisyUI 5 -->
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@handsontable/pikaday/css/pikaday.css"
   />
-</div>
+</svelte:head>
+<svelte:body
+  onmousemove={(e) => {
+    if (e.target instanceof HTMLElement) {
+      if (!e.target?.classList.contains('pika-button')) {
+        if (targeting) {
+          targeting = false;
+          setEndRange(endPicker?.getDate() ?? null);
+        }
+      } else {
+        targeting = true;
+        const pikaBtn = e.target!;
+        const pikaDate = new Date(
+          parseInt(pikaBtn.getAttribute('data-pika-year') ?? ''),
+          parseInt(pikaBtn.getAttribute('data-pika-month') ?? ''),
+          parseInt(pikaBtn.getAttribute('data-pika-day') ?? '')
+        );
+        setEndRange(pikaDate);
+      }
+    }
+  }}
+/>
+<details class="dropdown" bind:open>
+  <summary
+    class="input input-bordered min-w-40 flex flex-row flex-wrap w-fit min-h-fit"
+    onclick={() => {
+      if (!open) {
+        setEndRange(null);
+        startPicker?.setDate(null);
+        endPicker?.setDate(null);
+        open = true;
+        endPicker?.hide();
+        startPicker?.show();
+      } else {
+        open = false;
+        chosenDates = null;
+      }
+    }}
+  >
+    <input class="cal-input" type="text" readonly bind:this={startInput} {placeholder} />
+    <input class="cal-input" type="text" readonly bind:this={endInput} />
+  </summary>
+  <div
+    class="cal-container dropdown-content bg-base-100 rounded-box z-[1] w-fit p-2 shadow"
+    bind:this={pickContainer}
+  ></div>
+</details>
 
-<style>
-  .dateRangePicker {
-    flex: 1;
+<style lang="postcss">
+  .cal-container :global(.is-inrange .pika-button),
+  :global(.pika-button:hover) {
+    @apply border-accent bg-base-300 text-base-content;
   }
-  :global(.dateRangePicker .flatpickr-wrapper) {
-    width: 100%;
+  .cal-container :global(.is-startrange .pika-button),
+  :global(.is-selected .pika-button) {
+    @apply border-accent bg-accent;
   }
-  :global(.dateRangePicker .flatpickr-calendar) {
-    right: 0;
+  .cal-input {
+    margin: 4px 0;
+    width: fit-content;
+  }
+  .cal-input:first-child {
+    margin-right: 4px;
   }
 </style>
