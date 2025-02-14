674fd9af15fac9547f044fc2ef263d79b841bc78
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2025-02-13 22:28:50 -0500

    Final login screen updates

--- portal/src/routes/(unauthenticated)/(auth)/signout/+page.svelte
+++ portal/src/routes/(unauthenticated)/(auth)/signout/+page.svelte
@@ -3,8 +3,8 @@
   import { SignOut } from '@auth/sveltekit/components';
 </script>
 
-<div class="card bg-slate-50 border p-4 dark:bg-slate-800">
-  <h2 class="text-center">{m.header_signOut()} and return to home page</h2>
+<div class="card bg-white border p-4">
+  <h2 class="text-center text-black">{m.header_signOut()} and return to home page</h2>
   <SignOut signOutPage="logout" class="mx-auto">
     {#snippet submitButton()}
       <div class="btn w-full mx-auto">{m.header_signOut()}</div>
--- portal/src/routes/+page.svelte
+++ portal/src/routes/+page.svelte
@@ -2,6 +2,6 @@
   import LoginScreen from './(unauthenticated)/(auth)/login/LoginScreen.svelte';
 </script>
 
-<div class="grid w-full h-full place-items-center place-content-center">
+<div class="grid w-full h-full place-items-center place-content-center [background-color:#0068a6]">
   <LoginScreen />
 </div>
