bed920092353835f43186b722b209f111279a3c2
Aidan Jones <aejones4gm@gmail.com>
2025-03-12 14:59:58 -0400

    Add artifact filters from DWKit (#1077)
    * Limit which artifacts are queried for tasks
    
    Use filters from DWKit:
    - ProductLatestAAB
      - Used for CreateAppStoreEntry
    - ProductLatestAPK
      - Not used anywhere?
    - ProductLatestAssetPackage
      - Used for VerifyAndPublish only for Asset Packages
    
    * Fix conditional data inclusion in task page
    
    * Remove latestAPK
    
    It is not used anymore
    
    ---------
    
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

--- portal/common/public/workflow.ts
+++ portal/common/public/workflow.ts
@@ -98,10 +98,23 @@ export type WorkflowInstanceContext = {
     | 'projectLanguageCode'
   )[];
   includeReviewers: boolean;
-  includeArtifacts: 'apk' | 'aab' | boolean;
+  includeArtifacts: ArtifactLists | null;
   start?: WorkflowState;
   environment: Environment;
 };
+
+export type ArtifactLists = 'latestAAB' | 'latestAssetPackage' | 'all';
+
+export function artifactLists(listName: ArtifactLists) {
+  switch (listName) {
+  case 'latestAAB':
+    return ['aab', 'data-safety-csv'];
+  case 'latestAssetPackage':
+    return ['asset-package', 'asset-preview'];
+  case 'all':
+    return [];
+  }
+}
 
 export type Environment = { [key: ENVKeys | string]: string };
 
