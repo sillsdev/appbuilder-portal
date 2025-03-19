def11f30f5fc96db71c98d99e42a900d121815df
Aidan Jones <aejones4gm@gmail.com>
2025-03-19 11:11:05 -0400

    Handle when json parse in request fails
    This happens when BuildEngine is down entirely

--- source/SIL.AppBuilder.Portal/common/build-engine-api/requests.ts
+++ source/SIL.AppBuilder.Portal/common/build-engine-api/requests.ts
@@ -19,20 +19,18 @@ export async function request(
       }
     });
     if (!check?.SystemAvailable && resource !== 'system/check') {
-      return new Response(
-        JSON.stringify({
+      return {
+        ok: false,
+        status: 500,
+        json: {
           responseType: 'error',
           name: '',
           status: 500,
           code: 500,
           message: `System ${url} unavailable`,
           type: ''
-        } as Types.ErrorResponse),
-        {
-          status: 500,
-          statusText: 'Internal Server Error'
-        }
-      );
+        } as Types.ErrorResponse
+      };
     }
     return await fetch(`${url}/${resource}`, {
       method: method,
@@ -42,22 +40,24 @@ export async function request(
         'Content-Type': 'application/json'
       },
       body: body ? JSON.stringify(body) : undefined
-    });
+    }).then((r) => ({
+      ok: r.ok,
+      status: r.status,
+      json: r.json()
+    }));
   } catch (e) {
-    return new Response(
-      JSON.stringify({
+    return {
+      ok: false,
+      status: 500,
+      json: {
         responseType: 'error',
         name: '',
         status: 500,
         code: 500,
         message: typeof e === 'string' ? e.toUpperCase() : e instanceof Error ? e.message : e,
         type: ''
-      } as Types.ErrorResponse),
-      {
-        status: 500,
-        statusText: 'Internal Server Error'
-      }
-    );
+      } as Types.ErrorResponse
+    };
   }
 }
 export async function getURLandToken(organizationId: number) {
@@ -78,20 +78,20 @@ export async function getURLandToken(organizationId: number) {
 
   return org.UseDefaultBuildEngine
     ? {
-      url: process.env.DEFAULT_BUILDENGINE_URL,
-      token: process.env.DEFAULT_BUILDENGINE_API_ACCESS_TOKEN
-    }
+        url: process.env.DEFAULT_BUILDENGINE_URL,
+        token: process.env.DEFAULT_BUILDENGINE_API_ACCESS_TOKEN
+      }
     : {
-      url: org.BuildEngineUrl,
-      token: org.BuildEngineApiAccessToken
-    };
+        url: org.BuildEngineUrl,
+        token: org.BuildEngineApiAccessToken
+      };
 }
 
 export async function systemCheck(auth: Types.Auth) {
   const res = await request('system/check', auth);
   return res.ok
     ? ({ status: res.status } as Types.StatusResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    : ((await res.json) as Types.ErrorResponse);
 }
 
 export async function createProject(
@@ -100,16 +100,16 @@ export async function createProject(
 ): Promise<Types.ProjectResponse | Types.ErrorResponse> {
   const res = await request('project', auth, 'POST', project);
   return res.ok
-    ? ((await res.json()) as Types.ProjectResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.ProjectResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getProjects(
   auth: Types.Auth
 ): Promise<Types.ProjectResponse[] | Types.ErrorResponse> {
   const res = await request('project', auth);
   return res.ok
-    ? ((await res.json()) as Types.ProjectResponse[])
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.ProjectResponse[])
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getProject(
   auth: Types.Auth,
@@ -117,8 +117,8 @@ export async function getProject(
 ): Promise<Types.ProjectResponse | Types.ErrorResponse> {
   const res = await request(`project/${projectId}`, auth);
   return res.ok
-    ? ((await res.json()) as Types.ProjectResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.ProjectResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function deleteProject(
   auth: Types.Auth,
@@ -127,7 +127,7 @@ export async function deleteProject(
   const res = await request(`project/${projectId}`, auth, 'DELETE');
   return res.ok
     ? { responseType: 'delete', status: res.status }
-    : ((await res.json()) as Types.ErrorResponse);
+    : ((await res.json) as Types.ErrorResponse);
 }
 
 export async function getProjectAccessToken(
@@ -137,8 +137,8 @@ export async function getProjectAccessToken(
 ): Promise<Types.TokenResponse | Types.ErrorResponse> {
   const res = await request(`project/${projectId}/token`, auth, 'POST', token);
   return res.ok
-    ? ((await res.json()) as Types.TokenResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.TokenResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 
 export async function createJob(
@@ -147,16 +147,16 @@ export async function createJob(
 ): Promise<Types.JobResponse | Types.ErrorResponse> {
   const res = await request('job', auth, 'POST', job);
   return res.ok
-    ? ((await res.json()) as Types.JobResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.JobResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getJobs(
   auth: Types.Auth
 ): Promise<Types.JobResponse[] | Types.ErrorResponse> {
   const res = await request('job', auth);
   return res.ok
-    ? ((await res.json()) as Types.JobResponse[])
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.JobResponse[])
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getJob(
   auth: Types.Auth,
@@ -164,8 +164,8 @@ export async function getJob(
 ): Promise<Types.JobResponse | Types.ErrorResponse> {
   const res = await request(`job/${jobId}`, auth);
   return res.ok
-    ? ((await res.json()) as Types.JobResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.JobResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function deleteJob(
   auth: Types.Auth,
@@ -174,7 +174,7 @@ export async function deleteJob(
   const res = await request(`job/${jobId}`, auth, 'DELETE');
   return res.ok
     ? { responseType: 'delete', status: res.status }
-    : ((await res.json()) as Types.ErrorResponse);
+    : ((await res.json) as Types.ErrorResponse);
 }
 
 export async function createBuild(
@@ -184,8 +184,8 @@ export async function createBuild(
 ): Promise<Types.BuildResponse | Types.ErrorResponse> {
   const res = await request(`job/${jobId}/build`, auth, 'POST', build);
   return res.ok
-    ? ((await res.json()) as Types.BuildResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.BuildResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getBuild(
   auth: Types.Auth,
@@ -194,8 +194,8 @@ export async function getBuild(
 ): Promise<Types.BuildResponse | Types.ErrorResponse> {
   const res = await request(`job/${jobId}/build/${buildId}`, auth);
   return res.ok
-    ? ((await res.json()) as Types.BuildResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.BuildResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getBuilds(
   auth: Types.Auth,
@@ -203,8 +203,8 @@ export async function getBuilds(
 ): Promise<Types.BuildResponse[] | Types.ErrorResponse> {
   const res = await request(`job/${jobId}/build`, auth);
   return res.ok
-    ? ((await res.json()) as Types.BuildResponse[])
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.BuildResponse[])
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function deleteBuild(
   auth: Types.Auth,
@@ -214,7 +214,7 @@ export async function deleteBuild(
   const res = await request(`job/${jobId}/build/${buildId}`, auth, 'DELETE');
   return res.ok
     ? { responseType: 'delete', status: res.status }
-    : ((await res.json()) as Types.ErrorResponse);
+    : ((await res.json) as Types.ErrorResponse);
 }
 
 export async function createRelease(
@@ -225,8 +225,8 @@ export async function createRelease(
 ): Promise<Types.ReleaseResponse | Types.ErrorResponse> {
   const res = await request(`job/${jobId}/build/${buildId}`, auth, 'PUT', release);
   return res.ok
-    ? ((await res.json()) as Types.ReleaseResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.ReleaseResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function getRelease(
   auth: Types.Auth,
@@ -236,8 +236,8 @@ export async function getRelease(
 ): Promise<Types.ReleaseResponse | Types.ErrorResponse> {
   const res = await request(`job/${jobId}/build/${buildId}/release/${releaseId}`, auth);
   return res.ok
-    ? ((await res.json()) as Types.ReleaseResponse)
-    : ((await res.json()) as Types.ErrorResponse);
+    ? ((await res.json) as Types.ReleaseResponse)
+    : ((await res.json) as Types.ErrorResponse);
 }
 export async function deleteRelease(
   auth: Types.Auth,
@@ -248,5 +248,5 @@ export async function deleteRelease(
   const res = await request(`job/${jobId}/build/${buildId}/release/${releaseId}`, auth, 'DELETE');
   return res.ok
     ? { responseType: 'delete', status: res.status }
-    : ((await res.json()) as Types.ErrorResponse);
+    : ((await res.json) as Types.ErrorResponse);
 }
