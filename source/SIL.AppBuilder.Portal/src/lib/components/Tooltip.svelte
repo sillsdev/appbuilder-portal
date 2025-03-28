7e22eda4b651511d5494e2b062ce6f35fc522e6c
Aidan Jones <aejones4gm@gmail.com>
2025-03-28 10:17:02 -0400

    Fix tooltip to properly display m.directory_searchHelp() (#1096)

--- source/SIL.AppBuilder.Portal/src/lib/components/Tooltip.svelte
+++ source/SIL.AppBuilder.Portal/src/lib/components/Tooltip.svelte
@@ -2,21 +2,24 @@
   import type { Snippet } from 'svelte';
   
   interface Props {
-    tip: string | null | undefined;
-    className?: string; // TODO: Add snippet support once we have DaisyUI 5
+    tip?: string | null | undefined;
+    className?: string;
+    // tooltip-content can also be handled in the children snippet
     children?: Snippet;
   }
 
   let { tip, className = '', children }: Props = $props();
-  
 </script>
 
-<span class="tooltip [--tt-bg:#FFFFFF] dark:tooltip-secondary {className}" data-tip={tip}>
+<span
+  class="tooltip [--tt-bg:var(--color-white)] dark:tooltip-secondary {className}"
+  data-tip={tip}
+>
   {@render children?.()}
 </span>
 
 <style>
-  .tooltip::before {
+  .tooltip::before, .tooltip :global(.tooltip-content) {
     box-shadow: var(--shadow-md);
   }
 </style>
