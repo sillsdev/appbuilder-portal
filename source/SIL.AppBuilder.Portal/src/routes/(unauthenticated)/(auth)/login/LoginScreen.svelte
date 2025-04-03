61a26ed220b0c7f114534aa3a70f5e824ee32b0c
Aidan Jones <aejones4gm@gmail.com>
2025-04-03 12:15:39 -0400

    Fix missing i18n from rebase

--- source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/(auth)/login/LoginScreen.svelte
+++ source/SIL.AppBuilder.Portal/src/routes/(unauthenticated)/(auth)/login/LoginScreen.svelte
@@ -12,14 +12,12 @@
   <h1 class="text-center mx-4 py-0 pt-2 text-black">{m.welcome()}</h1>
   <div class="flex flex-col justify-evenly space-y-2">
     <p class="text-black m-4 text-center w-80">
-      Scriptoria helps you publish your apps and keep them updated every time the App Builder
-      software is improved. You provide the service with your app project data and it will build and
-      publish the app to the Google Play store or a website.
+      {m.about()}
     </p>
     <SignIn provider="auth0" signInPage="login" class="signin-button">
       <div slot="submitButton" class="btn btn-primary w-full mx-auto">{m.auth_login()}</div>
     </SignIn>
-    <p class="text-gray-700 text-sm text-center">or</p>
+    <p class="text-gray-700 text-sm text-center">{m.common_or()}</p>
     <SignIn
       provider="auth0"
       signInPage="login"
