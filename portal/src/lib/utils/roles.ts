9a982bc1bd62f26a6dd03ed600d80dc5ad0cd0fe
Aidan Jones <aejones4gm@gmail.com>
2025-04-04 10:51:43 -0400

    Fix logic error in roles utility functions
    I. .... should have been more careful...
    
    I mixed up which index of the roles array was the OrganizationId and which was the RoleId, the very same problem these functions were originally designed to solve. Anyways, they are correct now.

--- portal/src/lib/utils/roles.ts
+++ portal/src/lib/utils/roles.ts
@@ -1,46 +1,46 @@
 import { RoleId } from "sil.appbuilder.portal.common/prisma";
 
 /** returns true if user is a SuperAdmin, or is an OrgAdmin for the specified organization
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
  */
 export function isAdminForOrg(orgId: number, roles?: [number, number][]): boolean {
   return !!roles?.find(
-    (r) => r[0] === RoleId.SuperAdmin || (r[0] === RoleId.OrgAdmin && r[1] === orgId)
+    ([org, role]) => role === RoleId.SuperAdmin || (role === RoleId.OrgAdmin && org === orgId)
   );
 }
 /** returns true if user is a SuperAdmin, or is an OrgAdmin for any of the provided orgs
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
  */
 export function isAdminForOrgs(orgs: number[], roles?: [number, number][]): boolean {
   return !!roles?.find(
-    (r) => r[0] === RoleId.SuperAdmin || (r[0] === RoleId.OrgAdmin && orgs.includes(r[1]))
+    ([org, role]) => role === RoleId.SuperAdmin || (role === RoleId.OrgAdmin && orgs.includes(org))
   );
 }
 /** returns true if user is a SuperAdmin, or is an OrgAdmin for any organization
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
  */
 export function isAdmin(roles?: [number, number][]): boolean {
-  return !!roles?.find((r) => r[0] === RoleId.SuperAdmin || r[0] === RoleId.OrgAdmin);
+  return !!roles?.find((r) => r[1] === RoleId.SuperAdmin || r[1] === RoleId.OrgAdmin);
 }
 /** returns true if user is a SuperAdmin
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
  */
 export function isSuperAdmin(roles?: [number, number][]): boolean {
-  return !!roles?.find((r) => r[0] === RoleId.SuperAdmin);
+  return !!roles?.find((r) => r[1] === RoleId.SuperAdmin);
 }
 /** returns true if user has specified role in specified org
  * 
  * IS NOT SHORT-CIRCUITED by SuperAdmin
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
  */
 export function hasRoleForOrg(role: RoleId, orgId: number, roles?: [number, number][]): boolean {
-  return !!roles?.find((r) => r[0] === role && r[1] === orgId);
+  return !!roles?.find((r) => r[1] === role && r[0] === orgId);
 }
 /** returns a list of organizations where the user has the specified role 
  * 
  * IS NOT SHORT-CIRCUITED by SuperAdmin
- * @param roles [RoleId, OrganizationId][]
+ * @param roles [OrganizationId, RoleId][]
 */
 export function orgsForRole(role: RoleId, roles?: [number, number][]): number[] {
-  return roles?.filter((r) => r[0] === role).map((r) => r[1]) ?? [];
+  return roles?.filter((r) => r[1] === role).map((r) => r[0]) ?? [];
 }
