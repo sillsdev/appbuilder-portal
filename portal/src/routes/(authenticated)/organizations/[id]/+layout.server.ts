c17b6138bde59bc0e467559774fd5862ac7b19ff
Aidan Jones <aejones4gm@gmail.com>
2025-04-11 14:04:52 -0400

    Remove unnecessary perms check

--- portal/src/routes/(authenticated)/organizations/[id]/+layout.server.ts
+++ portal/src/routes/(authenticated)/organizations/[id]/+layout.server.ts
@@ -1,13 +1,12 @@
 import { localizeHref } from '$lib/paraglide/runtime';
-import { isAdminForOrg } from '$lib/utils/roles';
 import { redirect } from '@sveltejs/kit';
 import { prisma } from 'sil.appbuilder.portal.common';
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async (event) => {
   const id = parseInt(event.params.id);
-  const user = (await event.locals.auth())!.user;
-  if (isNaN(id) || !isAdminForOrg(id, user.roles)) return redirect(302, localizeHref('/organizations'));
+  // perms checked in auth.ts
+  if (isNaN(id)) return redirect(302, localizeHref('/organizations'));
   const organization = await prisma.organizations.findUnique({
     where: {
       Id: id
