8d1cf8bf5574c14577001ac511b8a61b477bdeec
Aidan Jones <aejones4gm@gmail.com>
2025-04-28 13:57:41 -0400

    Add database heartbeat check to hooks

--- portal/src/hooks.server.ts
+++ portal/src/hooks.server.ts
@@ -1,7 +1,10 @@
 // hooks.server.ts
+import { localizeHref } from '$lib/paraglide/runtime';
 import { paraglideMiddleware } from '$lib/paraglide/server';
-import type { Handle } from '@sveltejs/kit';
+import { Prisma } from '@prisma/client';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { prisma } from 'sil.appbuilder.portal.common';
 import {
   authRouteHandle,
   checkUserExistsHandle,
@@ -11,17 +14,42 @@ import {
 
 // creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
-		event.request = localizedRequest;
-		return resolve(event, {
-			transformPageChunk: ({ html }) => {
-				return html.replace('%lang%', locale);
-			}
-		});
-	});
+  paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+    event.request = localizedRequest;
+    return resolve(event, {
+      transformPageChunk: ({ html }) => {
+        return html.replace('%lang%', locale);
+      }
+    });
+  });
+
+const heartbeat: Handle = async ({ event, resolve }) => {
+  // this check is important to prevent infinite redirects...
+  if (
+    !(
+      event.route.id === '/(unauthenticated)/error' ||
+      event.route.id === '/(unauthenticated)/(auth)/login'
+    )
+  ) {
+    try {
+      const test = await prisma.userTasks.findFirst({ select: { Id: true } });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        // As best as I can tell, the only types of PrismaClientKnownRequestError that
+        // should be thrown by the above query would involve the database being unreachable.
+        // HTTP 503 *should* be the correct semantics here?
+        return redirect(302, localizeHref(`/error?code=503`));
+      } else {
+        throw e;
+      }
+    }
+  }
+  return resolve(event);
+};
 
 export const handle: Handle = sequence(
   paraglideHandle,
+  heartbeat,
   organizationInviteHandle,
   authRouteHandle,
   checkUserExistsHandle,
--- portal/src/routes/+error.svelte
+++ portal/src/routes/+error.svelte
@@ -6,13 +6,19 @@
 
 <div class="mx-auto px-2 w-full md:w-1/2 lg:w-1/3">
   {#if page.status === 403}
-    <p>{m.errors_friendlyForbidden()}</p>
+    <p class="pt-20">{m.errors_friendlyForbidden()}</p>
   {:else if page.status === 404}
     <h1 class="pl-0">{m.errors_notFoundTitle()}</h1>
     <p>{m.errors_notFoundDescription()}</p>
+  {:else if page.status === 503}
+    <p class="pt-20">{m.errors_appUnavailable()}</p>
   {:else}
     <h1>{page.status}: {page.error?.message}</h1>
   {/if}
-  <hr class="my-2">
-  <a class="btn btn-primary" href={localizeHref('/tasks')}>{m.home()}</a>
+  <hr class="my-2" />
+  {#if page.status !== 503}
+    <a class="btn btn-primary" href={localizeHref('/tasks')}>{m.home()}</a>
+  {:else}
+    <a class="btn btn-primary" href={localizeHref('/login')}>{m.auth_login()}</a>
+  {/if}
 </div>
