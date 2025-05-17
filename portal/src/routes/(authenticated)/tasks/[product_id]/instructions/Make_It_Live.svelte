58f0e66d9274e0fe389e869592d83bd0fd529bf5
Chris Hubbard <chris@thehubbards.org>
2025-05-17 21:04:49 +0200

    Update Google Play Forms to conform to current GP (#1138)

--- portal/src/routes/(authenticated)/tasks/[product_id]/instructions/Make_It_Live.svelte
+++ portal/src/routes/(authenticated)/tasks/[product_id]/instructions/Make_It_Live.svelte
@@ -1,39 +1,100 @@
 <h3>Instructions</h3>
-<p>
-  For apps being added to Scriptoria that are already published to Google Play, most of these steps have already been completed. You will still need to perform the last step to complete the rollout of the draft publish of the new version of
-  the app.
-</p>
+<p>For apps being added to Scriptoria that are already published to Google Play, most of these steps have already been completed. You will still need to perform the last step to complete the rollout of the draft publish of the new version of the app.</p>
 <ul>
-  <li>Go to <a href="https://play.google.com/apps/publish/" target="_blank">Google Play Developer Console</a> and sign in.</li>
-  <li>Select <strong>Store presence</strong> under the <strong>Grow</strong> section in the menu on the left. This will expand the items below it.</li>
-  <li>Select <strong>Store settings</strong> in the menu on the left and do the following:</li>
   <li>
-    <ul>
-      <li>
-        Set <strong>Category</strong> to <em>Books &amp; Reference</em> (or appropriate category)
-        <ul>
-          <li>Click on <strong>Manage tags</strong> link and set appropriate tags (e.g. Religious text) and click the <strong>Apply</strong> button at the bottom of the page.</li>
-          <li>Set <strong>Email address</strong></li>
-          <li>Set <strong>Website</strong> (optional)</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-        </ul>
-      </li>
-    </ul>
+    Go to <a href="https://play.google.com/apps/publish/" target="_blank">Google Play Developer Console</a>
+    and sign in.
   </li>
   <li>
-    <p>To complete the rollout, Select <strong>Production</strong> in the menu on the left and do the following:</p>
+    Select <strong>Grow users</strong>
+    &gt;
+    <strong>Store presence</strong>
+    section in the menu on the left.
   </li>
   <li>
-    <p>Click the <strong>Edit release</strong> button at the top of the page.</p>
+    Select <strong>Store settings</strong>
+    in the menu on the left and do the following:
   </li>
-  <li>Click the <strong>Review release</strong> button at the bottom of the page.</li>
-  <li>Click the <strong>Start rollout to Production</strong> button at the bottom of the page.</li>
+  <ul>
+    <li>
+      Click the <strong>Edit</strong>
+      link next to the
+      <strong>App category</strong>
+      section:
+    </li>
+    <li>
+      Set the <strong>Category</strong>
+      to
+      <em>Books & Reference</em>
+      (or appropriate category)
+    </li>
+    <li>
+      Click the <strong>Save</strong>
+      button at the bottom of the popup and close the popup window.
+    </li>
+    <li>
+      Click on <strong>Manage tags</strong>
+      link and set appropriate tags (e.g. Religious text) and click the
+      <strong>Apply</strong>
+      button at the bottom of the page.
+    </li>
+    <li>
+      Click the <strong>Edit</strong>
+      link next to the
+      <strong>Store listing contact details</strong>
+      section:
+    </li>
+    <li>
+      Set <strong>Email address</strong>
+    </li>
+    <li>
+      Set <strong>Website</strong>
+      (optional)
+    </li>
+    <li>
+      Click the <strong>Save</strong>
+      button at the bottom of the popup and close the popup window.
+    </li>
+  </ul>
+
+  <li>
+    To complete the rollout, Select <strong>Test and release</strong>
+    &gt;
+    <strong>Production</strong>
+    in the menu on the left and do the following:
+  </li>
+  <ul>
+    <li>
+      Click on the <strong>Releases</strong>
+      tab in the middle of the page.
+    </li>
+    <li>
+      Click the <strong>Edit release</strong>
+      link.
+    </li>
+    <li>
+      Click the <strong>Next</strong>
+      button at the bottom of the page.
+    </li>
+    <li>
+      Click the <strong>Save</strong>
+      button at the bottom of the page.
+    </li>
+    <li>
+      Click the <strong>Go to Overview</strong>
+      button on the
+      <strong>Go to Publishing overview?</strong>
+      dialog.
+    </li>
+    <li>
+      Click the <strong>Send XX Changes for reivew</strong>
+      button.
+    </li>
+    <li>
+      Click the <strong>Send changes for review</strong>
+      button on the
+      <strong>Send XX changes for review?</strong>
+      dialog.
+    </li>
+  </ul>
 </ul>
-<h4>Notes</h4>
-<ol>
-  <li>If the app uses one of the Restricted Users setting in Security, then you will need to provide instructions on how a reviewer can gain access.</li>
-  <li>
-    If your app isn&#39;t specifically targeted to appeal to children (like a Children&#39;s Bible or Bible stories picture book), then you should specify that it is intended for 13+ otherwise there are additional regulations that have to
-    be met.
-  </li>
-</ol>
