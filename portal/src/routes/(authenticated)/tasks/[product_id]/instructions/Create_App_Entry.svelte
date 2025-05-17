2164bbf306f2e76747a939c19f5246af172120e2
Chris Hubbard <chris@thehubbards.org>
2025-05-17 21:03:57 +0200

    Update Google Play Forms to conform to current GP

--- portal/src/routes/(authenticated)/tasks/[product_id]/instructions/Create_App_Entry.svelte
+++ portal/src/routes/(authenticated)/tasks/[product_id]/instructions/Create_App_Entry.svelte
@@ -1,139 +1,378 @@
 <h3>Instructions</h3>
 <ul>
-  <li>Download the AAB file by clicking on the entry with type <em>aab</em> in the <em>Product Files</em> grid view.</li>
-  <li>Go to <a href="https://play.google.com/apps/publish/" target="_blank">Google Play Developer Console</a> and sign in.</li>
   <li>
-    Click <strong>Create app</strong> button and complete the following items:
-    <ul>
-      <li>Set <strong>App name</strong> to Project Name shown above.</li>
-      <li>Set <strong>Default language</strong> to <em>Store Listing Language</em> shown above.</li>
-      <li>Set <strong>App or game</strong> to App.</li>
-      <li>Set <strong>Free or paid</strong> to Free.</li>
-      <li>
-        Check <strong>Confirm app meets the Developer Program Policies</strong> checkbox.
-        <ul>
-          <li>Please review the <a href="https://play.google.com/about/developer-content-policy/" target="_blank">policies</a>.</li>
-        </ul>
-      </li>
-      <li>Check <strong>Accept US export laws</strong> checkbox.</li>
-      <li>Click the <strong>Create app</strong> button.</li>
-    </ul>
+    Download the AAB file by clicking on the entry with type <em>aab</em>
+    in the
+    <em>Product Files</em>
+     grid view at the bottom of this page.
   </li>
   <li>
-    Select <strong>App content</strong> under the <strong>Policy</strong> section in the menu on the left and do the following:
+    Go to <a href="https://play.google.com/apps/publish/" target="_blank">Google Play Developer Console</a>
+     and sign in.
+  </li>
+  <li>
+    Click <strong>Create app</strong>
+    link on the right side of the page and complete the following items:
     <ul>
       <li>
-        Under the <strong>Privacy policy</strong> section, click the <strong>Start</strong> link.
+        Set <strong>App name</strong>
+         to Project Name shown above.
+      </li>
+      <li>
+        Set <strong>Default language</strong>
+        to
+        <em>Store Listing Language</em>
+         shown above.
+      </li>
+      <li>
+        Set <strong>App or game</strong>
+         to App.
+      </li>
+      <li>
+        Set <strong>Free or paid</strong>
+         to Free.
+      </li>
+      <li>
+        Check <strong>Confirm app meets the Developer Program Policies</strong>
+        checkbox.
         <ul>
           <li>
-            Fill in the URL for your app&#39;s privacy policy.
+            Please review the <a href="https://play.google.com/about/developer-content-policy/" target="_blank">policies</a>
+            .
           </li>
-          <li>For an example, see the <a href="https://www.wycliffe.org/scripture-app-privacy" target="_blank">Wycliffe Scripture App Privacy Policy</a></li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
         </ul>
       </li>
       <li>
-        Under the <strong>Ads</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Click on <strong>No</strong>, my app does not contain ads radio button.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
+        Check <strong>Accept US export laws</strong>
+         checkbox.
       </li>
       <li>
-        Under the <strong>App access</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Click the appropriate<sup>1</sup> radio button based on features used in the app.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App</strong> content link at the top of the page to go back.</li>
-        </ul>
+        Click the <strong>Create app</strong>
+         button at the bottom of the page.
       </li>
-      <li>
-        Under the <strong>Content ratings</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Click the <strong>Start questionnaire</strong> button and complete the questionnaire.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>Next</strong> button at the bottom of the page.</li>
-          <li>Review the ratings applied and then click the <strong>Submit</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-      <li>
-        Under the <strong>Target audience and content</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Check the appropriate<sup>2</sup> <strong>Target age groups</strong> for the app (we suggest <strong>13-15</strong>, <strong>16-17</strong>, and <strong>18 and over</strong>).</li>
-          <li>Click the <strong>Next</strong> button at the bottom of the page.</li>
-          <li>For the <strong>Appeals to children</strong> option, click the <strong>No</strong> radio button.</li>
-          <li>Click the <strong>Next</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-      <li>
-        Under the <strong>News apps</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Click on <strong>No</strong>, my app does not contain ads radio button.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-      <li>
-        Under the <strong>COVID-19 contract tracing and status apps</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Click on the <strong>My app is not a publicly available COVID-19 contact tracing or status app</strong> checkbox.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-      <li>
-        Under the <strong>Data safety</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>Download the Data Safety CSV by clicking on the entry with type <em>data-safety-csv</em> in the <em>Product Files</em> grid view.</li>
-          <li>Click the <strong>Import from CSV</strong> button at the top of the page.</li>
-          <li>In the <strong>Import from CSV</strong> dialog, click on the <strong>Upload</strong> link and select the downloaded Data Safety CSV (or drag and drop the file into the file upload area).</li>
-          <li>Click the <strong>Submit</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-      <li>
-        Under the <strong>Advertising ID</strong> section, click the <strong>Start</strong> link.
-        <ul>
-          <li>For the <strong>Does your app use advertising ID?</strong> question, click the <strong>No</strong> radio button.</li>
-          <li>Click the <strong>Save</strong> button at the bottom of the page.</li>
-          <li>Click the <strong>App content</strong> link at the top of the page to go back.</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-  <li>Select <strong>Production</strong> under the <strong>Release</strong> section in the menu on the left.</li>
-  <li>
-    On the right, click the <strong>Countries / regions</strong> link and do the following:
-    <ul>
-      <li>Click the <strong>Add countries / regions</strong> link.</li>
-      <li>Select the countries where the app will be available. You can click the checkbox next to <strong>Country / region</strong> to select all countries.</li>
-      <li>Click the <strong>Add countries / regions</strong> button at the bottom of the page.</li>
-      <li>Click the <strong>Add</strong> button in <strong>the Add countries / regions?</strong> dialog.</li>
     </ul>
   </li>
   <li>
-    On the right, click <strong>Create new release</strong> button at the top of the page and complete the following items:
+    Select <strong>Monitor and Improve</strong>
+    &gt;
+    <strong>Policy and programs</strong>
+    &gt;
+    <strong>App content</strong>
+    section in the menu on the left and do the following:
     <ul>
-      <li>In the <strong>App bundles</strong> section, click the <strong>Choose signing key</strong> link.</li>
-      <li>In the <strong>Choose signing key</strong> dialog, click the <strong>Use Google-generate key</strong> button.</li>
-      <li>Wait for the <strong>App signing key generated</strong> message at the bottom of the window.</li>
-      <li>Click the <strong>Upload</strong> link and select the downloaded AAB (or drag and drop the file into the file upload area).</li>
+      <li>
+        Under the <strong>Privacy policy</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>Fill in the URL for your app's privacy policy.</li>
+          <li>
+            For an example, see the <a href="https://www.wycliffe.org/scripture-app-privacy">Wycliffe Scripture App Privacy Policy</a>
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+             link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Under the <strong>Ads</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click on <strong>No</strong>
+            , my app does not contain ads radio button.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Under the <strong>App access</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click the appropriate
+            <sup>1</sup>
+             radio button based on features used in the app.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App</strong>
+             content link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Under the <strong>Content ratings</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click the <strong>Start questionnaire</strong>
+             button and complete the questionnaire. There will be multiple pages.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>Next</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Review the ratings applied and then click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+             link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Target audience and content</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Check the appropriate <strong>Target age groups</strong>
+            for the app (we suggest
+            <strong>13-15</strong>
+            ,
+            <strong>16-17</strong>
+            , and
+            <strong>18 and over</strong>
+            ).
+          </li>
+          <li>
+            Click the <strong>Next</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+             link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Data safety</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Download the Data Safety CSV by clicking on the entry with type <em>data-safety-csv</em>
+            in the
+            <em>Product Files</em>
+             grid view at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>Import from CSV</strong>
+             button at the top of the page.
+          </li>
+          <li>
+            In the <strong>Import from CSV</strong>
+            dialog, click on the
+            <strong>Upload</strong>
+             link and select the downloaded Data Safety CSV (or drag and drop the file into the file upload area).
+          </li>
+          <li>
+            Click the <strong>Import</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            The Data Safey section changes frequently, so you may need to answer addition questions until the <strong>Next</strong>
+             button is enabled.
+          </li>
+          <li>
+            Review each section and click the <strong>Next</strong>
+             button at the bottom of the page until you have completed the Data Safety section.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Advertising ID</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            For the <strong>Does your app use advertising ID?</strong>
+            question, click the
+            <strong>No</strong>
+             radio button.
+          </li>
+          <li>
+            If your app uses the <strong>Deferred Deep Linking - using Branch</strong>
+            feature, click the
+            <strong>Yes</strong>
+             radio button instead.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Government apps</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click on <strong>No</strong>
+             radio button.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Financial features</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click on <strong>My app doesn't provide any financial features</strong>
+             checkbox.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        Under the <strong>Financial features</strong>
+        section, click the
+        <strong>Start declaration</strong>
+        button.
+        <ul>
+          <li>
+            Click on <strong>My app does not have any health features</strong>
+             checkbox.
+          </li>
+          <li>
+            Click the <strong>Save</strong>
+             button at the bottom of the page.
+          </li>
+          <li>
+            Click the <strong>App content</strong>
+            link at the top of the page to go back.
+          </li>
+        </ul>
+      </li>
     </ul>
   </li>
-  <li>Click the <strong>Save</strong> link at the bottom of the page.</li>
-  <li>Click the <strong>Review release</strong> button and ignore the Errors<sup>1</sup> and Warnings<sup>2</sup> for now.</li>
+
+  <li>
+    Select <strong>Test and release</strong>
+    &gt;
+    <strong>Production</strong>
+     section in the menu on the left.
+  </li>
+  <ul>
+    <li>
+      Click the <strong>Countries / regions</strong>
+      tab in the center of the screen and do the following:
+      <ul>
+        <li>
+          Click the <strong>Add countries / regions</strong>
+           link.
+        </li>
+        <li>
+          Select the countries where the app will be available. You can click the checkbox next to <strong>Country / region</strong>
+           to select all countries.
+        </li>
+        <li>
+          Click the <strong>Save</strong>
+           button at the bottom of the page.
+        </li>
+      </ul>
+    </li>
+    <li>
+      On the right, click <strong>Create new release</strong>
+      button at the top of the page and complete the following items:
+      <ul>
+        <li>
+          In the <strong>App bundles</strong>
+          section, click the
+          <strong>Upload</strong>
+           link and select the downloaded AAB (or drag and drop the file into the file upload area).
+        </li>
+        <li>
+          Click the <strong>Next</strong>
+           button at the bottom of the page.
+        </li>
+        <li>
+          There will be a list of Errors
+          <sup>2</sup>
+          and Warnings
+          <sup>3</sup>
+           that can be ignored for now.
+        </li>
+      </ul>
+    </li>
+  </ul>
 </ul>
 <h4>Notes</h4>
 <ol>
-  <li>
-    The errors are due to missing store listing information which will be uploaded later by Scriptoria.
-  </li>
-  <li>
-    The warning is related to the upload containing native code and not uploading debug symbols.
-  </li>
+  <li>If parts of your app require an access code or registration, then you need to provide instructions to Google Play so that they can test all of your app.</li>
+  <li>The errors are due to missing store listing information which will be uploaded later by Scriptoria.</li>
+  <li>The warning is related to the upload containing native code and not uploading debug symbols.</li>
 </ol>
