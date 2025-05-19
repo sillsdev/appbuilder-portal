dee04697a2a5e0a0f722a2353b41f78a5fe0969c
Aidan Jones <aejones4gm@gmail.com>
2025-05-19 11:08:42 -0400

    Use TypeError instead of Error

--- portal/static/custom-polyfills.js
+++ portal/static/custom-polyfills.js
@@ -5,7 +5,7 @@ if (!Array.prototype.toSorted) {
     enumerable: false,
     value: function (/** @type {((a: any, b: any) => number) | undefined} */ compareFn) {
       if (compareFn && typeof compareFn !== 'function') {
-        throw new Error(`Array.prototype.toSorted: ${compareFn} is not a function!`);
+        throw new TypeError(`Array.prototype.toSorted: ${compareFn} is not a function!`);
       }
       return this.slice(0).sort(compareFn);
     }
