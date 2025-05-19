349e3c2f4dc3a6ec69766aaa358f4eef58d682b7
Aidan Jones <aejones4gm@gmail.com>
2025-05-19 11:06:54 -0400

    Fix issues from review

--- portal/static/custom-polyfills.js
+++ portal/static/custom-polyfills.js
@@ -1,10 +1,13 @@
 if (!Array.prototype.toSorted) {
-  Array.prototype.toSorted = function (
-    /** @type {((a: any, b: any) => number) | undefined} */ compareFn
-  ) {
-    if (typeof compareFn !== 'function') {
-      throw new Error(`Array.prototype.toSorted: ${compareFn} is not a function!`);
+  Object.defineProperty(Array.prototype, 'toSorted', {
+    configurable: true,
+    writable: true,
+    enumerable: false,
+    value: function (/** @type {((a: any, b: any) => number) | undefined} */ compareFn) {
+      if (compareFn && typeof compareFn !== 'function') {
+        throw new Error(`Array.prototype.toSorted: ${compareFn} is not a function!`);
+      }
+      return this.slice(0).sort(compareFn);
     }
-    return this.slice(0).sort(compareFn);
-  };
+  });
 }
