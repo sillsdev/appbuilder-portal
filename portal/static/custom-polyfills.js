5d0189b631f5e13db4df8d9b381cd94217d699f4
Aidan Jones <aejones4gm@gmail.com>
2025-05-19 10:35:16 -0400

    Create a custom polyfill for Array.toSorted

---
+++ portal/static/custom-polyfills.js
@@ -0,0 +1,9 @@
+if (!Array.prototype.toSorted)
+  Array.prototype.toSorted = function (
+    /** @type {((a: any, b: any) => number) | undefined} */ compareFn
+  ) {
+    if (typeof compareFn !== 'function') {
+      throw new Error(`Array.prototype.toSorted: ${compareFn} is not a function!`);
+    }
+    return this.slice(0).sort(compareFn);
+  };
