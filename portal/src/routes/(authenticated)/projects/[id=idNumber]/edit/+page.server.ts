03259bbf54680b65194b42b800852fb0e54bf6b1
Aidan Jones <aejones4gm@gmail.com>
2025-05-20 16:04:56 -0400

    Lockdown /projects/id

--- portal/src/routes/(authenticated)/projects/[id=idNumber]/edit/+page.server.ts
+++ portal/src/routes/(authenticated)/projects/[id=idNumber]/edit/+page.server.ts
@@ -1,3 +1,4 @@
+import { verifyCanViewAndEdit } from '$lib/projects/server';
 import { idSchema } from '$lib/valibot';
 import { DatabaseWrites, prisma } from 'sil.appbuilder.portal.common';
 import { fail, superValidate } from 'sveltekit-superforms';
@@ -50,10 +51,12 @@ export const load = (async ({ params }) => {
 
 export const actions: Actions = {
   default: async function (event) {
+<<<<<<< HEAD
     // permissions checked in auth
+=======
+>>>>>>> cc56cfa97 (Lockdown /projects/id)
     const form = await superValidate(event.request, valibot(projectPropertyEditSchema));
     if (!form.valid) return fail(400, { form, ok: false });
-    if (isNaN(parseInt(event.params.id))) return fail(400, { form, ok: false });
     const success = await DatabaseWrites.projects.update(parseInt(event.params.id), {
       Name: form.data.name,
       GroupId: form.data.group,
