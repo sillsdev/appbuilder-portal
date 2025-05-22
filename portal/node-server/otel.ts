6a19ac9b6024bc9f4b7b34855fc9feb773318bcb
Aidan Jones <aejones4gm@gmail.com>
2025-05-22 10:44:31 -0400

    Move OTEL to common, add singleton initializer

--- portal/node-server/otel.ts
+++ portal/node-server/otel.ts
@@ -1,29 +1,3 @@
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+import { OTEL } from "sil.appbuilder.portal.common";
 
-const endpoint = `http://${process.env.NODE_ENV === 'development' ? 'localhost' : 'otel-collector'}:6317`;
-
-const sdk = new NodeSDK({
-  resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: 'scriptoria',
-    [ATTR_SERVICE_VERSION]: '1.0'
-  }),
-  traceExporter: new OTLPTraceExporter({ url: endpoint }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({ url: endpoint })
-  }),
-  instrumentations: [
-    getNodeAutoInstrumentations({
-      '@opentelemetry/instrumentation-fs': {
-        enabled: false
-      }
-    })
-  ]
-});
-
-sdk.start();
+OTEL.getInstance().start();
