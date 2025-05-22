e68e680ab567caf87e81c9fe747eabae7c7296f9
Aidan Jones <aejones4gm@gmail.com>
2025-05-22 13:32:38 -0400

    Trace server hooks

--- portal/src/hooks.server.ts
+++ portal/src/hooks.server.ts
@@ -1,7 +1,7 @@
 // hooks.server.ts
 import { localizeHref } from '$lib/paraglide/runtime';
 import { paraglideMiddleware } from '$lib/paraglide/server';
-import { trace, type Span } from '@opentelemetry/api';
+import { type Span } from '@opentelemetry/api';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { connected, OTEL, Queues } from 'sil.appbuilder.portal.common';
@@ -9,36 +9,50 @@ import {
   authRouteHandle,
   checkUserExistsHandle,
   localRouteHandle,
-  organizationInviteHandle
+  organizationInviteHandle,
+  tracer
 } from './auth';
 
-const tracer = trace.getTracer('server-hooks');
-
 // creating a handle to use the paraglide middleware
-const paraglideHandle: Handle = ({ event, resolve }) =>
-  paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
-    event.request = localizedRequest;
-    return resolve(event, {
-      transformPageChunk: ({ html }) => {
-        return html.replace('%lang%', locale);
-      }
+const paraglideHandle: Handle = ({ event, resolve }) => {
+  return tracer.startActiveSpan('paraglide', (span: Span) => {
+    const res = paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+      event.request = localizedRequest;
+      span.setAttribute('locale', locale);
+      return resolve(event, {
+        transformPageChunk: ({ html }) => {
+          return html.replace('%lang%', locale);
+        }
+      });
     });
+    span.end();
+    return res;
   });
+};
 
 const heartbeat: Handle = ({ event, resolve }) => {
-  // this check is important to prevent infinite redirects...
-  if (
-    !(
-      event.route.id === '/(unauthenticated)/error' ||
-      event.route.id === '/(unauthenticated)/(auth)/login'
-    )
-  ) {
-    if (!(connected() && Queues.connected())) {
-      // HTTP 503 *should* be the correct semantics here?
-      return redirect(302, localizeHref(`/error?code=503`));
+  return tracer.startActiveSpan('heartbeat', (span: Span) => {
+    // this check is important to prevent infinite redirects...
+    if (
+      !(
+        event.route.id === '/(unauthenticated)/error' ||
+        event.route.id === '/(unauthenticated)/(auth)/login'
+      )
+    ) {
+      const p = connected();
+      const r = Queues.connected();
+      if (!(p && r)) {
+        if (!p) span.setAttribute('pgsql-connected', p);
+        if (!r) span.setAttribute('redis-connected', r);
+        span.end();
+        // HTTP 503 *should* be the correct semantics here?
+        return redirect(302, localizeHref(`/error?code=503`));
+      }
     }
-  }
-  return resolve(event);
+    const res = resolve(event);
+    span.end();
+    return res;
+  });
 };
 
 const sequenced: Handle = sequence(
