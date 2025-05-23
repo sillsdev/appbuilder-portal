0f3a9ec31be4925032b245b32b0413f716059bf8
Aidan Jones <aejones4gm@gmail.com>
2025-05-23 13:43:32 -0400

    Add aria-label to SearchBar

--- portal/src/lib/components/SearchBar.svelte
+++ portal/src/lib/components/SearchBar.svelte
@@ -10,12 +10,15 @@
   let { className = '', value = $bindable() }: Props = $props();
 </script>
 
-<div
-  class={className}
-  data-html="true"
->
+<div class={className} data-html="true">
   <label class="input input-bordered flex items-center gap-2 w-full">
-    <input type="search" placeholder={m.common_search()} class="grow" bind:value />
+    <input
+      type="search"
+      placeholder={m.common_search()}
+      aria-label={m.common_search()}
+      class="grow"
+      bind:value
+    />
     <IconContainer icon="mdi:search" class="ml-auto cursor-pointer" width={24} />
   </label>
 </div>
