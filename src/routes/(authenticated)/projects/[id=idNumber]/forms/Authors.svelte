bd62b55593553c0ca8e14f4300020877566554c9
Aidan Jones <aejones4gm@gmail.com>
2025-05-28 14:44:26 -0400

    Disable author addition/removal

--- src/routes/(authenticated)/projects/[id=idNumber]/forms/Authors.svelte
+++ src/routes/(authenticated)/projects/[id=idNumber]/forms/Authors.svelte
@@ -3,9 +3,11 @@
   import { type Infer, type SuperValidated, superForm } from 'sveltekit-superforms';
   import type { AuthorSchema } from './valibot';
   import { enhance as svk_enhance } from '$app/forms';
+  import BlockIfJobsUnavailable from '$lib/components/BlockIfJobsUnavailable.svelte';
   import IconContainer from '$lib/components/IconContainer.svelte';
   import { m } from '$lib/paraglide/messages';
   import { getLocale } from '$lib/paraglide/runtime';
+  import { toast } from '$lib/utils';
   import { byName } from '$lib/utils/sorting';
 
   interface Props {
@@ -35,7 +37,13 @@
   let { projectAuthors, availableAuthors, formData, createEndpoint, deleteEndpoint }: Props =
     $props();
 
-  const { form, enhance } = superForm(formData);
+  const { form, enhance } = superForm(formData, {
+    onError: ({ result }) => {
+      if (result.status === 503) {
+        toast('error', m.system_unavailable());
+      }
+    }
+  });
 </script>
 
 <div class="card card-bordered border-slate-400 overflow-hidden rounded-md max-w-full">
@@ -48,12 +56,29 @@
       {#each projectAuthors.toSorted((a, b) => byName(a.Users, b.Users, locale)) as author}
         <div class="flex flex-row w-full place-content-between p-2">
           <span>{author.Users.Name}</span>
-          <form action="?/{deleteEndpoint}" method="post" use:svk_enhance>
-            <input type="hidden" name="id" value={author.Id} />
-            <button type="submit" class="cursor-pointer">
+          <BlockIfJobsUnavailable>
+            {#snippet altContent()}
               <IconContainer icon="mdi:close" width="24" />
-            </button>
-          </form>
+            {/snippet}
+            <form
+              action="?/{deleteEndpoint}"
+              method="post"
+              use:svk_enhance={() =>
+                ({ update, result }) => {
+                  if (result.type === 'error') {
+                    if (result.status === 503) {
+                      toast('error', m.system_unavailable());
+                    }
+                  }
+                  update({ reset: false });
+                }}
+            >
+              <input type="hidden" name="id" value={author.Id} />
+              <button type="submit" class="cursor-pointer">
+                <IconContainer icon="mdi:close" width="24" />
+              </button>
+            </form>
+          </BlockIfJobsUnavailable>
         </div>
       {/each}
     {:else}
@@ -81,9 +106,14 @@
             </option>
           {/if}
         </select>
-        <button type="submit" class="btn btn-primary">
-          {m.authors_submit()}
-        </button>
+        <BlockIfJobsUnavailable className="btn btn-primary">
+          {#snippet altContent()}
+            {m.authors_submit()}
+          {/snippet}
+          <button type="submit" class="btn btn-primary">
+            {m.authors_submit()}
+          </button>
+        </BlockIfJobsUnavailable>
       </div>
     </form>
   </div>
