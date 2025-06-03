fa1c1f33f9bff79e3f4c191e7df5134e63ba4a9f
Aidan Jones <aejones4gm@gmail.com>
2025-06-03 12:46:37 -0400

    Add downloads page (#1132)
    * Add APK download page
    
    * Create file downloads page
    
    * Order manifests correctly
    
    * apply background color correctly
    
    * simple text contrast
    
    * Break out downloads page from inherited layouts
    
    ---------
    
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

---
+++ portal/src/routes/(unauthenticated)/downloads/products/[id]/files/published/[type]/+page.server.ts
@@ -0,0 +1,9 @@
+import { error } from '@sveltejs/kit';
+import { _getHeaders } from '../../../../../../api/products/[product_id]/files/published/[type]/+server';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+  const file = await _getHeaders(params.id, params.type);
+  if (!file) return error(404);
+  return file;
+};
