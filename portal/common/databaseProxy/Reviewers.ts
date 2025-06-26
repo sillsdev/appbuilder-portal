0c19a4f458e991f9b4a1b08e946f6b4b1dbe1ecd
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2025-06-26 13:45:16 -0400

    Implement reviewers in DatabaseWrites

---
+++ portal/common/databaseProxy/Reviewers.ts
@@ -0,0 +1,35 @@
+import { Prisma } from '@prisma/client';
+import { BullMQ, Queues } from '../index.js';
+import prisma from '../prisma.js';
+
+async function deleteReviewer(id: number) {
+  // Get the reviewer's project
+  const reviewer = await prisma.reviewers.findUnique({
+    where: { Id: id }
+  });
+  if (!reviewer) {
+    return false;
+  }
+  const ret = await prisma.reviewers.delete({
+    where: { Id: id }
+  });
+
+  Queues.SvelteProjectSSE.add(`Update Project #${reviewer?.ProjectId} (reviewer removed)`, {
+    type: BullMQ.JobType.SvelteProjectSSE_UpdateProject,
+    projectIds: [reviewer.ProjectId]
+  });
+  return ret;
+}
+
+export { deleteReviewer as delete };
+
+export async function create(reviewerData: Prisma.ReviewersUncheckedCreateInput) {
+  const ret = await prisma.reviewers.create({
+    data: reviewerData
+  });
+  Queues.SvelteProjectSSE.add(`Update Project #${reviewerData.ProjectId} (reviewer added)`, {
+    type: BullMQ.JobType.SvelteProjectSSE_UpdateProject,
+    projectIds: [reviewerData.ProjectId]
+  });
+  return ret;
+}
