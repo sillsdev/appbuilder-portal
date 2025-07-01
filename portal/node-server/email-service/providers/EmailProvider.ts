01cd3a45062d05a942cf69d8d31f7bbb71168b48
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2025-07-01 11:57:06 -0400

    Adjust env vars for sparkpost and ses

--- portal/node-server/email-service/providers/EmailProvider.ts
+++ portal/node-server/email-service/providers/EmailProvider.ts
@@ -1,7 +1,7 @@
 export abstract class EmailProvider {
   static readonly EMAIL_NAME =
     'Scriptoria' + (process.env.NODE_ENV === 'development' ? ' (dev)' : '');
-  static readonly EMAIL_FROM = process.env.VITE_SPARKPOST_EMAIL;
+  static readonly EMAIL_FROM = process.env.VITE_EMAIL_FROM;
 
   abstract sendEmail(
     to: { email: string; name: string }[],
