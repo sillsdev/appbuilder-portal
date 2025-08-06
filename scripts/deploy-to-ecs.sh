e0ced76e6a4bfb235d770b35abd674a13d7c8759
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2025-08-06 10:06:39 -0400

    Remove old files

--- scripts/deploy-to-ecs.sh
+++
@@ -1,9 +0,0 @@
-set -a
-
-source ./scripts/build-setup.sh
-
-echo "Attempting to deploy $CURRENT_VERSION to Cluster: '$ECS_CLUSTER'..."
-
-if [ -n "$ECS_CLUSTER" ]; then
-  ecs-deploy -c $ECS_CLUSTER -n portal -i ignore -to $CURRENT_VERSION --max-definitions 20 --timeout 600
-fi
