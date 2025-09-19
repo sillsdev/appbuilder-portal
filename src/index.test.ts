97338c24b4de6cebea1f5c406893a1a74576f9ac
Aidan Jones <aidan_jones@sil.org>
2025-09-19 11:38:36 -0500

    Use sendEmail directly in unit test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { RoleId } from '$lib/prisma';
-import { BullMQ, getQueues } from '$lib/server/bullmq';
-import { DatabaseReads, DatabaseWrites } from '$lib/server/database';
+import { sendEmail } from '$lib/server/email-service/EmailClient';
 
 describe('sum test', () => {
   it('adds 1 + 2 to equal 3', () => {
@@ -11,22 +9,12 @@ describe('sum test', () => {
 
 describe('email test', () => {
   it('send test email', async () => {
-    const email = 'success@simulator.amazonses.com';
-    const inviteToken = await DatabaseWrites.organizationMemberships.createOrganizationInvite(
-      email,
-      1, // SIL International
-      (await DatabaseReads.users.findFirstOrThrow({ where: { Email: 'ci@scriptoria.io' } })).Id, // Scriptoria CI
-      [RoleId.Author],
-      [1] // LSDEV
-    );
-    const job = await getQueues().Emails.add('Send test email', {
-      type: BullMQ.JobType.Email_InviteUser,
-      email,
-      inviteToken,
-      inviteLink: 'test'
-    });
-    await new Promise((resolve) => setTimeout(resolve, 10_000));
-    const res = await getQueues().Emails.getJob(job.id!);
-    expect(res?.returnvalue?.email).toBeTruthy();
-  }, 11_000);
+    expect(
+      await sendEmail(
+        [{ email: 'success@simulator.amazonses.com', name: 'SES Simulator' }],
+        'Test Email',
+        'This is a test email.'
+      )
+    ).toBeTruthy();
+  });
 });
