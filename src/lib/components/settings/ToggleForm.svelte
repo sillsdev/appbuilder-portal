81a44430ef5726563cbd3144ec7e08f2cc766ede
Judah Sotomayor <judah.sotomayor@pm.me>
2025-09-26 12:08:59 -0400

    feat(projects): Add ToggleForm component.
    Paper over the repetitive `form`s in settings using a new ToggleForm component.

---
+++ src/lib/components/settings/ToggleForm.svelte
@@ -0,0 +1,65 @@
+<!--
+    @component
+    An abstraction over Toggle which makes it easier to submit simple POSTs using a toggle.
+-->
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import Toggle from '$lib/components/settings/Toggle.svelte';
+  import { m } from '$lib/paraglide/messages';
+  import { toast } from '$lib/utils';
+  import type { ValueKey } from '$lib/locales.svelte';
+  type Method = 'POST' | 'GET';
+
+  interface Props {
+    name: string;
+    method: Method;
+    action: string;
+    formVar: boolean;
+    onmsg: string;
+    offmsg: string;
+    onIcon?: string;
+    offIcon?: string;
+    title: ValueKey;
+    message: ValueKey;
+  }
+
+  let { name, method, action, formVar, onmsg, offmsg, title, message, onIcon, offIcon }: Props =
+    $props();
+
+  let form: HTMLFormElement;
+</script>
+
+<form
+  bind:this={form}
+  {method}
+  {action}
+  use:enhance={() =>
+    ({ update, result }) => {
+      if (result.type === 'success') {
+        const res = result.data as ActionData;
+        if (res?.ok) {
+          if (formVar) {
+            toast('success', onmsg);
+          } else {
+            toast('success', offmsg);
+          }
+        } else {
+          toast('error', m.errors_generic({ errorMessage: '' }));
+          formVar = !formVar;
+        }
+      }
+      update({ reset: false });
+    }}
+>
+  <Toggle
+    {title}
+    {message}
+    {name}
+    bind:checked={formVar}
+    onchange={() => {
+      form.requestSubmit();
+    }}
+    {onIcon}
+    {offIcon}
+  />
+</form>
--- src/routes/(authenticated)/projects/[id=idNumber]/forms/Settings.svelte
+++ src/routes/(authenticated)/projects/[id=idNumber]/forms/Settings.svelte
@@ -2,8 +2,8 @@
   import type { Prisma } from '@prisma/client';
   import type { ActionData } from '../$types';
   import { enhance } from '$app/forms';
-  import InputWithMessage from '$lib/components/settings/InputWithMessage.svelte';
   import Toggle from '$lib/components/settings/Toggle.svelte';
+  import ToggleForm from '$lib/components/settings/ToggleForm.svelte';
   import { m } from '$lib/paraglide/messages';
   import { toast } from '$lib/utils';
 
@@ -29,70 +29,27 @@
 
 <h2 class="pl-0 pt-0">{m.project_settings_title()}</h2>
 <div class="space-y-2">
-  <form
-    bind:this={publicForm}
+  <ToggleForm
+    name="isPublic"
     method="POST"
     action="?/{publicEndpoint}"
-    use:enhance={() =>
-      ({ update, result }) => {
-        if (result.type === 'success') {
-          const res = result.data as ActionData;
-          if (res?.ok) {
-            if (isPublic) {
-              toast('success', m.project_acts_isPublic_on());
-            } else {
-              toast('success', m.project_acts_isPublic_off());
-            }
-          } else {
-            toast('error', m.errors_generic({ errorMessage: '' }));
-            isPublic = !isPublic;
-          }
-        }
-        update({ reset: false });
-      }}
-  >
-    <Toggle
-      title={{ key: 'project_visibility_title' }}
-      message={{ key: 'project_visibility_description' }}
-      name="isPublic"
-      bind:checked={isPublic}
-      onchange={() => {
-        publicForm.requestSubmit();
-      }}
-      onIcon="mdi:lock-open-variant"
-      offIcon="mdi:lock"
-    />
-  </form>
-  <form
+    formVar={isPublic}
+    onmsg={m.project_acts_isPublic_on()}
+    offmsg={m.project_acts_isPublic_off()}
+    title={{ key: 'project_visibility_title' }}
+    message={{ key: 'project_visibility_description' }}
+    onIcon="mdi:lock-open-variant"
+    offIcon="mdi:lock"
+  />
+
+  <ToggleForm
+    name="allowDownloads"
     method="POST"
     action="?/{downloadEndpoint}"
-    bind:this={downloadForm}
-    use:enhance={() =>
-      ({ update, result }) => {
-        if (result.type === 'success') {
-          const res = result.data as ActionData;
-          if (res?.ok) {
-            if (allowDownloads) {
-              toast('success', m.project_acts_downloads_on());
-            } else {
-              toast('success', m.project_acts_downloads_off());
-            }
-          } else {
-            toast('error', m.errors_generic({ errorMessage: '' }));
-            allowDownloads = !allowDownloads;
-          }
-        }
-        update({ reset: false });
-      }}
-  >
-    <Toggle
-      title={{ key: 'project_orgDownloads_title' }}
-      name="canDownload"
-      message={{ key: 'project_orgDownloads_description' }}
-      bind:checked={allowDownloads}
-      onchange={() => {
-        downloadForm.requestSubmit();
-      }}
-    />
-  </form>
+    title={{ key: 'project_orgDownloads_title' }}
+    message={{ key: 'project_orgDownloads_description' }}
+    onmsg={m.project_acts_downloads_on()}
+    offmsg={m.project_acts_downloads_off()}
+    formVar={allowDownloads}
+  />
 </div>
