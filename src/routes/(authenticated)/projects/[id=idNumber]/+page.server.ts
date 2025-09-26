d931921c8101e67f1ea6ca74b1a6561259d9de4b
Judah Sotomayor <judah.sotomayor@pm.me>
2025-09-26 13:15:01 -0400

    feat(server): Add routes for auto toggles.
    Add the routes for AutoPublishOnRebuild and RebuildOnSoftwareUpdate.

--- src/routes/(authenticated)/projects/[id=idNumber]/+page.server.ts
+++ src/routes/(authenticated)/projects/[id=idNumber]/+page.server.ts
@@ -295,6 +295,7 @@ export const actions = {
     });
     return { form, ok: true };
   },
+
   async toggleDownload(event) {
     event.locals.security.requireProjectWriteAccess(
       await DatabaseReads.projects.findUnique({
@@ -316,6 +317,43 @@ export const actions = {
     });
     return { form, ok: true };
   },
+
+
+  async toggleAutoPublishOnRebuild(event) {
+    // permissions checked in auth
+    const form = await superValidate(
+      event.request,
+      valibot(
+        v.object({
+          autoPublishOnRebuild: v.boolean()
+        })
+      )
+    );
+    if (!form.valid) return fail(400, { form, ok: false });
+    await DatabaseWrites.projects.update(parseInt(event.params.id), {
+      AutoPublishOnRebuild: form.data.autoPublishOnRebuild
+    });
+    return { form, ok: true };
+  },
+
+  async toggleRebuildOnSoftwareUpdate(event) {
+    // permissions checked in auth
+    const form = await superValidate(
+      event.request,
+      valibot(
+        v.object({
+          rebuildOnSoftwareUpdate: v.boolean()
+        })
+      )
+    );
+    if (!form.valid) return fail(400, { form, ok: false });
+    await DatabaseWrites.projects.update(parseInt(event.params.id), {
+      RebuildOnSoftwareUpdate: form.data.rebuildOnSoftwareUpdate
+    });
+    return { form, ok: true };
+  },
+
+
   async editOwnerGroup(event) {
     event.locals.security.requireAuthenticated(); // check this first, so unauthenticated can't dos db
     const form = await superValidate(event.request, valibot(updateOwnerGroupSchema));
