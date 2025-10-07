33ae0d36ee46cff613412d4e3f830d321bfd5270
Aidan Jones <aidan_jones@sil.org>
2025-10-07 12:04:35 -0500

    Block happy path if unchecked checkboxes in instructions

--- src/routes/(authenticated)/tasks/[product_id]/instructions/Evaluate_Error.svelte
+++ src/routes/(authenticated)/tasks/[product_id]/instructions/Evaluate_Error.svelte
@@ -1,1 +1,6 @@
-<!-- TODO -->
+<span class="flex flex-row">
+  <input type="checkbox" class="checkbox checkbox-accent" />
+  &nbsp; I confirm that I have taken the necessary steps to manually resolve the error in the&nbsp;
+  <a href="https://play.google.com/apps/publish/" target="_blank">Google Play Developer Console</a>
+  .
+</span>
