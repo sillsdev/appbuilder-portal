7a711a9a583848ec48d89d6b3c00127e13424f88
Aidan Jones <aidan_jones@sil.org>
2025-10-08 14:33:43 -0500

    Fix code TTL

--- src/routes/(unauthenticated)/api/auth/token/+server.ts
+++ src/routes/(unauthenticated)/api/auth/token/+server.ts
@@ -14,8 +14,8 @@ export async function GET({ locals, url }) {
   const code = randomUUID();
 
   // we may want to use IORedis key prefixes in the future (https://github.com/redis/ioredis?tab=readme-ov-file#transparent-key-prefixing)
-  await getAuthConnection().set(`auth0:code:${code}`, challenge, 'EX', 3_000); // 5 minute (3,000 s) TTL
-  await getAuthConnection().set(`auth0:user:${code}`, locals.security.userId, 'EX', 3_001); // 5 minute (3,000 s) TTL
+  await getAuthConnection().set(`auth0:code:${code}`, challenge, 'EX', 300); // 5 minute (300 s) TTL
+  await getAuthConnection().set(`auth0:user:${code}`, locals.security.userId, 'EX', 301); // 5 minute (300 s) TTL
 
   redirect(302, `${application}://auth/token?code=${code}`);
 }
