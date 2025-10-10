97f5f2f6a1efecb48fc64cfc4c46088d6753118e
Aidan Jones <aidan_jones@sil.org>
2025-10-10 16:34:55 -0500

    Fix line breaks in instruction

--- src/routes/(authenticated)/tasks/[product_id]/instructions/Evaluate_Error.svelte
+++ src/routes/(authenticated)/tasks/[product_id]/instructions/Evaluate_Error.svelte
@@ -40,11 +40,13 @@
 <p>
   <span class="flex flex-row">
     <input type="checkbox" class="checkbox checkbox-accent" />
-    &nbsp; I confirm that I have taken the necessary steps to manually upload the apk/aab and resolved
-    the error in the&nbsp;
-    <a href="https://play.google.com/apps/publish/" target="_blank">
-      Google Play Developer Console
-    </a>
-    .
+    <span>
+      &nbsp; I confirm that I have taken the necessary steps to manually upload the apk/aab and
+      resolved the error in the
+      <a href="https://play.google.com/apps/publish/" target="_blank">
+        Google Play Developer Console
+      </a>
+      .
+    </span>
   </span>
 </p>
