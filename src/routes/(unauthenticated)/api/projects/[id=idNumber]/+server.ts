28084ca655d236c1466251c415753b6d4d4909b8
Aidan Jones <aejones4gm@gmail.com>
2025-10-11 18:17:25 -0500

    Use json utility function

--- src/routes/(unauthenticated)/api/projects/[id=idNumber]/+server.ts
+++ src/routes/(unauthenticated)/api/projects/[id=idNumber]/+server.ts
@@ -1,5 +1,7 @@
+import { json } from '@sveltejs/kit';
+
 export async function GET({ params, locals }) {
   locals.security.requireAuthenticated();
   // ISSUE #1246 Handle can-rebuild attribute here
-  return new Response(JSON.stringify({ id: params.id }));
+  return json({ id: params.id });
 }
