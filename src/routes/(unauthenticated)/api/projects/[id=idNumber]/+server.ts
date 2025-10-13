35e7da970d810b1a6a0d0b7cefb32b64313b3dfa
Chris Hubbard <chris@thehubbards.org>
2025-10-13 15:27:51 -0400

    Stub out 'can-rebuild' for SAB testing

--- src/routes/(unauthenticated)/api/projects/[id=idNumber]/+server.ts
+++ src/routes/(unauthenticated)/api/projects/[id=idNumber]/+server.ts
@@ -3,5 +3,5 @@ import { json } from '@sveltejs/kit';
 export async function GET({ params, locals }) {
   locals.security.requireApiToken();
   // ISSUE #1246 Handle can-rebuild attribute here
-  return json({ id: params.id });
+  return json({ id: params.id, 'can-rebuild': true });
 }
