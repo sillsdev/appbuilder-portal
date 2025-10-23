4c53d98966b088622540fd800ff52c364060e4dc
Judah Sotomayor <judah.sotomayor@pm.me>
2025-10-23 15:11:16 -0400

    feat: Dim toggles when they are disabled.
    The toggle component is now greatly simplified.
    I replaced the custom toggle with daisyui's toggle-and-icon support.
    
    I also enabled an opacity reduction when the toggle is not enabled.

--- src/lib/components/settings/Toggle.svelte
+++ src/lib/components/settings/Toggle.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { HTMLInputAttributes } from 'svelte/elements';
   import IconContainer from '../IconContainer.svelte';
+  import Icon from '@iconify/svelte';
   import InputWithMessage from './InputWithMessage.svelte';
   import type { ValueKey } from '$lib/locales.svelte';
 
@@ -29,32 +30,20 @@
   }: Props = $props();
 </script>
 
-<InputWithMessage {title} {message} className="{className} {canEdit ? '' : 'cursor-not-allowed'}">
-  <label class="group">
-    <div class="relative">
-      <input
-        {name}
-        type="checkbox"
-        class="sr-only group"
-        bind:checked
-        {...inputAttr}
-        disabled={!canEdit}
-      />
-      <div
-        class="block h-8 rounded-full bg-gray-3 dark:bg-dark-2 w-14 toggle group-has-checked:border-accent"
-      ></div>
-      <div
-        class="absolute flex items-center justify-center w-6 h-6 transition bg-current rounded-full dark:bg-dark-5 left-1 top-1 group-has-checked:translate-x-full group-has-checked:bg-accent text-gray-400"
-      >
-        <span class="hidden group-has-checked:block">
-          <!-- Switch on -> public -->
-          <IconContainer icon={onIcon} class="-translate-y-px text-white" width="15" />
-        </span>
-        <span class="text-body-color dark:text-light group-has-checked:hidden">
-          <!-- Switch off -> private -->
-          <IconContainer icon={offIcon} class="-translate-y-px text-black" width="15" />
-        </span>
-      </div>
-    </div>
+<InputWithMessage {title} {message} className="{className} {canEdit ? '' : 'cursor-not-allowed'} ">
+  <label
+    class="toggle {checked ? 'border-accent' : ''} text-base-content
+           {canEdit ? '' : 'cursor-not-allowed opacity-50 pointer-events-none'}"
+  >
+    <input
+      {name}
+      type="checkbox"
+      disabled={!canEdit}
+      bind:checked
+      {...inputAttr}
+      class="checked:bg-accent checked:border-accent rounded-full"
+    />
+    <Icon icon={onIcon} width={20} height={20} />
+    <Icon icon={offIcon} width={20} height={20} color="white" />
   </label>
 </InputWithMessage>
