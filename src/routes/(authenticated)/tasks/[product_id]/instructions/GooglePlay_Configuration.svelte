733012ead62457c12dee0ed0d04dae32f906be17
Chris Hubbard <chris@thehubbards.org>
2025-11-03 08:32:18 -0500

    NOT COMPLETE: Add instructions for downloading play-listing.zip

--- src/routes/(authenticated)/tasks/[product_id]/instructions/GooglePlay_Configuration.svelte
+++ src/routes/(authenticated)/tasks/[product_id]/instructions/GooglePlay_Configuration.svelte
@@ -31,6 +31,26 @@
         to the name of a keystore not used to publish APKs.
       </li>
       <li>
+        For existing apps, if you have previously manually uploaded Play Listing information to
+        Google Play
+        <br />
+        and the Play Listing information is not in the Google Play Store Listing configuration page,
+        <br />
+        set
+        <strong>BUILD_DOWNLOAD_PLAY_LISTING</strong>
+        to the value 1.
+        <br />
+        During the build, the Play Listing information will be downloaded from Google Play
+        <br />
+        and will be available to import into the Google Play Store Listing configuration page.
+        <br />
+        On the Scriptoria page, you will need to uncheck the
+        <strong>Verify Google Play Store listing when sending app data</strong>
+        .
+        <br />
+        The downloaded Play Listing will be used during the publishing process.
+      </li>
+      <li>
         See the <a href="https://app.scriptoria.io/docs/Publishing+Properties.pdf" target="_blank">
           Publishing Properties Documentation
         </a>
@@ -48,6 +68,7 @@
         <strong>Enter App Project URL</strong>
         field.
       </li>
+      <li>For existing apps, if you have previously</li>
       <li>
         Click <strong>Login...</strong>
         to connect to Scriptoria.
