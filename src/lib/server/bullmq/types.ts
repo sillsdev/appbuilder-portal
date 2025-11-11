ca8f74e149fc460f96e5af252423f5cd189a6f53
Aidan Jones <aidan_jones@sil.org>
2025-11-11 14:46:30 -0600

    Move BullMQ types so types can be used in frontend
    Co-Authored-By: Aidan Jones <aidan_jones@sil.org>

--- src/lib/server/bullmq/types.ts
+++ src/lib/server/bullmq/types.ts
@@ -2,6 +2,8 @@
 import type { RepeatOptions } from 'bullmq';
 import type { RoleId } from '../../prisma';
 import type { BuildResponse, Channels, ReleaseResponse } from '../build-engine-api/types';
+import type { JobType } from '$lib/bullmq';
+export { JobSchedulerId, JobType, QueueName } from '$lib/bullmq';
 
 /** Retry a job for 72 hours every 10 minutes. Useful for build engine tasks */
 export const Retry0f600 = {
@@ -16,59 +18,6 @@ export const Retry0f600 = {
 export const RepeatEveryMinute: RepeatOptions = {
   pattern: '*/1 * * * *' // every minute
 } as const;
-
-export enum QueueName {
-  Builds = 'Builds',
-  System_Recurring = 'System (Recurring)',
-  System_Startup = 'System (Startup)',
-  Products = 'Products',
-  Projects = 'Projects',
-  Publishing = 'Publishing',
-  Polling = 'Polling',
-  UserTasks = 'User Tasks',
-  Emails = 'Emails',
-  SvelteSSE = 'Svelte SSE'
-}
-
-export enum JobType {
-  // Build Jobs
-  Build_Product = 'Build Product',
-  Build_PostProcess = 'Postprocess Build',
-  // Polling Jobs
-  Poll_Build = 'Check Product Build',
-  Poll_Project = 'Check Project Creation',
-  Poll_Publish = 'Check Product Publish',
-  // Product Jobs
-  Product_Create = 'Create Product - BuildEngine',
-  Product_Delete = 'Delete Product - BuildEngine',
-  Product_GetVersionCode = 'Get VersionCode for Uploaded Product',
-  Product_CreateLocal = 'Create Local Product',
-  // Project Jobs
-  Project_Create = 'Create Project',
-  Project_ImportProducts = 'Import Products for Project',
-  // Publishing Jobs
-  Publish_Product = 'Publish Product',
-  Publish_PostProcess = 'Postprocess Publish',
-  // System Jobs
-  System_CheckEngineStatuses = 'Check BuildEngine Statuses',
-  System_RefreshLangTags = 'Refresh langtags.json',
-  System_Migrate = 'Migrate Features from S1 to S2',
-  // UserTasks Job
-  UserTasks_Modify = 'Modify UserTasks',
-  // Email Jobs
-  Email_InviteUser = 'Invite User',
-  Email_SendNotificationToUser = 'Send Notification to User',
-  Email_SendNotificationToReviewers = 'Send Notification to Product Reviewers',
-  Email_SendNotificationToOrgAdminsAndOwner = 'Send Notification to Org Admins and Owners',
-  Email_SendBatchUserTaskNotifications = 'Send Batch User Task Notifications',
-  Email_NotifySuperAdminsOfNewOrganizationRequest = 'Notify Super Admins of New Organization Request',
-  Email_NotifySuperAdminsOfOfflineSystems = 'Notify Super Admins of Offline Systems',
-  Email_NotifySuperAdminsLowPriority = 'Notify Super Admins (Low Priority)',
-  Email_ProjectImportReport = 'Project Import Report',
-  // Svelte Project SSE
-  SvelteSSE_UpdateProject = 'Update Project',
-  SvelteSSE_UpdateUserTasks = 'Update UserTasks'
-}
 
 interface BaseJob {
   type: JobType;
@@ -359,10 +308,3 @@ export type JobTypeMap = {
   [JobType.SvelteSSE_UpdateUserTasks]: SvelteProjectSSE.UpdateUserTasks;
   // Add more mappings here as needed
 };
-
-export enum JobSchedulerId {
-  SystemStatusEmail = 'SystemStatusEmail',
-  RefreshLangTags = 'RefreshLangTags',
-  CheckSystemStatuses = 'CheckSystemStatuses',
-  PruneUsers = 'PruneUsers'
-}
