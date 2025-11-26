9d45141c79d0d1e15860287e3ce190d937b33b33
Peter Schumacher <pcschumacher@liberty.edu>
2025-11-26 15:47:54 -0600

    format fixes

--- src/routes/(authenticated)/software-update/[[orgId=idNumber]]/+page.svelte
+++ src/routes/(authenticated)/software-update/[[orgId=idNumber]]/+page.svelte
@@ -26,24 +26,28 @@
 </script>
 
 <div class="w-full">
-<h1>{m.admin_nav_software_update()}</h1>
-<div class="m-4">
-<p class="pl-4">{m.admin_nav_software_update_description()}</p>
-<br/>
-<p class="pl-4"><b>This will affect the following organizations: {data.organizations}</b></p>
-<br/>
-<form class="pl-4" method="post" action="?/start" use:enhance>
-  <LabeledFormInput key="admin_nav_software_update_comment">
-    <input
-      type="text"
-      name="comment"
-      class="input input-bordered w-full validator"
-      bind:value={$form.comment}
-      required
-    />
-    <span class="validator-hint">{m.admin_software_update_comment_required()}</span>
-  </LabeledFormInput>
-  <input type="submit" class="btn btn-primary" value={m.admin_software_update_rebuild_start()} />
-</form>
-</div>
+  <h1>{m.admin_nav_software_update()}</h1>
+  <div class="m-4">
+    <p class="pl-4">{m.admin_nav_software_update_description()}</p>
+    <br />
+    <p class="pl-4"><b>This will affect the following organizations: {data.organizations}</b></p>
+    <br />
+    <form class="pl-4" method="post" action="?/start" use:enhance>
+      <LabeledFormInput key="admin_nav_software_update_comment">
+        <input
+          type="text"
+          name="comment"
+          class="input input-bordered w-full validator"
+          bind:value={$form.comment}
+          required
+        />
+        <span class="validator-hint">{m.admin_software_update_comment_required()}</span>
+      </LabeledFormInput>
+      <input
+        type="submit"
+        class="btn btn-primary"
+        value={m.admin_software_update_rebuild_start()}
+      />
+    </form>
+  </div>
 </div>
