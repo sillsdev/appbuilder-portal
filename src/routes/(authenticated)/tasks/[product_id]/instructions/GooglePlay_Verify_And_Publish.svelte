a25af7d37d7d784ee4894d652093b564b060b902
Chris Hubbard <chris@thehubbards.org>
2025-12-05 19:30:49 -0500

    Download Google Play Store Listing during build for existing apps (#1410)
    * NOT COMPLETE: Add instructions for downloading play-listing.zip
    
    * Fix wording in GP Configuration
    
    * Update GooglePlay_Configuration downloading play listing
    
    * Update instructions
    
    * Fix wording
    
    * Address codereview issues

--- src/routes/(authenticated)/tasks/[product_id]/instructions/GooglePlay_Verify_And_Publish.svelte
+++ src/routes/(authenticated)/tasks/[product_id]/instructions/GooglePlay_Verify_And_Publish.svelte
@@ -27,6 +27,27 @@
     grid view.
   </li>
   <li>
+    If you set <strong>BUILD_DOWNLOAD_PLAY_LISTING</strong>
+    to have Scriptoria download the Google Play Store Listing information during the buid, it is available
+    in the
+    <strong>Product Files</strong>
+    as
+    <strong>play-listing-download</strong>
+    . Click on that entry to download it. Open the
+    <strong>Publishing &gt; App Store &gt; Google Play Store Listing</strong>
+    page and click on the
+    <strong>Import From Google Play...</strong>
+    button. This will start a wizard where you can select
+    <strong>Import from a download of the Google Play listing from Scriptoria</strong>
+    .
+  </li>
+  <li>
+    <strong>IMPORTANT!</strong>
+    Make sure to remove the
+    <strong>BUILD_DOWNLOAD_PLAY_LISTING</strong>
+    entry from the Publishing Properties to avoid downloading the listing on every build.
+  </li>
+  <li>
     Click <strong>Approve</strong>
     when you are satisfied that everything is ready to go and the app will be published.
     <sup>2</sup>
