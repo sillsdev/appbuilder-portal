sillsdev/appbuilder-portal

commit ee9952cf62322ffa0883fa0b299cc04d7d30d9b8

Fix logic for Google Play specific steps (#1411)
* App Store Preview only when Approval required

Fixes #1406

Co-Authored-By: Aidan Jones <aidan_jones@sil.org>

* Correct filter for new/existing app

Fixes #1408

Co-Authored-By: Aidan Jones <aidan_jones@sil.org>

* Simplify jump logic for author steps

Co-Authored-By: Aidan Jones <aidan_jones@sil.org>

* Fix inclusion filter for Make It Live

Co-Authored-By: Aidan Jones <aidan_jones@sil.org>

* Fix make it live transition filter for vis

src/lib/server/workflow/state-machine.ts
@@ -18,7 +18,8 @@ import {
   hasReviewers,
   isAuthorState,
   isDeprecated,
-  jump
+  jump,
+  newGPApp
 } from '../../workflowTypes';
 import { BullMQ, getQueues } from '../bullmq';
 import { deleteWorkflow, markResolved } from './dbProcedures';
@@ -61,7 +62,8 @@ export const WorkflowStateMachine = setup({
     options: input.options,
     productId: input.productId,
     hasAuthors: input.hasAuthors,
-    hasReviewers: input.hasReviewers
+    hasReviewers: input.hasReviewers,
+    existingApp: input.existingApp
   }),
   states: {
     [WorkflowState.Start]: {
@@ -99,42 +101,35 @@ export const WorkflowStateMachine = setup({
             workflowType: { is: WorkflowType.Startup }
           }
         }),
-        jump(
-          {
-            target: WorkflowState.Author_Configuration,
-            filter: {
-              workflowType: { is: WorkflowType.Startup }
-            }
-          },
-          [hasAuthors]
-        ),
+        jump({
+          target: WorkflowState.Author_Configuration,
+          filter: {
+            workflowType: { is: WorkflowType.Startup },
+            guards: [hasAuthors]
+          }
+        }),
         jump({ target: WorkflowState.Synchronize_Data }),
-        jump(
-          {
-            target: WorkflowState.Author_Download
-          },
-          [hasAuthors]
-        ),
+        jump({
+          target: WorkflowState.Author_Download,
+          filter: { guards: [hasAuthors] }
+        }),
         //note: authors can upload at any time, this state is just to prompt an upload
-        jump(
-          {
-            target: WorkflowState.Author_Upload
-          },
-          [hasAuthors]
-        ),
+        jump({
+          target: WorkflowState.Author_Upload,
+          filter: { guards: [hasAuthors] }
+        }),
         jump({ target: WorkflowState.Product_Build }),
         jump({
           target: WorkflowState.App_Store_Preview,
           filter: {
-            productType: { is: ProductType.Android_GooglePlay },
-            workflowType: { is: WorkflowType.Startup }
+            options: { has: WorkflowOptions.ApprovalProcess },
+            guards: [newGPApp]
           }
         }),
         jump({
           target: WorkflowState.Create_App_Store_Entry,
           filter: {
-            productType: { is: ProductType.Android_GooglePlay },
-            workflowType: { is: WorkflowType.Startup }
+            guards: [newGPApp]
           }
         }),
         jump({ target: WorkflowState.Verify_and_Publish }),
@@ -146,8 +141,7 @@ export const WorkflowStateMachine = setup({
         jump({
           target: WorkflowState.Make_It_Live,
           filter: {
-            productType: { is: ProductType.Android_GooglePlay },
-            workflowType: { is: WorkflowType.Startup }
+            guards: [newGPApp]
           }
         }),
         jump({ target: WorkflowState.Published }),
@@ -363,6 +357,7 @@ export const WorkflowStateMachine = setup({
             }
           },
           actions: assign({
+            existingApp: true,
             environment: ({ context }) => ({
               ...context.environment,
               [ENVKeys.GOOGLE_PLAY_EXISTING]: '1'
@@ -527,19 +522,20 @@ export const WorkflowStateMachine = setup({
             meta: {
               type: ActionType.Auto,
               includeWhen: {
-                productType: { is: ProductType.Android_GooglePlay },
-                workflowType: { is: WorkflowType.Startup }
+                options: { has: WorkflowOptions.ApprovalProcess },
+                guards: [newGPApp]
               }
             },
             /*
              * This transition is valid iff:
              * The product type is google play AND
              * The workflow type is startup AND
+             * The workflow requires admin approval AND
              * The project has NOT been uploaded to google play
              */
             guard: ({ context }) =>
-              context.productType === ProductType.Android_GooglePlay &&
-              context.workflowType === WorkflowType.Startup &&
+              newGPApp({ context }) &&
+              context.options.has(WorkflowOptions.ApprovalProcess) &&
               !context.environment[ENVKeys.PUBLISH_GOOGLE_PLAY_UPLOADED_BUILD_ID],
             target: WorkflowState.App_Store_Preview
           },
@@ -566,8 +562,8 @@ export const WorkflowStateMachine = setup({
     [WorkflowState.App_Store_Preview]: {
       meta: {
         includeWhen: {
-          productType: { is: ProductType.Android_GooglePlay },
-          workflowType: { is: WorkflowType.Startup }
+          options: { has: WorkflowOptions.ApprovalProcess },
+          guards: [newGPApp]
         }
       },
       entry: assign({
@@ -589,22 +585,7 @@ export const WorkflowStateMachine = setup({
           {
             meta: {
               type: ActionType.User,
-              user: RoleId.OrgAdmin,
-              includeWhen: {
-                options: {
-                  has: WorkflowOptions.AdminStoreAccess
-                }
-              }
-            },
-            target: WorkflowState.Create_App_Store_Entry
-          },
-          {
-            meta: {
-              type: ActionType.User,
-              user: RoleId.AppBuilder,
-              includeWhen: {
-                options: { none: new Set([WorkflowOptions.AdminStoreAccess]) }
-              }
+              user: RoleId.OrgAdmin
             },
             target: WorkflowState.Create_App_Store_Entry
           }
@@ -613,20 +594,7 @@ export const WorkflowStateMachine = setup({
           {
             meta: {
               type: ActionType.User,
-              user: RoleId.OrgAdmin,
-              includeWhen: {
-                options: { has: WorkflowOptions.AdminStoreAccess }
-              }
-            },
-            target: WorkflowState.Synchronize_Data
-          },
-          {
-            meta: {
-              type: ActionType.User,
-              user: RoleId.AppBuilder,
-              includeWhen: {
-                options: { none: new Set([WorkflowOptions.AdminStoreAccess]) }
-              }
+              user: RoleId.OrgAdmin
             },
             target: WorkflowState.Synchronize_Data
           }
@@ -636,8 +604,7 @@ export const WorkflowStateMachine = setup({
     [WorkflowState.Create_App_Store_Entry]: {
       meta: {
         includeWhen: {
-          productType: { is: ProductType.Android_GooglePlay },
-          workflowType: { is: WorkflowType.Startup }
+          guards: [newGPApp]
         }
       },
       entry: assign({
@@ -813,14 +780,10 @@ export const WorkflowStateMachine = setup({
             meta: {
               type: ActionType.Auto,
               includeWhen: {
-                productType: { is: ProductType.Android_GooglePlay },
-                workflowType: { is: WorkflowType.Startup }
+                guards: [newGPApp]
               }
             },
-            guard: ({ context }) =>
-              context.productType === ProductType.Android_GooglePlay &&
-              !context.environment[ENVKeys.GOOGLE_PLAY_EXISTING] &&
-              context.workflowType === WorkflowType.Startup,
+            guard: newGPApp,
             target: WorkflowState.Make_It_Live
           },
           {
@@ -862,13 +825,11 @@ export const WorkflowStateMachine = setup({
               type: ActionType.User,
               user: RoleId.OrgAdmin,
               includeWhen: {
-                workflowType: { is: WorkflowType.Startup }
+                guards: [newGPApp]
               }
             },
             actions: ({ context }) => markResolved(context.productId),
-            guard: ({ context }) =>
-              !context.environment[ENVKeys.GOOGLE_PLAY_EXISTING] &&
-              context.workflowType === WorkflowType.Startup,
+            guard: newGPApp,
             target: WorkflowState.Make_It_Live
           },
           {
@@ -876,13 +837,11 @@ export const WorkflowStateMachine = setup({
               type: ActionType.User,
               user: RoleId.AppBuilder,
               includeWhen: {
-                workflowType: { is: WorkflowType.Startup }
+                guards: [newGPApp]
               }
             },
             actions: ({ context }) => markResolved(context.productId),
-            guard: ({ context }) =>
-              !context.environment[ENVKeys.GOOGLE_PLAY_EXISTING] &&
-              context.workflowType === WorkflowType.Startup,
+            guard: newGPApp,
             target: WorkflowState.Make_It_Live
           },
           {
@@ -935,8 +894,7 @@ export const WorkflowStateMachine = setup({
     [WorkflowState.Make_It_Live]: {
       meta: {
         includeWhen: {
-          productType: { is: ProductType.Android_GooglePlay },
-          workflowType: { is: WorkflowType.Startup }
+          guards: [newGPApp]
         }
       },
       entry: assign({

src/lib/server/workflow/index.ts
@@ -11,6 +11,7 @@ import type { RoleId } from '../../prisma';
 import { ProductTransitionType, WorkflowType } from '../../prisma';
 import {
   ActionType,
+  ENVKeys,
   WorkflowAction,
   WorkflowState,
   includeStateOrTransition,
@@ -70,7 +71,8 @@ export class Workflow {
       ...config,
       hasAuthors: !!check?.Project._count.Authors,
       hasReviewers: !!check?.Project._count.Reviewers,
-      productId
+      productId,
+      existingApp: false
     });
     flow.flow = createActor(WorkflowStateMachine, {
       inspect: (e) => {
@@ -181,18 +183,20 @@ export class Workflow {
     if (!instance) {
       return null;
     }
+    const context = JSON.parse(instance.Context) as WorkflowInstanceContext;
     return {
       instanceId: instance.Id,
       definitionId: instance.WorkflowDefinition.Id,
       state: instance.State,
-      context: JSON.parse(instance.Context) as WorkflowInstanceContext,
+      context,
       input: {
         workflowType: instance.WorkflowDefinition.Type,
         productType: instance.WorkflowDefinition.ProductType,
         options: new Set(instance.WorkflowDefinition.WorkflowOptions),
         hasAuthors: !!instance.Product.Project._count.Authors,
         hasReviewers: !!instance.Product.Project._count.Reviewers,
-        productId
+        productId,
+        existingApp: !!context.environment[ENVKeys.GOOGLE_PLAY_EXISTING]
       }
     };
   }
@@ -364,7 +368,8 @@ export class Workflow {
       hasAuthors: undefined,
       hasReviewers: undefined,
       productType: undefined,
-      options: undefined
+      options: undefined,
+      existingApp: undefined
     } as WorkflowInstanceContext;
     const prodDefinition = (await DatabaseReads.products.findUnique({
       where: {

src/lib/workflowTypes.ts
@@ -1,5 +1,5 @@
-import { type TransitionConfig, and } from 'xstate';
-import type { RoleId, WorkflowType } from './prisma';
+import { type TransitionConfig } from 'xstate';
+import { type RoleId, WorkflowType } from './prisma';
 import type { SetFilter, ValueFilter } from './utils';
 import { filterSet, filterValue } from './utils';
 
@@ -188,6 +188,7 @@ export type WorkflowInput = WorkflowConfig & {
   productId: string;
   hasAuthors: boolean;
   hasReviewers: boolean;
+  existingApp: boolean;
 };
 
 /** Used for filtering based on specified WorkflowOptions and/or ProductType */
@@ -259,15 +260,21 @@ export function hasAuthors(args: { context: WorkflowInput }): boolean {
 export function hasReviewers(args: { context: WorkflowInput }): boolean {
   return args.context.hasReviewers;
 }
-export type Guards = typeof hasAuthors | typeof hasReviewers;
+export function newGPApp(args: { context: WorkflowInput }): boolean {
+  return (
+    args.context.productType === ProductType.Android_GooglePlay &&
+    args.context.workflowType === WorkflowType.Startup &&
+    !args.context.existingApp
+  );
+}
+export type Guards = typeof hasAuthors | typeof hasReviewers | typeof newGPApp;
 /**
  * @param params expected params of `canJump` guard from StartupWorkflow
  * @param optionalGuards other guards that can optionally be added.
  * @returns A properly configured object for the `always` array of the `Start` state for jumping to an arbitrary state.
  */
 export function jump(
-  params: JumpParams,
-  optionalGuards?: Guards[]
+  params: JumpParams
 ):
   | TransitionConfig<
       WorkflowContext,
@@ -281,9 +288,8 @@ export function jump(
       WorkflowStateMeta | WorkflowTransitionMeta
     >
   | string {
-  const j = (args: { context: WorkflowContext }) => canJump(args, params);
   return {
-    guard: optionalGuards ? and(optionalGuards.concat([j as Guards])) : j,
+    guard: (args: { context: WorkflowContext }) => canJump(args, params),
     target: params.target
   };
 }

commit 37d16e34c40622a6d1bd8dcc163b2a1884f6b814

Allow transition from Product Build to Create App Store Entry
Fixes issue introduced by #1411

src/lib/server/workflow/state-machine.ts
@@ -528,8 +528,7 @@ export const WorkflowStateMachine = setup({
             },
             /*
              * This transition is valid iff:
-             * The product type is google play AND
-             * The workflow type is startup AND
+             * The product is a *new* Google Play app
              * The workflow requires admin approval AND
              * The project has NOT been uploaded to google play
              */
@@ -538,6 +537,23 @@ export const WorkflowStateMachine = setup({
               context.options.has(WorkflowOptions.ApprovalProcess) &&
               !context.environment[ENVKeys.PUBLISH_GOOGLE_PLAY_UPLOADED_BUILD_ID],
             target: WorkflowState.App_Store_Preview
+          },
+          {
+            meta: {
+              type: ActionType.Auto,
+              includeWhen: {
+                guards: [newGPApp]
+              }
+            },
+            /*
+             * This transition is valid iff:
+             * The product is a *new* Google Play app
+             * The project has NOT been uploaded to google play
+             */
+            guard: ({ context }) =>
+              newGPApp({ context }) &&
+              !context.environment[ENVKeys.PUBLISH_GOOGLE_PLAY_UPLOADED_BUILD_ID],
+            target: WorkflowState.Create_App_Store_Entry
           },
           {
             // this is the normal transition for a successful build