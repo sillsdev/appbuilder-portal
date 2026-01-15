aa4e8f0e686851d4f26796d7a27c40733ccea31e
Aidan Jones <aidan_jones@sil.org>
2026-01-15 11:26:04 -0600

    Return error message for unexpected errors in SAB API

--- src/routes/(unauthenticated)/api/projects/[id=idNumber]/rebuild/+server.ts
+++ src/routes/(unauthenticated)/api/projects/[id=idNumber]/rebuild/+server.ts
@@ -5,42 +5,46 @@ import { doProductAction } from '$lib/products/server';
 import { DatabaseReads } from '$lib/server/database';
 
 export async function POST({ params, locals }) {
-  // Validate API token and get user ID
-  locals.security.requireApiToken();
-  const userId = locals.security.userId;
+  try {
+    // Validate API token and get user ID
+    locals.security.requireApiToken();
+    const userId = locals.security.userId;
 
-  const projectId = parseInt(params.id);
+    const projectId = parseInt(params.id);
 
-  // Fetch project and owner
-  const project = await DatabaseReads.projects.findFirst({
-    where: {
-      Id: projectId,
-      // Enforces ownership
-      Owner: { Id: userId }
-    },
-    select: {
-      Owner: { select: { Id: true } }
+    // Fetch project and owner
+    const project = await DatabaseReads.projects.findFirst({
+      where: {
+        Id: projectId,
+        // Enforces ownership
+        Owner: { Id: userId }
+      },
+      select: {
+        Owner: { select: { Id: true } }
+      }
+    });
+
+    if (!project) {
+      return createAppBuildersError(404, `Project id=${projectId} not found or access denied`);
     }
-  });
+    const rebuildableProducts = await DatabaseReads.products.findMany({
+      where: rebuildableProductsWhere(projectId, userId),
+      select: { Id: true }
+    });
 
-  if (!project) {
-    return createAppBuildersError(404, `Project id=${projectId} not found or access denied`);
+    if (rebuildableProducts.length === 0) {
+      return createAppBuildersError(400, 'Project does not meet rebuild conditions');
+    }
+
+    // Trigger rebuild on each eligible product
+    for (const product of rebuildableProducts) {
+      await doProductAction(product.Id, ProductActionType.Rebuild);
+    }
+
+    return json({
+      message: `Triggered rebuild for ${rebuildableProducts.length} product(s)`
+    });
+  } catch (e) {
+    return createAppBuildersError(500, `Unexpected Error: ${e}`);
   }
-  const rebuildableProducts = await DatabaseReads.products.findMany({
-    where: rebuildableProductsWhere(projectId, userId),
-    select: { Id: true }
-  });
-
-  if (rebuildableProducts.length === 0) {
-    return createAppBuildersError(400, 'Project does not meet rebuild conditions');
-  }
-
-  // Trigger rebuild on each eligible product
-  for (const product of rebuildableProducts) {
-    await doProductAction(product.Id, ProductActionType.Rebuild);
-  }
-
-  return json({
-    message: `Triggered rebuild for ${rebuildableProducts.length} product(s)`
-  });
 }
