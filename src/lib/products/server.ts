ddf7516e56e7a71829247c94fedccba8721801db
Aidan Jones <aidan_jones@sil.org>
2026-01-15 11:52:35 -0600

    Use DateUpdated to get most recent publication

--- src/lib/products/server.ts
+++ src/lib/products/server.ts
@@ -111,7 +111,7 @@ export async function getPublishedFile(productId: string, type: string) {
       }
     },
     orderBy: {
-      ReleaseId: 'desc'
+      DateUpdated: 'desc'
     }
   });
   for (const publication of publications) {
