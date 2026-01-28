683553d0be652fd587f30815246d7113855984fc
Aidan Jones <aidan_jones@sil.org>
2026-01-28 13:54:11 -0600

    Redesign main groups page

--- src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.server.ts
+++ src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.server.ts
@@ -1,9 +1,8 @@
-import { fail, superValidate } from 'sveltekit-superforms';
+import { superValidate } from 'sveltekit-superforms';
 import { valibot } from 'sveltekit-superforms/adapters';
 import * as v from 'valibot';
-import type { Actions, PageServerLoad } from './$types';
-import { DatabaseReads, DatabaseWrites } from '$lib/server/database';
-import { deleteSchema } from '$lib/valibot';
+import type { PageServerLoad } from './$types';
+import { DatabaseReads } from '$lib/server/database';
 
 const addGroupSchema = v.object({
   name: v.string(),
@@ -14,35 +13,20 @@ export const load = (async (event) => {
   event.locals.security.requireAdminOfOrg(parseInt(event.params.id));
   const { organization } = await event.parent();
   return {
-    groups: await DatabaseReads.groups.findMany({ where: { OwnerId: organization.Id } }),
+    groups: await DatabaseReads.groups.findMany({
+      where: { OwnerId: organization.Id },
+      select: {
+        Id: true,
+        Name: true,
+        Description: true,
+        _count: {
+          select: {
+            Users: true,
+            Projects: true
+          }
+        }
+      }
+    }),
     form: await superValidate(valibot(addGroupSchema))
   };
 }) satisfies PageServerLoad;
-
-export const actions = {
-  async addGroup(event) {
-    event.locals.security.requireAdminOfOrg(parseInt(event.params.id));
-    const form = await superValidate(event.request, valibot(addGroupSchema));
-    if (!form.valid) return fail(400, { form, ok: false });
-    const group = await DatabaseWrites.groups.createGroup(
-      form.data.name,
-      form.data.description,
-      parseInt(event.params.id)
-    );
-    return { form, ok: true, createdId: group.Id };
-  },
-  async deleteGroup(event) {
-    event.locals.security.requireAdminOfOrg(parseInt(event.params.id));
-    const form = await superValidate(event.request, valibot(deleteSchema));
-    if (!form.valid) return fail(400, { form, ok: false });
-    if (
-      // if user modified hidden values
-      !(await DatabaseReads.groups.findFirst({
-        where: { Id: form.data.id, OwnerId: parseInt(event.params.id) }
-      }))
-    ) {
-      return fail(403, { form, ok: false });
-    }
-    return { form, ok: await DatabaseWrites.groups.deleteGroup(form.data.id) };
-  }
-} satisfies Actions;
--- src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.svelte
+++ src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import { superForm } from 'sveltekit-superforms';
-  import type { ActionData, PageData } from './$types';
-  import { enhance as svk_enhance } from '$app/forms';
+  import type { PageData } from './$types';
+  import { goto } from '$app/navigation';
   import IconContainer from '$lib/components/IconContainer.svelte';
-  import LabeledFormInput from '$lib/components/settings/LabeledFormInput.svelte';
+  import DataDisplayBox from '$lib/components/settings/DataDisplayBox.svelte';
   import { m } from '$lib/paraglide/messages';
-  import { getLocale } from '$lib/paraglide/runtime';
-  import { toast } from '$lib/utils';
+  import { getLocale, localizeHref } from '$lib/paraglide/runtime';
   import { byName } from '$lib/utils/sorting';
 
   interface Props {
@@ -17,93 +15,25 @@
 
   let groups = $state(data.groups);
 
-  const { form, enhance } = superForm(data.form, {
-    resetForm: true,
-    onUpdate({ form, result, formElement }) {
-      if (form.valid && result.type === 'success') {
-        toast('success', m.org_groupCreated());
-        groups.push({
-          Id: result.data.createdId,
-          Name: form.data.name,
-          Description: form.data.description,
-          OwnerId: data.organization.Id
-        });
-        formElement.reset();
-      } else {
-        toast('error', m.errors_generic({ errorMessage: '' }));
-      }
-    }
-  });
+  const base = $derived(`/organizations/${data.organization.Id}/settings/groups`);
 </script>
 
-{#each groups.toSorted((a, b) => byName(a, b, getLocale())) as group}
-  <form
-    action="?/deleteGroup"
-    class="m-2"
-    method="post"
-    use:svk_enhance={() => {
-      return async ({ result }) => {
-        if (result.type === 'success') {
-          const data = result.data as ActionData;
-          if (data?.ok) {
-            toast('success', m.org_groupDeleted());
-            groups = groups.filter((g) => g.Id !== group.Id);
-          } else if (data?.form.valid) {
-            toast('error', m.org_groupHasProjects({ group: group.Name ?? '' }));
-          } else {
-            toast('error', m.errors_generic({ errorMessage: '' }));
-          }
-        }
-      };
-    }}
-  >
-    <input type="hidden" name="id" value={group.Id} />
-    <div class="border w-full flex flex-row p-2 rounded-md items-center place-content-between">
-      <div>
-        <span class="p-1">{group.Name}</span>
-      </div>
-        <span class="p-1 badge badge-primary rounded-md">{group.Description}</span>
-      <button class="btn btn-xs btn-ghost p-0" type="submit">
-        <IconContainer icon="mdi:close" class="" width={26} />
-      </button>
-    </div>
-  </form>
-{/each}
-<form
-  action="?/addGroup"
-  class="m-2 fieldset border border-black/20 dark:border-gray-50/20 rounded-lg p-2"
-  method="post"
-  use:enhance
-  onreset={(e) => {
-    for (const el of e.currentTarget.elements) {
-      if (el instanceof HTMLInputElement) {
-        el.setCustomValidity('');
-      }
-    }
-  }}
->
-  <div class="fieldset-legend">{m.org_addGroupButton()}</div>
-  <div class="flex flex-col sm:flex-row w-full sm:space-x-2 items-center">
-    <LabeledFormInput key="common_name">
-      <input
-        class="w-full input input-bordered validator"
-        type="text"
-        name="name"
-        bind:value={$form.name}
-        required
-      />
-      <span class="validator-hint">{m.formErrors_nameEmpty()}</span>
-    </LabeledFormInput>
-    <LabeledFormInput key="common_description">
-      <input
-        class="w-full input input-bordered validator"
-        type="text"
-        name="description"
-        bind:value={$form.description}
-        required
-      />
-      <span class="validator-hint">&nbsp;</span>
-    </LabeledFormInput>
-    <input type="submit" class="btn btn-primary w-full sm:w-auto" value={m.common_save()} />
-  </div>
-</form>
+<a href={localizeHref(`${base}/new`)} class="btn btn-outline m-4 mt-0">
+  {m.org_addGroupButton()}
+</a>
+
+<div class="flex flex-col w-full">
+  {#each groups.toSorted((a, b) => byName(a, b, getLocale())) as group}
+    <DataDisplayBox
+      editable
+      onEdit={() => goto(localizeHref(`${base}/edit?id=${group.Id}`))}
+      title={group.Name}
+      fields={[{ key: 'common_description', value: group.Description }]}
+    >
+      <IconContainer icon="mdi:account-group" width={20} />
+      {group._count.Users}
+      <IconContainer icon="fa7-solid:diagram-project" width={20} />
+      {group._count.Projects}
+    </DataDisplayBox>
+  {/each}
+</div>
