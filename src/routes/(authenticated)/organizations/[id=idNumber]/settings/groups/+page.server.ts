7403d5a0f0b5ad955320d547accc538a702d4089
Aidan Jones <aidan_jones@sil.org>
2026-01-29 14:18:39 -0600

    Remove unneeded form

--- src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.server.ts
+++ src/routes/(authenticated)/organizations/[id=idNumber]/settings/groups/+page.server.ts
@@ -1,13 +1,5 @@
-import { superValidate } from 'sveltekit-superforms';
-import { valibot } from 'sveltekit-superforms/adapters';
-import * as v from 'valibot';
 import type { PageServerLoad } from './$types';
 import { DatabaseReads } from '$lib/server/database';
-
-const addGroupSchema = v.object({
-  name: v.string(),
-  description: v.string()
-});
 
 export const load = (async (event) => {
   event.locals.security.requireAdminOfOrg(parseInt(event.params.id));
@@ -26,7 +18,6 @@ export const load = (async (event) => {
           }
         }
       }
-    }),
-    form: await superValidate(valibot(addGroupSchema))
+    })
   };
 }) satisfies PageServerLoad;
