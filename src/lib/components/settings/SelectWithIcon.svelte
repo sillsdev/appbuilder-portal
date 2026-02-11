d3b303b8f46322f61e241e36c268e5f5e740bf15
Aidan Jones <aidan_jones@sil.org>
2026-02-11 15:24:10 -0600

    Break select with icon into separate component

---
+++ src/lib/components/settings/SelectWithIcon.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { ClassValue, HTMLSelectAttributes } from 'svelte/elements';
+  import IconContainer from '$lib/components/IconContainer.svelte';
+  import { getLocale } from '$lib/paraglide/runtime';
+  import { byName } from '$lib/utils/sorting';
+
+  interface Props {
+    class?: ClassValue;
+    items: {
+      Id: number;
+      Name: string;
+      icon: string;
+    }[];
+    value: number | null;
+    extra?: Snippet;
+    attr?: HTMLSelectAttributes;
+  }
+
+  let { class: classes, items, value = $bindable(), extra, attr = {} }: Props = $props();
+
+  const current = $derived(items.find((item) => item.Id === value));
+</script>
+
+<div class={['select gap-4', classes]}>
+  {#if current}
+    <IconContainer icon={current.icon} width={20} />
+  {/if}
+  <select {...attr} bind:value class:ps-0!={current}>
+    {@render extra?.()}
+    {#each items.toSorted((a, b) => byName(a, b, getLocale())) as item}
+      <option value={item.Id}>
+        <IconContainer icon={item.icon} width={20} />
+        {item.Name}
+      </option>
+    {/each}
+  </select>
+</div>
--- src/lib/projects/components/ProductDefinitionFilter.svelte
+++ src/lib/projects/components/ProductDefinitionFilter.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import type { Prisma } from '@prisma/client';
   import type { ClassValue } from 'svelte/elements';
-  import IconContainer from '$lib/components/IconContainer.svelte';
+  import SelectWithIcon from '$lib/components/settings/SelectWithIcon.svelte';
   import { getProductIcon } from '$lib/icons';
   import { m } from '$lib/paraglide/messages';
-  import { getLocale } from '$lib/paraglide/runtime';
-  import { byName } from '$lib/utils/sorting';
 
   interface Props {
     class?: ClassValue;
@@ -24,19 +22,15 @@
   }
 
   let { class: classes, productDefinitions, value = $bindable() }: Props = $props();
-
-  const current = $derived(productDefinitions.find((pD) => pD.Id === value));
 </script>
 
-<label class={['select gap-4', classes]}>
-  <IconContainer icon={current ? getProductIcon(current!.Workflow.ProductType) : ''} width={20} />
-  <select bind:value name="productDefinitionId" class="ps-0!">
+<SelectWithIcon
+  class={classes}
+  attr={{ name: 'productDefinitionId' }}
+  bind:value
+  items={productDefinitions.map((pd) => ({ ...pd, icon: getProductIcon(pd.Workflow.ProductType) }))}
+>
+  {#snippet extra()}
     <option value={null} selected>{m.filters_allProdDefs()}</option>
-    {#each productDefinitions.toSorted((a, b) => byName(a, b, getLocale())) as pD}
-      <option value={pD.Id}>
-        <IconContainer icon={getProductIcon(pD.Workflow.ProductType)} width={20} />
-        {pD.Name}
-      </option>
-    {/each}
-  </select>
-</label>
+  {/snippet}
+</SelectWithIcon>
