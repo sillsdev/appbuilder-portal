ab812b5a35f2d17a535270b4327f9a8a7996a08d
James-Voight <jpvoight@liberty.edu>
2026-02-16 14:13:45 -0500

    local language names now accessible and data is pulled from the play listing manifest

---
+++ src/routes/(unauthenticated)/+layout.server.ts
@@ -0,0 +1,27 @@
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import type { LayoutServerLoad } from './$types';
+import { type L10NEntries, type L10NKeys } from '$lib/locales.svelte';
+import { type Locale, locales } from '$lib/paraglide/runtime';
+import type { Entries } from '$lib/utils';
+
+export const load: LayoutServerLoad = async () => {
+  const localDir = join(process.cwd(), 'languages');
+
+  return {
+    localizedNames: (await Promise.all(
+      locales.map(async (locale) => {
+        const filePath = join(localDir, locale, 'ldml.json');
+
+        let ret = null;
+        if (existsSync(filePath)) {
+          const file = (await readFile(filePath)).toString();
+          ret = JSON.parse(file) as Entries<L10NKeys, Entries<string, string>>;
+        }
+        return [locale, ret] as [Locale, typeof ret];
+      })
+    )) as L10NEntries
+  };
+};
+
