74e4ac1afc86573f50cb358dbcee297bdf205712
James-Voight <jpvoight@liberty.edu>
2026-02-16 14:13:45 -0500

    local language names now accessible and data is pulled from the play listing manifest

--- src/routes/(unauthenticated)/+layout.svelte
+++ src/routes/(unauthenticated)/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
+  import type { LayoutData } from './$types';
+  import { createl10nMapFromEntries, l10nMap } from '$lib/locales.svelte';
 
   interface Props {
+    data: LayoutData;
     children?: Snippet;
   }
 
-  let { children }: Props = $props();
+  let { data, children }: Props = $props();
+
+  $effect(() => {
+    l10nMap.value = createl10nMapFromEntries(data.localizedNames);
+  });
 </script>
 
 <div
   data-theme="light"
-  class="grid w-full h-full place-items-center place-content-center text-base-content"
+  class="grid grid-cols-1 w-full min-h-screen place-items-stretch place-content-start text-base-content sm:place-items-center sm:place-content-center"
 >
   {@render children?.()}
 </div>
