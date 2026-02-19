cfa1d5433c53bd220e9a02accbf4424aa081f5ef
Aidan Jones <aidan_jones@sil.org>
2026-02-19 10:46:10 -0600

    Add icon support to LabeledFormInput

--- src/lib/components/settings/LabeledFormInput.svelte
+++ src/lib/components/settings/LabeledFormInput.svelte
@@ -2,16 +2,24 @@
     @component
     An input slot for a form (stylized)
 -->
-<script lang="ts">
+<script lang="ts" generics="T">
   import type { Snippet } from 'svelte';
+  import type { ClassValue, HTMLInputAttributes } from 'svelte/elements';
+  import IconContainer from '../IconContainer.svelte';
   import type { ValueKey } from '$lib/locales.svelte';
   import { m } from '$lib/paraglide/messages';
 
   interface Props extends ValueKey {
     children?: Snippet;
+    input?: HTMLInputAttributes & {
+      class?: ClassValue;
+      err?: string;
+      icon?: string;
+    };
+    value?: T;
   }
 
-  let { key, params = {}, class: classes, children }: Props = $props();
+  let { key, params = {}, class: classes, children, input, value = $bindable() }: Props = $props();
 </script>
 
 <label class={['flex flex-col w-full', classes]}>
@@ -21,5 +29,17 @@
       {m[key](params as any)}
     </span>
   </div>
-  {@render children?.()}
+  {#if input}
+    <div class={['input input-bordered w-full validator']}>
+      {#if input.icon}
+        <IconContainer icon={input.icon} width={20} class="cursor-pointer opacity-80" />
+      {/if}
+      <input type="text" {...input} bind:value />
+    </div>
+    <span class="validator-hint">
+      {#if input.err}{input.err}{:else}&nbsp;{/if}
+    </span>
+  {:else}
+    {@render children?.()}
+  {/if}
 </label>
