a4264d0a472f9d567c05e3addd0b67ca4bb7a27c
Josiah Lane <josiahjlane@gmail.com>
2026-02-20 10:43:56 -0500

    Cloudflare turnstile

---
+++ src/routes/(unauthenticated)/api/delete-request/+server.ts
@@ -0,0 +1,30 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const { email, token } = await request.json();
+
+    if (!token) return json({ success: false }, { status: 400 });
+
+    const secret = process.env.TURNSTILE_SECRET_KEY;
+    const formData = new URLSearchParams();
+
+    formData.append('secret', secret!);
+    formData.append('response', token);
+
+    const verification = await fetch(
+        'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+        {
+            method: 'POST',
+            body: formData
+        }
+    );
+
+    const result = await verification.json();
+
+    if (!result.success) return json({ success: false }, { status: 400 });
+
+    // TODO: process deletion request with `email` here
+
+    return json({ success: true });
+};
