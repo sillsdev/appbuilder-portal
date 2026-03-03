a4655613cfc7a69d8438a3bc655a7071f6868379
Aidan Jones <aidan_jones@sil.org>
2026-03-03 11:55:56 -0600

    Refactor SystemStatus display

--- src/routes/(authenticated)/admin/settings/build-engines/+page.server.ts
+++ src/routes/(authenticated)/admin/settings/build-engines/+page.server.ts
@@ -1,21 +1,27 @@
 // src/routes/admin/settings/build-engines/+page.server.ts
 
 import type { PageServerLoad } from './$types';
+import { activeSystems } from '$lib/organizations/server';
 import { DatabaseReads } from '$lib/server/database';
 
 export const load = (async (event) => {
   event.locals.security.requireSuperAdmin();
-  const buildEngines = await DatabaseReads.systemStatuses.findMany({
-    include: { SupportedVersions: true, Organizations: { select: { Name: true } } }
-  });
-
-  const applications = await DatabaseReads.applicationTypes.findMany({
-    select: { Id: true, Description: true }
-  });
 
   return {
-    buildEngines,
-    applications,
+    buildEngines: await DatabaseReads.systemStatuses.findMany({
+      where: activeSystems,
+      include: {
+        SystemVersions: true,
+        Organization: { select: { Name: true } }
+      }
+    }),
+    applications: new Map(
+      (
+        await DatabaseReads.applicationTypes.findMany({
+          select: { Id: true, Description: true }
+        })
+      ).map((t) => [t.Id, t.Description])
+    ),
     defaultUsers: await DatabaseReads.organizations.count({
       where: { UseDefaultBuildEngine: true }
     })
