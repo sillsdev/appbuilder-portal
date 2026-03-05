b17b77de948256ef93d1ea55484146bf85409e06
Aidan Jones <aidan_jones@sil.org>
2026-03-05 10:46:50 -0600

    Update SSE for user addition/removal

--- src/lib/server/database/Organizations.ts
+++ src/lib/server/database/Organizations.ts
@@ -1,4 +1,5 @@
 import type { Prisma } from '@prisma/client';
+import { BullMQ, getQueues } from '../bullmq';
 import prisma from './prisma';
 import type { RequirePrimitive } from './utility';
 
@@ -79,7 +80,7 @@ export async function toggleUser(OrganizationId: number, UserId: number, enabled
     select: { Id: true }
   });
 
-  return (
+  const updated =
     (enabled || !userHasProjectInOrg) &&
     !!(await prisma.users.update({
       where: { Id: UserId },
@@ -93,6 +94,41 @@ export async function toggleUser(OrganizationId: number, UserId: number, enabled
       select: {
         Id: true
       }
-    }))
-  );
+    }));
+
+  if (updated) {
+    // remove user from groups/roles too
+    if (!enabled) {
+      await prisma.users.update({
+        where: { Id: UserId },
+        data: {
+          Groups: {
+            disconnect: await prisma.groups.findMany({
+              where: { OwnerId: OrganizationId, Users: { some: { Id: UserId } } },
+              select: { Id: true }
+            })
+          },
+          UserRoles: {
+            deleteMany: {
+              OrganizationId
+            }
+          }
+        }
+      });
+    }
+    await getQueues().SvelteSSE.add(
+      `Update Projects for Org #${OrganizationId} (user #${UserId} ${enabled ? 'added' : 'removed'})`,
+      {
+        type: BullMQ.JobType.SvelteSSE_UpdateProjectGroups,
+        projectIds: (
+          await prisma.projects.findMany({
+            where: { OrganizationId },
+            select: { Id: true }
+          })
+        ).map((p) => p.Id)
+      }
+    );
+  }
+
+  return updated;
 }
