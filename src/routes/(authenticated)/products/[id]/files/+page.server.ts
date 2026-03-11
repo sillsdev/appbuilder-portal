901b31ce4270ce5e3f787b3ec55f5d221a32c87b
Aidan Jones <aidan_jones@sil.org>
2026-03-11 13:26:07 -0500

    Fix security check order in product files

--- src/routes/(authenticated)/products/[id]/files/+page.server.ts
+++ src/routes/(authenticated)/products/[id]/files/+page.server.ts
@@ -7,6 +7,7 @@ import { DatabaseReads } from '$lib/server/database';
 import { paginateSchema } from '$lib/valibot';
 
 export const load = (async ({ params, locals }) => {
+  locals.security.requireAuthenticated();
   const project = (
     await DatabaseReads.products.findUnique({
       where: { Id: params.id },
@@ -17,7 +18,6 @@ export const load = (async ({ params, locals }) => {
     })
   )?.Project;
   if (!project) error(404);
-  locals.security.requireAuthenticated();
   locals.security.requireProjectReadAccess(
     await DatabaseReads.groups.findMany({
       where: { Users: { some: { Id: locals.security.userId } }, Id: project.GroupId }
