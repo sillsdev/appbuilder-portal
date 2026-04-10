e5bde28d29d8e75f33585f16c6301f16ba27f822
Judah Sotomayor <judah.sotomayor@pm.me>
2026-04-09 22:58:14 -0400

    Lint fix.

--- src/lib/server/workflow/index.ts
+++ src/lib/server/workflow/index.ts
@@ -133,12 +133,12 @@ export class Workflow {
     flow.flow = createActor(WorkflowStateMachine, {
       snapshot: snap
         ? WorkflowStateMachine.resolveState({
-          value: snap.state,
-          context: {
-            ...snap.context,
-            ...flow.input
-          }
-        })
+            value: snap.state,
+            context: {
+              ...snap.context,
+              ...flow.input
+            }
+          })
         : undefined,
       inspect: (e) => {
         if (e.type === '@xstate.snapshot') flow.inspect(e);
@@ -427,9 +427,9 @@ export class Workflow {
       AllowedUserNames:
         t.meta.type === ActionType.User
           ? userNames
-            .filter(([name, roles]) => name && roles.has(t.meta.user))
-            .map((user) => user[0])
-            .join(', ')
+              .filter(([name, roles]) => name && roles.has(t.meta.user))
+              .map((user) => user[0])
+              .join(', ')
           : null,
       TransitionType: ProductTransitionType.Activity,
       InitialState: Workflow.stateName(state),
@@ -556,13 +556,13 @@ export class Workflow {
 
     const user = userId
       ? await DatabaseReads.users.findUnique({
-        where: {
-          Id: userId
-        },
-        select: {
-          Name: true
-        }
-      })
+          where: {
+            Id: userId
+          },
+          select: {
+            Name: true
+          }
+        })
       : null;
 
     if (transition) {
