2e3b9178d780f179dacc382ce7c6accca32ef83e
Judah Sotomayor <judah.sotomayor@pm.me>
2026-04-09 22:39:37 -0400

    Complete refactor of load function.
    Once again overhauling load to further simplify the query and data
    returned by it.

--- src/routes/(authenticated)/software-update/[[orgId=number]]/+page.server.ts
+++ src/routes/(authenticated)/software-update/[[orgId=number]]/+page.server.ts
@@ -1,113 +1,13 @@
 import { fail } from '@sveltejs/kit';
-import { Prisma, PrismaClient } from '@prisma/client';
 import { superValidate } from 'sveltekit-superforms';
 import { valibot } from 'sveltekit-superforms/adapters';
 import * as v from 'valibot';
 import type { Actions, PageServerLoad } from './$types';
 import { ProductActionType } from '$lib/products';
 import { doProductAction } from '$lib/products/server';
-import { BuildEngine } from '$lib/server/build-engine-api';
 import { DatabaseReads, DatabaseWrites } from '$lib/server/database';
 import { getRebuildsForOrgIds } from '$lib/software-updates/sse';
 import { filterAdminOrgs } from '$lib/utils/roles';
-
-interface ProductToRebuild {
-  id: string; // Product ID (UUID)
-  latestVersion: string | null;
-  requiredVersion: string | null;
-  projectId: number;
-  projectName: string;
-  applicationTypeId: number;
-  buildEngineUrl: string;
-  organizationId: number;
-}
-
-/**
- * Fetches products that need to be rebuilt based on the provided organizations.
- * Checks to make sure the product is part of a project that has rebuildOnSoftwareUpdate enabled,
- * and that the latest product build's AppBuilderVersion does not match the required SystemVersion.
- * @param searchOrgs An array or organizations to include products from.
- * @param selectedTypeIds Optional array of ApplicationTypeIds to filter by. If provided, only products with these types are returned.
- * @returns Array of Products
- */
-async function getProductsForRebuild(
-  searchOrgs: number[],
-  selectedTypeIds?: number[]
-): Promise<ProductToRebuild[]> {
-
-  // Get system versions into a table.
-  let systemVersions = (await DatabaseReads.systemVersions.findMany(
-    {
-      select: {
-        BuildEngineUrl: true,
-        ApplicationTypeId: true,
-        Version: true
-      }
-    })).reduce((acc, v) => {
-
-      if (!acc[v.BuildEngineUrl]) {
-        acc[v.BuildEngineUrl] = {};
-      }
-
-      acc[v.BuildEngineUrl][v.ApplicationTypeId] = v.Version;
-      return acc;
-    }, {});
-
-  // Get products eligible to rebuild.
-  // If a product's latest rebuild does not use its current build engine's version, it needs to be rebuilt.
-  const eligibleProducts = (await DatabaseReads.products.findMany({
-    where: {
-      Project: {
-        OrganizationId: { in: searchOrgs },
-        DateArchived: null, // Project has not been archived
-        RebuildOnSoftwareUpdate: true, // Project setting is true
-        ...(selectedTypeIds && selectedTypeIds.length > 0 && { TypeId: { in: selectedTypeIds } })
-      },
-      WorkflowInstance: null, // Only products without active workflows
-      DatePublished: { not: null } // Only products that have been published at least once
-    },
-    select: {
-      Id: true,
-      ProjectId: true,
-      ProductBuilds: {
-        orderBy: { DateCreated: 'desc' },
-        take: 1, // Only take the most recent
-        select: {
-          AppBuilderVersion: true
-        }
-      },
-      Project: {
-        select: {
-          TypeId: true,
-          Name: true,
-          OrganizationId: true,
-          Organization: {
-            select: {
-              BuildEngineUrl: true
-            }
-          },
-        }
-      }
-    }
-  })).map((product) => {
-    const buildEngineUrl = product.Project.Organization.BuildEngineUrl ?? '';
-    const organizationId = product.Project.OrganizationId;
-    const latestVersion = product.ProductBuilds[0]?.AppBuilderVersion ?? null;
-    const requiredVersion = systemVersions[buildEngineUrl][organizationId.toString()] ?? null;
-    return {
-      id: product.Id,
-      projectId: product.ProjectId,
-      projectName: product.Project.Name ?? '',
-      applicationTypeId: product.Project.TypeId,
-      buildEngineUrl,
-      organizationId,
-      latestVersion,
-      requiredVersion,
-    };
-  }).filter((product) => product.requiredVersion !== product.latestVersion);
-
-  return eligibleProducts;
-}
 
 const formSchema = v.object({
   comment: v.pipe(v.string(), v.minLength(1)),
@@ -121,171 +21,104 @@ export const load = (async ({ locals, params }) => {
     locals.security.requireAdminOfAny();
   }
 
-  // Determine what organizations are being affected and fetch their names.
-  const organizationIds = (await DatabaseReads.organizations.findMany({
-    where: filterAdminOrgs(locals.security, params.orgId ? Number(params.orgId) : undefined),
-    select: {
-      Id: true,
-    }
-  })).map((o) => o.Id);
-
-  let systemVersions = (await DatabaseReads.systemVersions.findMany(
-    {
-      select: {
-        BuildEngineUrl: true,
-        ApplicationTypeId: true,
-        Version: true
-      }
-    })).reduce((acc, v) => {
-
-      if (!acc[v.BuildEngineUrl]) {
-        acc[v.BuildEngineUrl] = {};
-      }
-
-      acc[v.BuildEngineUrl][v.ApplicationTypeId] = v.Version;
-      return acc;
-    }, {});
-
-  const prisma = DatabaseReads.$extends({
-    result: {
-      products: {
-        LatestVersion: {
-          compute(product) {
-            return product.ProductBuilds[0].AppBuilderVersion;
-          },
-        },
-        RequiredVersion: {
-          compute(product) {
-            const p = product.Project;
-            return systemVersions[p.Organization.BuildEngineUrl][p.ApplicationType.Id];
-          }
+  const eligibleProducts = await DatabaseReads.products.findMany({
+    // Products that are rebuildable:
+    where: {
+      // - Have already been published once
+      DatePublished: { not: null },
+      // - Are not currently being rebuild
+      WorkflowInstance: null,
+      // - Have a definition that specifies a rebuild workflow
+      NOT: {
+        ProductDefinition: { RebuildWorkflow: null }
+      },
+      Project: {
+        Organization: {
+          ...filterAdminOrgs(locals.security, params.orgId ? Number(params.orgId) : undefined)
         }
       }
     },
-  });
-
-  const projects = (await prisma.organizations.findMany({
-    where: {
-      ...filterAdminOrgs(locals.security, params.orgId ? Number(params.orgId) : undefined),
-    },
     select: {
       Id: true,
-      Name: true,
-      Projects: {
-        orderBy: { Name: 'asc' },
-        where: {
-          DateArchived: null, // Project has not been archived
-          RebuildOnSoftwareUpdate: true, // Project setting is true
-          Products: {
-            some: {
-              WorkflowInstance: null,
-              DatePublished: { not: null },
-            }
-          }
-        },
+      Project: {
         select: {
           Name: true,
-          ApplicationType: {
-            select: {
-              Id: true,
-            }
-          },
-          Products: {
-            where: {
-              WorkflowInstance: null,
-              DatePublished: { not: null },
-            },
-            select: {
-              Id: true,
-              LatestVersion: true,
-              RequiredVersion: true,
-
-              Project: {
-                select: {
-                  ApplicationType: {
-                    select: {
-                      Id: true,
-                    }
-                  },
-                  Organization: {
-                    select: {
-                      BuildEngineUrl: true,
-                    }
-                  },
-                }
-              },
-
-              ProductBuilds: {
-                orderBy: { DateCreated: 'desc' },
-                take: 1,
-
-                select: {
-                  AppBuilderVersion: true
-                }
-              }
-            }
-          }
+          ApplicationType: { select: { Id: true } },
+          Organization: { select: { Id: true } }
         }
+      },
+      ProductBuilds: {
+        orderBy: { DateCreated: 'desc' },
+        take: 1,
+        select: { AppBuilderVersion: true }
       }
     }
-  })).map((org) => {
-    // Filter projects that still have at least one product after version check
-    const filteredProjects = org.Projects
-      .map((project) => {
-        // Filter products where LatestVersion !== RequiredVersion
-        const filteredProducts = project.Products
-          .filter(({ LatestVersion, RequiredVersion }) =>
-            LatestVersion !== RequiredVersion)
-          .map(({ Id, LatestVersion, RequiredVersion }) => ({
-            Id,
-            LatestVersion,
-            RequiredVersion,
-          }));
+  });
 
-        return {
-          ...project,
-          Products: filteredProducts,
-        };
-      })
-      // Remove projects with no remaining products
-      .filter((project) => project.Products.length > 0);
+  console.log(params);
+  const systemStatuses = await DatabaseReads.systemStatuses.findMany({
+    where: {
+      Organization: {
+        ...filterAdminOrgs(locals.security, params.orgId ? Number(params.orgId) : undefined),
+        Id: {
+          in: eligibleProducts.map((p) => p.Project.Organization.Id)
+        }
+      }
+    },
+    select: {
+      Organization: { select: { Id: true, Name: true } },
+      SystemVersions: { select: { ApplicationTypeId: true, Version: true } }
+    }
+  });
 
-    return {
-      ...org,
-      Projects: filteredProjects,
-    };
-  }).reduce((acc, org) => {
-    acc[org.Id.toString()] = org;
-    return acc
-  }, {});
+  const organizations = systemStatuses.map((ss) => ss.Organization.Name);
+  const systems = new Map<number, Map<number, string>>(
+    systemStatuses.map((s) => [
+      s.Organization.Id,
+      new Map(s.SystemVersions.map((v) => [v.ApplicationTypeId, v.Version]))
+    ])
+  );
 
-  // Get current rebuilds for the affected organizations to show in the UI
-  const rebuildsData = await getRebuildsForOrgIds(organizationIds);
+  const products = eligibleProducts
+    .filter(
+      (p) =>
+        p.ProductBuilds[0].AppBuilderVersion !==
+        systems.get(p.Project.Organization.Id).get(p.Project.ApplicationType.Id)
+    )
+    .map((p) => {
+      return {
+        ProjectName: p.Project.Name,
+        TypeId: p.Project.ApplicationType.Id,
+        Id: p.Id,
+        NewVersion: systems.get(p.Project.Organization.Id).get(p.Project.ApplicationType.Id)
+      };
+    });
+
+  console.log(organizations);
+  console.log(products);
 
   // Fetch all available ApplicationTypes for the form toggles
   const applicationTypes = await DatabaseReads.applicationTypes.findMany({
     select: {
       Id: true,
-      Name: true,
       Description: true
     }
   });
 
+  const rebuilds = (await getRebuildsForOrgIds(Array.from(systems.keys()))).rebuilds;
   const form = await superValidate(valibot(formSchema));
 
   return {
     form,
-    rebuilds: rebuildsData.rebuilds,
     applicationTypes,
-    projects,
+    products,
+    organizations,
+    rebuilds
   };
 }) satisfies PageServerLoad;
 
 /// ACTIONS
 export const actions = {
-  //
-  /// START: Starts rebuilds for affected organizations.
-  //
   async start({ request, locals, params }) {
     // Determine what organizations are being affected and check security
     if (params.orgId) {
@@ -311,6 +144,9 @@ export const actions = {
       form.data.applicationTypeIds
     );
 
+    // TODO Simply give a product ID to have it rebuilt.
+    // WARNING check that user has permission to rebuild a product.
+    // select organizations having projects having productid matching one of these products and then locals.security.requireAdminOfOrgIn(orgs)
     // Attempt rebuilds for each affected product
     const rebuildResults = await Promise.allSettled(
       productsToRebuild.map((p) => {
