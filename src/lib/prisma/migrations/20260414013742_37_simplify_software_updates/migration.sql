dbbad992f131b9ba15372f996eebe6ffc349cb34
Judah Sotomayor <judah.sotomayor@pm.me>
2026-04-13 22:08:55 -0400

    Correct prisma migration.

--- src/lib/prisma/migrations/20260414013742_37_simplify_software_updates/migration.sql
+++ src/lib/prisma/migrations/20260414013742_37_simplify_software_updates/migration.sql
@@ -3,9 +3,9 @@
 
   - You are about to drop the column `ApplicationTypeId` on the `SoftwareUpdates` table. All the data in the column will be lost.
   - You are about to drop the column `BuildEngineUrl` on the `SoftwareUpdates` table. All the data in the column will be lost.
-  - You are about to drop the column `DateCompleted` on the `SoftwareUpdates` table. All the data in the column will be lost.
   - You are about to drop the column `DateUpdated` on the `SoftwareUpdates` table. All the data in the column will be lost.
   - You are about to drop the column `Version` on the `SoftwareUpdates` table. All the data in the column will be lost.
+  - Made the column `DateCreated` on table `SoftwareUpdates` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- DropForeignKey
@@ -15,4 +15,5 @@ ALTER TABLE "public"."SoftwareUpdates" DROP CONSTRAINT "SoftwareUpdates_Applicat
 ALTER TABLE "SoftwareUpdates" DROP COLUMN "ApplicationTypeId",
 DROP COLUMN "BuildEngineUrl",
 DROP COLUMN "DateUpdated",
-DROP COLUMN "Version";
+DROP COLUMN "Version",
+ALTER COLUMN "DateCreated" SET NOT NULL;
