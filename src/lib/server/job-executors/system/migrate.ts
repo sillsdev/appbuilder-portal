ad255b8ac65ad80585e608b7e2511670f274c891
Aidan Jones <aidan_jones@sil.org>
2026-05-08 16:54:48 -0500

    Find matching console text contents

--- src/lib/server/job-executors/system/migrate.ts
+++ src/lib/server/job-executors/system/migrate.ts
@@ -4,6 +4,7 @@ import { randomInt } from 'crypto';
 import { BuildEngine } from '../../build-engine-api';
 import { BullMQ, getQueues } from '../../bullmq';
 import { DatabaseReads, DatabaseWrites } from '../../database';
+import { checkBuildRetryCondition } from '../common.build-publish';
 import { JobSchedulerId } from '$lib/bullmq';
 import { type WorkflowInstanceContext, WorkflowState } from '$lib/workflowTypes';
 
@@ -259,7 +260,7 @@ async function renameRetryComments() {
   const filter: Prisma.ProductTransitionsWhereInput = {
     Comment: 'Build may have failed due to insufficient memory. Retrying with medium compute type.'
   };
-  const chunkSize = 100;
+  const chunkSize = 20;
 
   const before = await DatabaseReads.productTransitions.count({
     where: filter
@@ -282,16 +283,14 @@ async function renameRetryComments() {
             where: {
               ArtifactType: 'consoleText'
             },
-
             select: {
               Url: true
             }
           }
         },
         orderBy: {
-          DateCreated: 'asc'
-        },
-        take: 1
+          DateCreated: 'desc'
+        }
       }
     },
     take: chunkSize,
@@ -301,14 +300,18 @@ async function renameRetryComments() {
   const fixed = await Promise.all(
     chunk.map(async (p) => {
       try {
-        const url = p.ProductBuilds.at(0)?.ProductArtifacts?.at(0)?.Url ?? '';
+        const urls = p.ProductBuilds.map((b) => b.ProductArtifacts?.at(0)?.Url ?? '');
+        let matchingUrl = '';
+        for (const url of urls) {
+          if (url && (await checkBuildRetryCondition(url))) matchingUrl = url;
+        }
         await DatabaseWrites.productTransitions.update({
           where: { Id: p.Id },
           data: {
-            Comment: `system.build-retry,${url}`
+            Comment: `system.build-retry,${matchingUrl}`
           }
         });
-        return { ...p, ConsoleText: url };
+        return { ...p, ConsoleText: matchingUrl };
       } catch (e) {
         return { ...p, Error: e };
       }
