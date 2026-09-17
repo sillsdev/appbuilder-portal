260ed5eefbbf645d2264f24ae846e5efc9bf1e13
Aidan Jones <aejones4gm@gmail.com>
2026-09-17 12:57:51 -0500

    Change expected shape of request body in /api/projects/[id]/publishing-properties (#1540)

--- src/routes/(unauthenticated)/api/projects/[id=number]/publishing-properties/+server.ts
+++ src/routes/(unauthenticated)/api/projects/[id=number]/publishing-properties/+server.ts
@@ -4,10 +4,6 @@ import { createAppBuildersError } from '../common';
 import { DatabaseReads, DatabaseWrites } from '$lib/server/database';
 import { JSONStringSchema } from '$lib/valibot';
 
-const bodySchema = v.object({
-  properties: v.nullable(JSONStringSchema)
-});
-
 export async function POST({ params, locals, request }) {
   // Validate API token and get user ID
   try {
@@ -38,9 +34,14 @@ export async function POST({ params, locals, request }) {
   let props: string | null = null;
 
   try {
-    const parsed = v.safeParse(bodySchema, await request.json());
-    if (parsed.success && parsed.output) {
-      props = parsed.output.properties;
+    const parsed = v.safeParse(JSONStringSchema, await request.text());
+    if (parsed.success) {
+      props = parsed.output;
+    } else {
+      return createAppBuildersError(
+        400,
+        `Error validating request body: ${JSON.stringify(parsed.issues)}`
+      );
     }
   } catch {
     return createAppBuildersError(400, `Error validating request body`);
